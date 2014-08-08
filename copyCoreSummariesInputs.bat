c6039f7ace04341af73f4d5dc4dc854b73657241
Lisa Zorn <lzorn@mtc.ca.gov>
2014-08-08 14:48:23 -0700

    Snapshot - active transportation in progress but this runs clean.

--- copyCoreSummariesInputs.bat
+++ copyCoreSummariesInputs.bat
@@ -1,36 +1,40 @@
-@echo on
-set RUN_NAME=2010_04_ZZZ
+echo on
+set RUN_NAME=2020_03_116
 
 :: destination directory - on the local disk
 set TARGET_DIR=C:\Users\lzorn\Documents\%RUN_NAME%
 set TIMEPERIODS=EA AM MD PM EV
 
-:: map this drive for the copy -- it's failing otherwise :(
-:: it will prompt for the password
-set MAINMODELSHARE=\\mainmodel\MainModelShare
-set NETUSEDRIVE=B:
-
-net use %NETUSEDRIVE%
-if %ERRORLEVEL% EQU 2 (
-  net use %NETUSEDRIVE% %mainmodelshare% /user:mtcpub *
+if %RUN_NAME% EQU 2010_04_ZZZ (
+  set CITER=3
+  set RUN_DIR=B:\Projects\%RUN_NAME%.archived
+  set POPSYN_HH=hhFile.p2011s3a1.2010
+  set POPSYN_PERS=personFile.p2011s3a1.2010
 )
-net use
 
-set OUTPUT_DIR=M:\Development\Travel Model One\Version 04\%RUN_NAME%\OUTPUT
-set POPSYN_DIR=B:\Projects\%RUN_NAME%.archived\popsyn
-set DB_DIR=B:\Projects\%RUN_NAME%.archived\database
+if %RUN_NAME% EQU 2020_03_116 (
+  set CITER=1
+  set RUN_DIR=B:\Projects\2020_03_116.archived
+  set POPSYN_HH=hhFile.p2011s6g.2020
+  set POPSYN_PERS=personFile.p2011s6g.2020
+)
 
-if not exist "%TARGET_DIR%\hhFile.p2011s3a1.2010.csv" ( copy "%POPSYN_DIR%\hhFile.p2011s3a1.2010.csv"  "%TARGET_DIR%  )
-if not exist "%TARGET_DIR%\householdData_3.csv"       ( copy "%OUTPUT_DIR%\householdData_3.csv"        "%TARGET_DIR%" )
-if not exist "%TARGET_DIR%\tazData.csv"               ( copy "%OUTPUT_DIR%\tazData.csv"                "%TARGET_DIR%" )
-if not exist "%TARGET_DIR%\indivTripData_3.csv"       ( copy "%OUTPUT_DIR%\indivTripData_3.csv"        "%TARGET_DIR%" )
-if not exist "%TARGET_DIR%\jointTripData_3.csv"       ( copy "%OUTPUT_DIR%\jointTripData_3.csv"        "%TARGET_DIR%" )
+if not exist %TARGET_DIR% (mkdir %TARGET_DIR%)
+
+if not exist "%TARGET_DIR%\%POPSYN_HH%.csv"     ( copy "%RUN_DIR%\popsyn\%POPSYN_HH%.csv"     "%TARGET_DIR%" )
+if not exist "%TARGET_DIR%\%POPSYN_PERS%.csv"   ( copy "%RUN_DIR%\popsyn\%POPSYN_PERS%.csv"   "%TARGET_DIR%" )
+
+if not exist "%TARGET_DIR%\tazData.csv"               ( copy "%RUN_DIR%\INPUT\landuse\tazData.csv"             "%TARGET_DIR%" )
+if not exist "%TARGET_DIR%\householdData_3.csv"       ( copy "%RUN_DIR%\main\householdData_%CITER%.csv"        "%TARGET_DIR%" )
+if not exist "%TARGET_DIR%\personData_3.csv"          ( copy "%RUN_DIR%\main\personData_%CITER%.csv"           "%TARGET_DIR%" )
+if not exist "%TARGET_DIR%\indivTripData_3.csv"       ( copy "%RUN_DIR%\main\indivTripData_%CITER%.csv"        "%TARGET_DIR%" )
+if not exist "%TARGET_DIR%\jointTripData_3.csv"       ( copy "%RUN_DIR%\main\jointTripData_%CITER%.csv"        "%TARGET_DIR%" )
+if not exist "%TARGET_DIR%\jointTourData_3.csv"       ( copy "%RUN_DIR%\main\jointTourData_%CITER%.csv"        "%TARGET_DIR%" )
 
 FOR %%H in (%TIMEPERIODS%) DO (
   if not exist "%TARGET_DIR%\ActiveTimeSkimsDatabase%%H.csv" (
-    copy "%DB_DIR%\ActiveTimeSkimsDatabase%%H.csv" "%TARGET_DIR%"
+    copy "%RUN_DIR%\database\ActiveTimeSkimsDatabase%%H.csv" "%TARGET_DIR%"
   )
 )
 
-:: disconnect NETUSEDRIVE
-net use /delete %NETUSEDRIVE%
+:done
