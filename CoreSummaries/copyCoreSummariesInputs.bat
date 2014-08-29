fe3d1110011c23d07c30b3985b82b96483d6284e
Lisa Zorn <lzorn@mtc.ca.gov>
2014-08-29 14:09:59 -0700

    Save inputs into a subdir, cos we probably don't want to keep them and it's easier to delete them.

--- CoreSummaries/copyCoreSummariesInputs.bat
+++ CoreSummaries/copyCoreSummariesInputs.bat
@@ -12,36 +12,41 @@
 ::
 :: Files are only copied if they don't exist yet in the target directory.
 ::
+:: @echo off
+setlocal enabledelayedexpansion
 
-if not exist %TARGET_DIR% (mkdir %TARGET_DIR%)
+if not exist %TARGET_DIR%            (mkdir %TARGET_DIR%)
+if not exist %TARGET_DIR%\modelfiles (mkdir %TARGET_DIR%\modelfiles)
 
-if not exist "%TARGET_DIR%\%POPSYN_HH%.csv"     ( copy "%RUN_DIR%\popsyn\%POPSYN_HH%.csv"     "%TARGET_DIR%" )
-if not exist "%TARGET_DIR%\%POPSYN_PERS%.csv"   ( copy "%RUN_DIR%\popsyn\%POPSYN_PERS%.csv"   "%TARGET_DIR%" )
+if not exist "%TARGET_DIR%\modelfiles\%POPSYN_HH%.csv"     ( copy "%RUN_DIR%\popsyn\%POPSYN_HH%.csv"     "%TARGET_DIR%\modelfiles" )
+if not exist "%TARGET_DIR%\modelfiles\%POPSYN_PERS%.csv"   ( copy "%RUN_DIR%\popsyn\%POPSYN_PERS%.csv"   "%TARGET_DIR%\modelfiles" )
 
-if not exist "%TARGET_DIR%\tazData.csv"                    ( copy "%RUN_DIR%\INPUT\landuse\tazData.csv"         "%TARGET_DIR%" )
-if not exist "%TARGET_DIR%\householdData_%ITER%.csv"       ( copy "%RUN_DIR%\main\householdData_%ITER%.csv"     "%TARGET_DIR%" )
-if not exist "%TARGET_DIR%\personData_%ITER%.csv"          ( copy "%RUN_DIR%\main\personData_%ITER%.csv"        "%TARGET_DIR%" )
-if not exist "%TARGET_DIR%\indivTripData_%ITER%.csv"       ( copy "%RUN_DIR%\main\indivTripData_%ITER%.csv"     "%TARGET_DIR%" )
-if not exist "%TARGET_DIR%\indivTourData_%ITER%.csv"       ( copy "%RUN_DIR%\main\indivTourData_%ITER%.csv"     "%TARGET_DIR%" )
-if not exist "%TARGET_DIR%\jointTripData_%ITER%.csv"       ( copy "%RUN_DIR%\main\jointTripData_%ITER%.csv"     "%TARGET_DIR%" )
-if not exist "%TARGET_DIR%\jointTourData_%ITER%.csv"       ( copy "%RUN_DIR%\main\jointTourData_%ITER%.csv"     "%TARGET_DIR%" )
-if not exist "%TARGET_DIR%\avgload5period.csv"             ( copy "%RUN_DIR%\hwy\iter%ITER%\avgload5period.csv" "%TARGET_DIR%" )
+if not exist "%TARGET_DIR%\modelfiles\tazData.csv"                    ( copy "%RUN_DIR%\INPUT\landuse\tazData.csv"         "%TARGET_DIR%\modelfiles" )
+if not exist "%TARGET_DIR%\modelfiles\householdData_%ITER%.csv"       ( copy "%RUN_DIR%\main\householdData_%ITER%.csv"     "%TARGET_DIR%\modelfiles" )
+if not exist "%TARGET_DIR%\modelfiles\personData_%ITER%.csv"          ( copy "%RUN_DIR%\main\personData_%ITER%.csv"        "%TARGET_DIR%\modelfiles" )
+if not exist "%TARGET_DIR%\modelfiles\indivTripData_%ITER%.csv"       ( copy "%RUN_DIR%\main\indivTripData_%ITER%.csv"     "%TARGET_DIR%\modelfiles" )
+if not exist "%TARGET_DIR%\modelfiles\indivTourData_%ITER%.csv"       ( copy "%RUN_DIR%\main\indivTourData_%ITER%.csv"     "%TARGET_DIR%\modelfiles" )
+if not exist "%TARGET_DIR%\modelfiles\jointTripData_%ITER%.csv"       ( copy "%RUN_DIR%\main\jointTripData_%ITER%.csv"     "%TARGET_DIR%\modelfiles" )
+if not exist "%TARGET_DIR%\modelfiles\jointTourData_%ITER%.csv"       ( copy "%RUN_DIR%\main\jointTourData_%ITER%.csv"     "%TARGET_DIR%\modelfiles" )
+if not exist "%TARGET_DIR%\modelfiles\avgload5period.csv"             ( copy "%RUN_DIR%\hwy\iter%ITER%\avgload5period.csv" "%TARGET_DIR%\modelfiles" )
 
 set TIMEPERIODS=EA AM MD PM EV
 FOR %%H in (%TIMEPERIODS%) DO (
-  if not exist "%TARGET_DIR%\ActiveTimeSkimsDatabase%%H.csv" (
-    copy "%RUN_DIR%\database\ActiveTimeSkimsDatabase%%H.csv" "%TARGET_DIR%"
+  if not exist "%TARGET_DIR%\modelfiles\ActiveTimeSkimsDatabase%%H.csv" (
+    copy "%RUN_DIR%\database\ActiveTimeSkimsDatabase%%H.csv" "%TARGET_DIR%\modelfiles"
   )
-  if not exist "%TARGET_DIR%\CostSkimsDatabase%%H.csv" (
-    copy "%RUN_DIR%\database\CostSkimsDatabase%%H.csv" "%TARGET_DIR%"
+  if not exist "%TARGET_DIR%\modelfiles\CostSkimsDatabase%%H.csv" (
+    copy "%RUN_DIR%\database\CostSkimsDatabase%%H.csv" "%TARGET_DIR%\modelfiles"
   )
-  if not exist "%TARGET_DIR%\DistanceSkimsDatabase%%H.csv" (
-    copy "%RUN_DIR%\database\DistanceSkimsDatabase%%H.csv" "%TARGET_DIR%"
+  if not exist "%TARGET_DIR%\modelfiles\DistanceSkimsDatabase%%H.csv" (
+    copy "%RUN_DIR%\database\DistanceSkimsDatabase%%H.csv" "%TARGET_DIR%\modelfiles"
   )
   
-  if not exist "%TARGET_DIR%\TimeSkimsDatabase%%H.csv" (
-    copy "%RUN_DIR%\database\TimeSkimsDatabase%%H.csv" "%TARGET_DIR%"
+  if not exist "%TARGET_DIR%\modelfiles\TimeSkimsDatabase%%H.csv" (
+    copy "%RUN_DIR%\database\TimeSkimsDatabase%%H.csv" "%TARGET_DIR%\modelfiles"
   )
 )
+
+endlocal
 
 :done
