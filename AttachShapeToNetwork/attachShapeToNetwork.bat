682c4a24ce8e282d0e706edf68cbb0c68049c930
Lisa Zorn <lzorn@mtc.ca.gov>
2014-09-18 17:04:29 -0700

    Initial revision of attach shape to network script

---
+++ AttachShapeToNetwork/attachShapeToNetwork.bat
@@ -0,0 +1,6 @@
+
+set CODEDIR=C:\Users\lzorn\Documents\Travel Model One Utilities\AttachShapeToNetwork
+
+:: copy B:\Projects\2010_04_ZZZ.archived\INPUT\hwy\freeflow.net .
+
+python "%CODEDIR%\attachShapeToNetwork.py" -s id -s name -c cityid -c cityname freeflow.NET "G:\Workspace Users\lzorn\PBA_Cities_NAD_1983_UTM_Zone_10N.shp" attach_cityname_test\freeflow_cities.net
