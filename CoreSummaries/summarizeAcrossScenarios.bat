4bc5cb77c1d5a010855067e2856047fccb270878
Lisa Zorn <lzorn@mtc.ca.gov>
2014-09-26 14:09:32 -0700

    Small updates.
    I was having batch troubles.  Shouldn't really change anything

--- CoreSummaries/summarizeAcrossScenarios.bat
+++ CoreSummaries/summarizeAcrossScenarios.bat
@@ -8,7 +8,7 @@ set RUN_NAME_SET=2010_04_ZZZ 2040_03_116 2040_03_127
 :: (e.g. subdirs = CTRAMP, database, hwy, INPUT, landuse, etc...)
 :: Set to 0 if if running from from results directory
 :: (e.g. M:\Application\, subdirs=INPUT,OUTPUT)
-set ORIGINAL_RUNDIR=0
+set ORIGINAL_RUNDIR=1
 
 IF %USERNAME%==lzorn (
   rem I AM SPECIAL
@@ -31,14 +31,14 @@ set RDATA=ActiveTransport ActivityPattern AutomobileOwnership CommuteByEmploymen
 
 :: Create the Tablea Data Extracts covering all scenarios
 :: First, create the summary dirs list
-IF %ORIGINAL_RUNDIR% EQU 1 (
-  set SUMMARY_DIRS=%RUN_NAME_SET: =\core_summaries %
-  set SUMMARY_DIRS=%SUMMARY_DIRS%\core_summaries
-) ELSE (
-  set SUMMARY_DIRS=%RUN_NAME_SET: =\OUTPUT\core_summaries %
-  set SUMMARY_DIRS=%SUMMARY_DIRS%\OUTPUT\core_summaries
+set SUMMARY_DIRS=%RUN_NAME_SET: =\core_summaries %
+set SUMMARY_DIRS=%SUMMARY_DIRS%\core_summaries
+:: insert OUTPUT into paths
+IF %ORIGINAL_RUNDIR%==0 (
+  set SUMMARY_DIRS=%SUMMARY_DIRS:\core_summaries=\OUTPUT\core_summaries%
 )
-echo SUMMARY_DIRS=[%SUMMARY_DIRS%]
+echo Reading Rdata files from SUMMARY_DIRS=
+echo   [%SUMMARY_DIRS%]
 
 :: Run the conversion script to aggregate all rdata files into a single tde
 for %%H in (%RDATA%) DO (
@@ -50,28 +50,26 @@ for %%H in (%RDATA%) DO (
 )
 
 :: Convert the avgload5period.csv
-IF %ORIGINAL_RUNDIR% EQU 1 (
-  set MODELFILE_DIRS=%RUN_NAME_SET: =\hwy\iter3 %
-  set MODELFILE_DIRS=%MODELFILE_DIRS%\hwy\iter3
-) ELSE (
-  set MODELFILE_DIRS=%RUN_NAME_SET: =\OUTPUT %
-  set MODELFILE_DIRS=%MODELFILE_DIRS%\OUTPUT
+set HWYFILE_DIRS=%RUN_NAME_SET: =\hwy\iter3 %
+set HWYFILE_DIRS=%HWYFILE_DIRS%\hwy\iter3
+IF %ORIGINAL_RUNDIR%==0 (
+  set HWYFILE_DIRS=%HWYFILE_DIRS:\hwy\iter3=\OUTPUT %
 )
-echo MODELFILE_DIRS=[%MODELFILE_DIRS%]
+echo Reading avgload5period.csv files from HWYFILE_DIRS=
+echo   [%HWYFILE_DIRS%]
 
 if not exist "%COMBINED_DIR%\avgload5period.tde" (
-  python "%CODE_DIR%\csvToTableauExtract.py" %MODELFILE_DIRS% "%COMBINED_DIR%" avgload5period.csv
+  python "%CODE_DIR%\csvToTableauExtract.py" %HWYFILE_DIRS% "%COMBINED_DIR%" avgload5period.csv
 )
 
 :: convert the transit files
-IF %ORIGINAL_RUNDIR% EQU 1 (
-  set MODELFILE_DIRS=%RUN_NAME_SET: =\trn %
-  set MODELFILE_DIRS=%MODELFILE_DIRS%\trn
-) ELSE (
-  set MODELFILE_DIRS=%RUN_NAME_SET: =\OUTPUT\trn %
-  set MODELFILE_DIRS=%MODELFILE_DIRS%\OUTPUT\trn
+set TRNFILE_DIRS=%RUN_NAME_SET: =\trn %
+set TRNFILE_DIRS=%TRNFILE_DIRS%\trn
+IF %ORIGINAL_RUNDIR%==0 (
+  set TRNFILE_DIRS=%TRNFILE_DIRS:\trn=\OUTPUT\trn%
 )
-echo MODELFILE_DIRS=[%MODELFILE_DIRS%]
+echo Reading trnline*.csv files from TRNFILE_DIRS=
+echo   [%TRNFILE_DIRS%]
 
 if not exist "%COMBINED_DIR%\trnline.tde" (
   FOR %%H in (EA AM MD PM EV) DO (
