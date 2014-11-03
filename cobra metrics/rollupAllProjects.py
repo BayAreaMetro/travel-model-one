15341778cb1d1b00fa311dfa514fcfd658f5ea0c
Lisa Zorn <lzorn@mtc.ca.gov>
2014-11-03 14:43:11 -0800

    Output two files: data and description

--- cobra metrics/rollupAllProjects.py
+++ cobra metrics/rollupAllProjects.py
@@ -15,30 +15,44 @@ USAGE = """
 
 if __name__ == '__main__':
 
-    ALL_PROJECTS_FILENAME = "AllProjects.csv"
+    ALL_PROJECTS_DATA_FILENAME = "AllProjects_Data.csv"
+    ALL_PROJECTS_DESC_FILENAME = "AllProjects_Desc.csv"
+
     all_projs_dict = {}
     for proj_file in os.listdir("."):
 
         # skip this one, if it exists already
-        if proj_file == ALL_PROJECTS_FILENAME: continue
+        if proj_file == ALL_PROJECTS_DATA_FILENAME: continue
+        if proj_file == ALL_PROJECTS_DESC_FILENAME: continue
+        if proj_file == "base.csv": continue
 
-        proj_series = pd.Series.from_csv(proj_file, index_col=[0,1,2])
+        proj_series = pd.Series.from_csv(proj_file, index_col=[0,1,2], header=0)
         proj_id     = proj_series.loc['Project ID',numpy.NaN,numpy.NaN]
         assert("%s.csv" % proj_id == proj_file)
         all_projs_dict[proj_id] = proj_series
 
 
-    all_projs_dataframe = pd.DataFrame(all_projs_dict).transpose()
-    
-    # flatten the multi index for Tableau
-    new_cols = []
-    for col in all_projs_dataframe.columns:
-        if str(col[1]) == "nan":
-            new_cols.append(col[0])
-        else:
-            new_cols.append("%s - %s - %s" % col)
+    all_projs_dataframe = pd.DataFrame(all_projs_dict)
+    # print all_projs_dataframe
+    # all_projs_dataframe now has a 3-tuple MultiIndex for rows,
+    # and the columns are the project ids
 
-    all_projs_dataframe.columns = pd.Index(new_cols)
-    del all_projs_dataframe['category1 - category2 - variable_name']
-    print all_projs_dataframe
-    all_projs_dataframe.to_csv(ALL_PROJECTS_FILENAME, index=False)
+    # save the project attributes (constants) aside for later
+    projs_desc          = all_projs_dataframe.iloc[:16,]
+
+    # stack the rows -- that is, before the columns were the project ids, one col per project
+    # convert it to rows -- so there's one value column, and a row per project id
+    all_projs_dataframe = all_projs_dataframe.iloc[16:,].stack()
+    # Name the new column in the index 'Values'
+    new_names = list(all_projs_dataframe.index.names)
+    new_names[-1] = 'Project ID'
+    all_projs_dataframe.index.names = new_names
+    all_projs_dataframe.name = 'Values'
+
+    # save this
+    all_projs_dataframe.to_csv(ALL_PROJECTS_DATA_FILENAME, header=True)
+
+    projs_desc.index = projs_desc.index.get_level_values(0)
+    projs_desc = projs_desc.transpose()
+    projs_desc.to_csv(ALL_PROJECTS_DESC_FILENAME, index=False, header=True)
+
