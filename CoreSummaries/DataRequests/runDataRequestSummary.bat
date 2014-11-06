d14ff3793842cb9d2c7a1bb31f362c06da3a4b14
Lisa Zorn <lzorn@mtc.ca.gov>
2014-11-06 10:14:29 -0800

    TARGET_DIR is assumed to be the directory in which this is running.  It can have a space.

--- CoreSummaries/DataRequests/runDataRequestSummary.bat
+++ CoreSummaries/DataRequests/runDataRequestSummary.bat
@@ -1,13 +1,15 @@
 ::
-:: Example batch file to create a requested summary
+:: Example batch file to create a requested summary.
+:: Set SCRIPT and run in the target directory
 ::
 set R_HOME=C:\Program Files\R\R-3.1.1
 set ITER=3
-set TARGET_DIR=C:\Users\lzorn\Documents\2010_04_ZZZ
+set TARGET_DIR=%CD%
 set CODE_DIR=C:\Users\lzorn\Documents\Travel-Model-One-Utilities\CoreSummaries\DataRequests
 :: leave off the .Rmd
 set SCRIPT=Trips_origSD_destSD_departhour_tripmode
 
 call "%R_HOME%\bin\x64\Rscript.exe" --vanilla "%CODE_DIR%\knit_Rmd.R"
 
-move %SCRIPT%.html %TARGET_DIR%\core_summaries
+move "%SCRIPT%.html" "%TARGET_DIR%\core_summaries"
+del "%SCRIPT%.md"
