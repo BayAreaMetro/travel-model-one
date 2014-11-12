4f87503c86719cdbdfb83a73d4cac6fd54af69f8
Lisa Zorn <lzorn@mtc.ca.gov>
2014-11-12 10:32:44 -0800

    Adding RunModel and RunIteration batch files

---
+++ model-files/RunIteration.bat
@@ -0,0 +1,178 @@
+::~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+:: RunIteration.bat
+::
+:: MS-DOS batch file to execute a single iteration of the MTC travel model.  This script is repeatedly 
+:: called by the RunModel batch file.  
+::
+:: For complete details, please see http://mtcgis.mtc.ca.gov/foswiki/Main/RunIterationBatch.
+::
+:: dto (2012 02 15) gde (2009 10 9)
+::
+::~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+:: ------------------------------------------------------------------------------------------------------
+::
+:: Step 0:  If iteration equals zero, go to step four (i.e. skip the demand models)
+::
+:: ------------------------------------------------------------------------------------------------------
+
+if %ITER%==0 goto hwyAssign
+
+
+:: ------------------------------------------------------------------------------------------------------
+::
+:: Step 1:  Build highway and transit skims
+::
+:: ------------------------------------------------------------------------------------------------------
+
+:skims
+
+:: Create the automobile level-of-service matrices
+runtpp CTRAMP\scripts\skims\HwySkims.job
+if ERRORLEVEL 2 goto done
+
+:: Prepare the highway network for use by the transit network
+runtpp CTRAMP\scripts\skims\PrepHwyNet.job
+if ERRORLEVEL 2 goto done
+
+:: Create the transit networks
+runtpp CTRAMP\scripts\skims\BuildTransitNetworks.job
+if ERRORLEVEL 2 goto done
+
+:: Create the public transport level-of-service matrices
+runtpp CTRAMP\scripts\skims\TransitSkims.job
+if ERRORLEVEL 2 goto done
+
+:: Create accessibility measures for use by the automobile ownership sub-model
+runtpp CTRAMP\scripts\skims\Accessibility.job
+if ERRORLEVEL 2 goto done
+
+
+:: ------------------------------------------------------------------------------------------------------
+::
+:: Step 2:  Execute the choice models using CT-RAMP java code
+::
+:: ------------------------------------------------------------------------------------------------------
+
+:core
+
+::  Call the MtcTourBasedModel class
+java -showversion -Xmx6000m -cp %CLASSPATH% -Dlog4j.configuration=log4j.xml -DJAVA_HOME_32="%JAVA_PATH_32%" -DJAVA_32_PORT=1181 -Djava.library.path=%RUNTIME% -Djppf.config=jppf-client.properties com.pb.mtc.ctramp.MtcTourBasedModel mtcTourBased -iteration %ITER% -sampleRate %SAMPLESHARE% -sampleSeed %SEED%
+if ERRORLEVEL 2 goto done
+
+
+:: ------------------------------------------------------------------------------------------------------
+::
+:: Step 3:  Execute the internal/external and commercial vehicle models
+::
+:: ------------------------------------------------------------------------------------------------------
+
+:nonres
+
+:: Apply diurnal factors to the fixed internal/external demand matrices
+runtpp CTRAMP\scripts\nonres\IxTimeOfDay.job
+if ERRORLEVEL 2 goto done
+
+:: Apply a value toll choice model for the interna/external demand
+runtpp CTRAMP\scripts\nonres\IxTollChoice.job
+if ERRORLEVEL 2 goto done
+
+:: Apply the commercial vehicle generation models
+runtpp CTRAMP\scripts\nonres\TruckTripGeneration.job
+if ERRORLEVEL 2 goto done
+
+:: Apply the commercial vehicle distribution models
+runtpp CTRAMP\scripts\nonres\TruckTripDistribution.job
+if ERRORLEVEL 2 goto done
+
+:: Apply the commercial vehicle diurnal factors
+runtpp CTRAMP\scripts\nonres\TruckTimeOfDay.job
+if ERRORLEVEL 2 goto done
+
+:: Apply a value toll choice model for eligibile commercial demand
+runtpp CTRAMP\scripts\nonres\TruckTollChoice.job
+if ERRORLEVEL 2 goto done
+
+
+:: ------------------------------------------------------------------------------------------------------
+::
+:: Step 4:  Build matrices from trip lists and assign trips to the highway network
+::
+:: ------------------------------------------------------------------------------------------------------
+
+:hwyAssign
+
+:: If demand models were executed, translate the trip lists to demand matrices
+if %ITER% GTR 0 (
+	runtpp CTRAMP\scripts\assign\PrepAssign.job
+	if ERRORLEVEL 2 goto done
+)
+
+:: Assign the demand matrices to the highway network
+runtpp CTRAMP\scripts\assign\HwyAssign.job
+if ERRORLEVEL 2 goto done
+
+
+:: ------------------------------------------------------------------------------------------------------
+::
+:: Step 5:  Prepare the networks for the next iteration
+::
+:: ------------------------------------------------------------------------------------------------------
+
+:feedback
+
+
+:: Move assigned networks to a iteration-specific directory
+mkdir hwy\iter%ITER%      
+
+move hwy\LOADEA.net hwy\iter%ITER%\LOADEA.net
+move hwy\LOADAM.net hwy\iter%ITER%\LOADAM.net
+move hwy\LOADMD.net hwy\iter%ITER%\LOADMD.net
+move hwy\LOADPM.net hwy\iter%ITER%\LOADPM.net
+move hwy\LOADEV.net hwy\iter%ITER%\LOADEV.net
+
+:: Give the default TP+ variables more intuitive names
+runtpp CTRAMP\scripts\feedback\RenameAssignmentVariables.job
+
+:: Average the demand for this and the previous iteration and compute a speed estimate for each link 
+IF %ITER% GTR 1 (
+	runtpp CTRAMP\scripts\feedback\AverageNetworkVolumes.job
+	if ERRORLEVEL 2 goto done
+	runtpp CTRAMP\scripts\feedback\CalculateSpeeds.job
+	if ERRORLEVEL 2 goto done
+) ELSE (
+	copy hwy\iter%ITER%\LOADEA_renamed.net hwy\iter%ITER%\avgLOADEA.net /Y
+	copy hwy\iter%ITER%\LOADAM_renamed.net hwy\iter%ITER%\avgLOADAM.net /Y
+	copy hwy\iter%ITER%\LOADMD_renamed.net hwy\iter%ITER%\avgLOADMD.net /Y
+	copy hwy\iter%ITER%\LOADPM_renamed.net hwy\iter%ITER%\avgLOADPM.net /Y
+	copy hwy\iter%ITER%\LOADEV_renamed.net hwy\iter%ITER%\avgLOADEV.net /Y
+)
+
+:: Compute network statistics to measure convergence
+runtpp CTRAMP\scripts\feedback\TestNetworkConvergence.job
+if ERRORLEVEL 2 goto done
+
+:: Combine the time-of-day-specific networks into a single network
+runtpp CTRAMP\scripts\feedback\MergeNetworks.job  
+if ERRORLEVEL 2 goto done                
+
+:: Place a copy of the loaded networks into the root \hwy directory for access by the next iteration
+copy hwy\iter%ITER%\avgLOADEA.net hwy\avgLOADEA.net /Y
+copy hwy\iter%ITER%\avgLOADAM.net hwy\avgLOADAM.net /Y
+copy hwy\iter%ITER%\avgLOADMD.net hwy\avgLOADMD.net /Y
+copy hwy\iter%ITER%\avgLOADPM.net hwy\avgLOADPM.net /Y
+copy hwy\iter%ITER%\avgLOADEV.net hwy\avgLOADEV.net /Y
+
+:: Delete temporary files
+del hwy\iter%ITER%\x*.net
+
+
+:: ------------------------------------------------------------------------------------------------------
+::
+:: Last Step:  Stamp the time of completion to the feedback report file
+::
+:: ------------------------------------------------------------------------------------------------------
+
+echo FINISHED ITERATION %ITER%  %DATE% %TIME% >> logs\feedback.rpt 
+
+:done
---
+++ model-files/RunModel.bat
@@ -0,0 +1,282 @@
+::~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+:: RunModel.bat
+::
+:: MS-DOS batch file to execute the MTC travel model.  Each of the model steps are sequentially
+:: called here.  
+::
+:: For complete details, please see http://mtcgis.mtc.ca.gov/foswiki/Main/RunModelBatch.
+::
+:: dto (2012 02 15) gde (2009 04 22)
+::
+::~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+:: ------------------------------------------------------------------------------------------------------
+::
+:: Step 1:  Set the necessary path variables
+::
+:: ------------------------------------------------------------------------------------------------------
+
+:: The location of the 64-bit java development kit
+set JAVA_PATH=C:\Program Files\Java\jdk1.7.0_51
+
+:: The location of the 32-bit java runtime environment
+set JAVA_PATH_32=C:\Program Files (x86)\Java\jre6
+
+:: The location of the GAWK binary executable files
+set GAWK_PATH=M:\UTIL\Gawk
+
+:: The location of the RUNTPP executable from Citilabs
+set TPP_PATH=C:\Program Files (x86)\Citilabs\CubeVoyager
+
+:: The location of the MTC.JAR file
+set RUNTIME=CTRAMP/runtime
+
+:: Add these variables to the PATH environment variable, moving the current path to the back
+set OLD_PATH=%PATH%
+set PATH=%RUNTIME%;%JAVA_PATH%/bin;%TPP_PATH%;%GAWK_PATH%/bin;%OLD_PATH%
+
+::  Set the Java classpath (locations where Java needs to find configuration and JAR files)
+set CLASSPATH=%RUNTIME%/config;%RUNTIME%;%RUNTIME%/config/jppf-2.4/jppf-2.4-admin-ui/lib/*;%RUNTIME%/mtc.jar
+
+::  Set the IP address of the host machine which sends tasks to the client machines 
+set HOST_IP_ADDRESS=192.168.1.200
+
+
+:: ------------------------------------------------------------------------------------------------------
+::
+:: Step 2:  Create the directory structure
+::
+:: ------------------------------------------------------------------------------------------------------
+
+:: Create the working directories
+mkdir hwy
+mkdir trn
+mkdir skims
+mkdir landuse
+mkdir popsyn
+mkdir nonres
+mkdir main
+mkdir logs
+mkdir database
+
+:: Stamp the feedback report with the date and time of the model start
+echo STARTED MODEL RUN  %DATE% %TIME% >> logs\feedback.rpt 
+
+:: Move the input files, which are not accessed by the model, to the working directories
+copy INPUT\hwy\                 hwy\
+copy INPUT\trn\transit_lines\   trn\
+copy INPUT\trn\transit_fares\   trn\ 
+copy INPUT\trn\transit_support\ trn\
+copy INPUT\landuse\             landuse\
+copy INPUT\popsyn\              popsyn\
+copy INPUT\nonres\              nonres\
+copy INPUT\warmstart\main\	main\
+copy INPUT\warmstart\nonres\	nonres\
+
+
+:: ------------------------------------------------------------------------------------------------------
+::
+:: Step 3:  Pre-process steps
+::
+:: ------------------------------------------------------------------------------------------------------
+
+: Pre-Process
+
+:: Set the prices in the roadway network
+runtpp CTRAMP\scripts\preprocess\SetTolls.job
+if ERRORLEVEL 2 goto done
+
+:: Set a penalty to dummy links connecting HOV/HOT lanes and general purpose lanes
+runtpp CTRAMP\scripts\preprocess\SetHovXferPenalties.job
+if ERRORLEVEL 2 goto done
+
+:: Create time-of-day-specific 
+runtpp CTRAMP\scripts\preprocess\CreateFiveHighwayNetworks.job
+if ERRORLEVEL 2 goto done
+
+
+:: ------------------------------------------------------------------------------------------------------
+::
+:: Step 4:  Build non-motorized level-of-service matrices
+::
+:: ------------------------------------------------------------------------------------------------------
+
+: Non-Motorized Skims
+
+:: Translate the roadway network into a non-motorized network
+runtpp CTRAMP\scripts\skims\CreateNonMotorizedNetwork.job
+if ERRORLEVEL 2 goto done
+
+:: Build the skim tables
+runtpp CTRAMP\scripts\skims\NonMotorizedSkims.job
+if ERRORLEVEL 2 goto done
+
+
+:: ------------------------------------------------------------------------------------------------------
+::
+:: Step 5:  Prepare for Iteration 0
+::
+:: ------------------------------------------------------------------------------------------------------
+
+: iter0
+
+:: Set the iteration parameters
+set ITER=0
+set PREV_ITER=0
+set WGT=1.0
+set PREV_WGT=0.00
+
+
+:: ------------------------------------------------------------------------------------------------------
+::
+:: Step 6:  Execute the RunIteration batch file
+::
+:: ------------------------------------------------------------------------------------------------------
+
+call CTRAMP\RunIteration.bat
+if ERRORLEVEL 2 goto done
+
+:: ------------------------------------------------------------------------------------------------------
+::
+:: Step 7:  Prepare for iteration 1 and execute RunIteration batch file
+::
+:: ------------------------------------------------------------------------------------------------------
+
+: iter1
+
+:: Set the iteration parameters
+set ITER=1
+set PREV_ITER=1
+set WGT=1.0
+set PREV_WGT=0.00
+set SAMPLESHARE=0.15
+set SEED=0
+
+:: Set the usual workplace shadow pricing parameters
+copy CTRAMP\runtime\mtcTourBased-master.properties CTRAMP\runtime\mtcTourBased.properties
+echo. >> CTRAMP\runtime\mtcTourBased.properties
+echo UsualWorkAndSchoolLocationChoice.ShadowPricing.MaximumIterations = 4 >> CTRAMP\runtime\mtcTourBased.properties
+
+:: Call RunIteration batch file
+call CTRAMP\RunIteration.bat
+if ERRORLEVEL 2 goto done
+
+
+:: ------------------------------------------------------------------------------------------------------
+::
+:: Step 8:  Prepare for iteration 2 and execute RunIteration batch file
+::
+:: ------------------------------------------------------------------------------------------------------
+
+: iter2
+
+:: Set the iteration parameters
+set ITER=2
+set PREV_ITER=1
+set WGT=0.50
+set PREV_WGT=0.50
+set SAMPLESHARE=0.30
+set SEED=0
+
+:: Set the usual workplace shadow pricing parameters
+copy CTRAMP\runtime\mtcTourBased-master.properties CTRAMP\runtime\mtcTourBased.properties
+echo. >> CTRAMP\runtime\mtcTourBased.properties
+echo UsualWorkAndSchoolLocationChoice.ShadowPrice.Input.File          = main/ShadowPricing_3.csv >> CTRAMP\runtime\mtcTourBased.properties
+echo UsualWorkAndSchoolLocationChoice.ShadowPricing.MaximumIterations = 2 >> CTRAMP\runtime\mtcTourBased.properties
+
+:: Call RunIteration batch file
+call CTRAMP\RunIteration.bat
+if ERRORLEVEL 2 goto done
+
+
+:: ------------------------------------------------------------------------------------------------------
+::
+:: Step 9:  Prepare for iteration 3 and execute RunIteration batch file
+::
+:: ------------------------------------------------------------------------------------------------------
+
+: iter3
+
+:: Set the iteration parameters
+set ITER=3
+set PREV_ITER=2
+set WGT=0.33
+set PREV_WGT=0.67
+set SAMPLESHARE=1.00
+set SEED=0
+
+:: Set the usual workplace shadow pricing parameters
+copy CTRAMP\runtime\mtcTourBased-master.properties CTRAMP\runtime\mtcTourBased.properties
+echo.  >> CTRAMP\runtime\mtcTourBased.properties
+echo UsualWorkAndSchoolLocationChoice.ShadowPrice.Input.File          = main/ShadowPricing_5.csv >> CTRAMP\runtime\mtcTourBased.properties
+echo UsualWorkAndSchoolLocationChoice.ShadowPricing.MaximumIterations = 2 >> CTRAMP\runtime\mtcTourBased.properties
+
+:: Call RunIteration batch file
+call CTRAMP\RunIteration.bat
+if ERRORLEVEL 2 goto done
+
+
+:: ------------------------------------------------------------------------------------------------------
+::
+:: Step 10:  Assign transit trips to the transit network
+::
+:: ------------------------------------------------------------------------------------------------------
+
+: trnAssign
+
+runtpp CTRAMP\scripts\assign\TransitAssign.job
+if ERRORLEVEL 2 goto done
+
+
+:: ------------------------------------------------------------------------------------------------------
+::
+:: Step 11:  Build simplified skim databases
+::
+:: ------------------------------------------------------------------------------------------------------
+
+: database
+
+runtpp CTRAMP\scripts\database\SkimsDatabase.job
+if ERRORLEVEL 2 goto done
+
+
+:: ------------------------------------------------------------------------------------------------------
+::
+:: Step 12:  Build destination choice logsums
+::
+:: ------------------------------------------------------------------------------------------------------
+
+: logsums
+
+call RunAccessibility
+if ERRORLEVEL 2 goto done
+
+
+
+:: ------------------------------------------------------------------------------------------------------
+::
+:: Step 13:  Directory clean up
+::
+:: ------------------------------------------------------------------------------------------------------
+
+
+: cleanup
+
+:: Put the PATH back the way you found it
+set PATH=%OLD_PATH%
+
+:: Move all the TP+ printouts to the \logs folder
+copy *.prn logs\*.prn
+
+:: Delete all the temporary TP+ printouts and cluster files
+del *.prn
+del *.script.*
+del *.script
+
+:: Success target and message
+:success
+ECHO FINISHED SUCCESSFULLY!
+
+:: Complete target and message
+:done
+ECHO FINISHED.  
