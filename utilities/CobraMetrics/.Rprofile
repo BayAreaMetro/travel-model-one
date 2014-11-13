bac4fd3e0aaab9692af0ce913444be92e1439f56
Lisa Zorn <lzorn@mtc.ca.gov>
2014-11-12 18:42:29 -0800

    Replace joinTripsWithIncome.job with .R version; the tpplus script is way too slow. Also name output main\trips(EA|AM|MD|PM|EV)allinc.tpp so as not to overwrite originals.

---
+++ utilities/CobraMetrics/.Rprofile
@@ -0,0 +1,5 @@
+.First <- function() {
+  Sys.setenv(TARGET_DIR="C:\\Users\\lzorn\\Documents\\2010_04_ZZZ")  # The location of the input files
+  Sys.setenv(ITER="3")
+  # cat("\nWelcome at", date(), "\n")
+}
--- utilities/CobraMetrics/RunMetrics.bat
+++ utilities/CobraMetrics/RunMetrics.bat
@@ -8,11 +8,15 @@ set ITER=3
 :: * SAMPLESHARE=the sampling used for this iteration
 set SAMPLESHARE=1.00
 :: * ALL_PROJECT_METRICS_DIR=the location to collect all the metrics files from projects.
+::   These will be rolled up into a single dashboard.
 set ALL_PROJECT_METRICS_DIR=..\all_project_metrics
-:: These will be rolled up into a single dashboard.
+:: * Location of R
+set R_HOME=C:\Program Files\R\R-3.1.1
 
 :: Location of the metrics scripts
 set CODE_DIR=.\CTRAMP\scripts\metrics
+:: Location of the model files
+set TARGET_DIR=%CD%
 
 :: Required input file:  INPUT\metrics\BC_config.csv
 if not exist metrics (mkdir metrics)
@@ -25,22 +29,21 @@ if not exist metrics\autos_owned.csv (
   python "%CODE_DIR%\tallyAutos.py"
 )
 
-if not exist main\IndivTripDataIncome_%ITER%.csv (
+if not exist main\indivTripDataIncome_%ITER%.csv (
   rem Attach income to individual trips.  Uses 2 processes.
-  rem Input : main\IndivTripData_%ITER%.csv,  main\householdData_%ITER%.csv,
-  rem         main\ointTripData_%ITER%.csv,   main\householdData_%ITER%.csv
-  rem Output: main\IndivTripDataIncome.csv,   main\JointTripDataIncome.csv
-  runtpp "%CODE_DIR%\joinTripsWithIncome.job"
+  rem Input : main\householdData_%ITER%.csv,
+  rem         main\indivTripData_%ITER%.csv, main\jointTripData_%ITER%.csv
+  rem Output: main\indivTripDataIncome.csv,  main\JointTripDataIncome.csv
+  call "%R_HOME%\bin\Rscript.exe" "%CODE_DIR%\joinTripsWithIncome.R"
   IF ERRORLEVEL 2 goto error
 )
 
-if not exist main\tripsEV.tpp (
+if not exist main\tripsEVinc1.tpp (
   rem Convert trip tables into time/income/mode OD matrices
   rem Input : main\(indiv|joint)TripDataIncome.csv
-  rem Output: main\IndivTrips(EA|AM|MD|PM|EV)inc[1-4].dat,
-  rem         main\Jointrips(EA|AM|MD|PM|EV)inc[1-4].dat
+  rem Output: main\(indiv|join)Trips(EA|AM|MD|PM|EV)inc[1-4].dat,
   rem         main\trips(EA|AM|MD|PM|EV)inc[1-4].tpp,
-  rem         main\trips(EA|AM|MD|PM|EV).tpp
+  rem         main\trips(EA|AM|MD|PM|EV)allinc.tpp
   runtpp "%CODE_DIR%\prepAssignIncome.job"
   IF ERRORLEVEL 2 goto error
 )
@@ -49,7 +52,7 @@ if not exist metrics (mkdir metrics)
 
 if not exist metrics\transit_times_by_mode_income.csv (
   rem Reads trip tables and skims and outputs tallies for trip attributes
-  rem Input : trips(EA|AM|MD|PM|EV).tpp, transit skims
+  rem Input : main\trips(EA|AM|MD|PM|EV)allinc.tpp, transit skims
   rem Output: metrics\transit_times.csv
   runtpp "%CODE_DIR%\sumTransitTimes.job"
   if ERRORLEVEL 2 goto error
@@ -57,7 +60,7 @@ if not exist metrics\transit_times_by_mode_income.csv (
 
 if not exist metrics\auto_times.csv (
   rem Reads trip tables and skims and outputs tallies for trip attributes
-  rem Input : trips(EA|AM|MD|PM|EV).tpp, hwy skims
+  rem Input : main\trips(EA|AM|MD|PM|EV)inc[1-4].tpp, hwy skims
   rem Output: metrics\auto_times.csv
   runtpp "%CODE_DIR%\sumAutoTimes.job"
   if ERRORLEVEL 2 goto error
@@ -65,7 +68,7 @@ if not exist metrics\auto_times.csv (
 
 if not exist metrics\nonmot_times.csv (
   rem Reads trip tables and skims and outputs tallies for trip attributes
-  rem Input : trips(EA|AM|MD|PM|EV).tpp, hwy skims
+  rem Input : trips(EA|AM|MD|PM|EV)inc[1-4].tpp, hwy skims
   rem Output: metrics\nonmot_times.csv
   runtpp "%CODE_DIR%\sumNonmotTimes.job"
   if ERRORLEVEL 2 goto error
