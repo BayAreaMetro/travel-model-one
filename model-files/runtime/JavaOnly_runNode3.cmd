c49acbdf31ff309beccb0205f62b1b453d8fa0ff
Lisa Zorn <lzorn@mtc.ca.gov>
2014-12-04 15:10:47 -0800

    Runtime files from M:\Development\Travel Model One\Version 04\2010_04_ZZZ\CTRAMP\runtime

---
+++ model-files/runtime/JavaOnly_runNode3.cmd
@@ -0,0 +1,20 @@
+cd ..
+cd ..
+mkdir logs
+
+rem ############  PARAMETERS  ############
+set JAVA_PATH=C:\Program Files\Java\jdk1.7.0_51
+set GAWK_PATH=M:\UTIL\Gawk
+set TPP_PATH=C:\Program Files (x86)\Citilabs\CubeVoyager
+
+set RUNTIME=CTRAMP/runtime
+set OLD_PATH=%PATH%
+set PATH=%RUNTIME%;%JAVA_PATH%/bin;%TPP_PATH%;%GAWK_PATH%/bin;%OLD_PATH%
+set CLASSPATH=%RUNTIME%/config;%RUNTIME%;%RUNTIME%/config/jppf-2.4/jppf-2.4-admin-ui/lib/*;%RUNTIME%/mtc.jar
+
+set HOST_IP=192.168.1.200
+
+rem ############  JPPF DRIVER  ############
+start java -server -Xmx128m -Dlog4j.configuration=log4j-node3.xml -Djppf.config=jppf-node3.properties org.jppf.node.NodeLauncher
+
+set PATH=%OLD_PATH%
