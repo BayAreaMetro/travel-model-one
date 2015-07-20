5d6ceb3e86bdac756313361a4588ef627e965068
Lisa Zorn <lzorn@mtc.ca.gov>
2015-07-20 12:43:30 -0700

    Use warmstart input trip tables and run Normal Frank Wolfe and BiConjugate at the same time. Maxiters=1500

--- utilities/PBAU/hwyAssignLogsumInvestigation.bat
+++ utilities/PBAU/hwyAssignLogsumInvestigation.bat
@@ -3,9 +3,8 @@ set INPUT_DIR=M:\Projects\2040_05_444
 set GIT_DIR=C:\Users\mtcpb\Documents\GitHub\travel-model-one-v05
 
 :: --------------------------------------------------------------------------
-:: relative gap = 0.005,   Normal Frank-Wolfe
+:: relative gap = 0.005
 set RELGAP=0.005
-set ENHANCE=0
 
 mkdir test_relgap%RELGAP%_enh%ENHANCE%
 cd test_relgap%RELGAP%_enh%ENHANCE%
@@ -15,9 +14,8 @@ IF ERRORLEVEL 2 goto done
 cd ..
 
 :: --------------------------------------------------------------------------
-:: relative gap = 0.0005,  Normal Frank-Wolfe
+:: relative gap = 0.0005
 set RELGAP=0.0005
-set ENHANCE=0
 
 mkdir test_relgap%RELGAP%_enh%ENHANCE%
 cd test_relgap%RELGAP%_enh%ENHANCE%
@@ -27,9 +25,8 @@ IF ERRORLEVEL 2 goto done
 cd ..
 
 :: --------------------------------------------------------------------------
-:: relative gap = 0.00005, Normal Frank-Wolfe
+:: relative gap = 0.00005
 set RELGAP=0.00005
-set ENHANCE=0
 
 mkdir test_relgap%RELGAP%_enh%ENHANCE%
 cd test_relgap%RELGAP%_enh%ENHANCE%
@@ -38,39 +35,5 @@ IF ERRORLEVEL 2 goto done
 
 cd ..
 
-:: --------------------------------------------------------------------------
-:: relative gap = 0.005,   Bi-conjugate Frank-Wolfe
-set RELGAP=0.005
-set ENHANCE=2
 
-mkdir test_relgap%RELGAP%_enh%ENHANCE%
-cd test_relgap%RELGAP%_enh%ENHANCE%
-runtpp %GIT_DIR%\utilities\PBAU\hwyAssignLogsumInvestigation.job
-IF ERRORLEVEL 2 goto done
-
-cd ..
-
-:: --------------------------------------------------------------------------
-:: relative gap = 0.0005,  Bi-conjugate Frank-Wolfe
-set RELGAP=0.0005
-set ENHANCE=2
-
-mkdir test_relgap%RELGAP%_enh%ENHANCE%
-cd test_relgap%RELGAP%_enh%ENHANCE%
-runtpp %GIT_DIR%\utilities\PBAU\hwyAssignLogsumInvestigation.job
-IF ERRORLEVEL 2 goto done
-
-cd ..
-
-:: --------------------------------------------------------------------------
-:: relative gap = 0.00005, Bi-conjugate Frank-Wolfe
-set RELGAP=0.00005
-set ENHANCE=2
-
-mkdir test_relgap%RELGAP%_enh%ENHANCE%
-cd test_relgap%RELGAP%_enh%ENHANCE%
-runtpp %GIT_DIR%\utilities\PBAU\hwyAssignLogsumInvestigation.job
-IF ERRORLEVEL 2 goto done
-
-cd ..
 :done
