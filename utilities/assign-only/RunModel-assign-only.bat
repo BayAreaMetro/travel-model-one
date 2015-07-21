9072fb83cc53574884994c2b40b421c9d020acad
DavidOry <dory@mtc.ca.gov>
2015-07-21 15:56:59 -0700

    assign-only utility batch

---
+++ utilities/assign-only/RunModel-assign-only.bat
@@ -0,0 +1,112 @@
+::~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+:: RunModel.bat
+::
+:: MS-DOS batch file to execute the MTC travel model.  Each of the model steps are sequentially
+:: called here.  
+::
+:: For complete details, please see http://mtcgis.mtc.ca.gov/foswiki/Main/RunModelBatch.
+::
+:: dto (2012 02 15) gde (2009 04 22)
+::
+::~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+:: ------------------------------------------------------------------------------------------------------
+::
+:: Step 1:  Set the necessary path variables
+::
+:: ------------------------------------------------------------------------------------------------------
+
+:: The location of the RUNTPP executable from Citilabs
+set TPP_PATH=C:\Program Files (x86)\Citilabs\CubeVoyager
+
+:: Add these variables to the PATH environment variable, moving the current path to the back
+set OLD_PATH=%PATH%
+set PATH=%TPP_PATH%;%OLD_PATH%
+
+:: ------------------------------------------------------------------------------------------------------
+::
+:: Step 2:  Create the directory structure
+::
+:: ------------------------------------------------------------------------------------------------------
+
+:: Create the working directories
+mkdir hwy
+mkdir trn
+mkdir skims
+mkdir landuse
+mkdir popsyn
+mkdir nonres
+mkdir main
+mkdir logs
+mkdir database
+
+:: Stamp the feedback report with the date and time of the model start
+echo STARTED MODEL RUN  %DATE% %TIME% >> logs\feedback.rpt 
+
+:: Move the input files, which are not accessed by the model, to the working directories
+copy INPUT\hwy\ hwy\
+copy INPUT\warmstart\main\ main\
+copy INPUT\warmstart\nonres\ nonres\
+
+
+:: ------------------------------------------------------------------------------------------------------
+::
+:: Step 3:  Pre-process steps
+::
+:: ------------------------------------------------------------------------------------------------------
+
+:: Set the prices in the roadway network
+runtpp CTRAMP\scripts\preprocess\SetTolls.job
+if ERRORLEVEL 2 goto done
+
+:: Set a penalty to dummy links connecting HOV/HOT lanes and general purpose lanes
+runtpp CTRAMP\scripts\preprocess\SetHovXferPenalties.job
+if ERRORLEVEL 2 goto done
+
+:: Create time-of-day-specific 
+runtpp CTRAMP\scripts\preprocess\CreateFiveHighwayNetworks.job
+if ERRORLEVEL 2 goto done
+
+:: ------------------------------------------------------------------------------------------------------
+::
+:: Step 4:  Prepare for Iteration 0
+::
+:: ------------------------------------------------------------------------------------------------------
+
+: iter0
+
+:: Set the iteration parameters
+set ITER=0
+set PREV_ITER=0
+set WGT=1.0
+set PREV_WGT=0.00
+
+
+:: ------------------------------------------------------------------------------------------------------
+::
+:: Step 6:  Execute the RunIteration batch file
+::
+:: ------------------------------------------------------------------------------------------------------
+
+call CTRAMP\RunIteration.bat
+if ERRORLEVEL 2 goto done
+
+
+:: Put the PATH back the way you found it
+set PATH=%OLD_PATH%
+
+:: Move all the TP+ printouts to the \logs folder
+copy *.prn logs\*.prn
+
+:: Delete all the temporary TP+ printouts and cluster files
+del *.prn
+del *.script.*
+del *.script
+
+:: Success target and message
+:success
+ECHO FINISHED SUCCESSFULLY!
+
+:: Complete target and message
+:done
+ECHO FINISHED.  
