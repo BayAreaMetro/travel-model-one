42a1daa7e977cd464e5812a0e8be4d88d4e1abd0
DavidOry <dory@mtc.ca.gov>
2015-12-17 13:32:15 -0800

    added list of R packages for easier install

---
+++ utilities/r-install/install-r-packages.R
@@ -0,0 +1,9 @@
+# R packages needed for Travel Model One and utilities
+install.packages("knitr")
+install.packages("dplyr")
+install.packages("RODBC")
+install.packages("shiny")
+install.packages("ggplot2")
+install.packages("reshape2")
+install.packages("scales")
+
