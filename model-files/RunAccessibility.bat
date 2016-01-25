aa85dd86c368431c69e265117f47925777a60243
DavidOry <dory@mtc.ca.gov>
2016-01-25 11:04:52 -0800

    point accessibility to new jar
    the new jar caused problems with CT-RAMP.  So keep the old jar for
    CT-RAMP and use the new one for accessibility.

--- model-files/RunAccessibility.bat
+++ model-files/RunAccessibility.bat
@@ -14,27 +14,11 @@
 ::
 :: ------------------------------------------------------------------------------------------------------
 
-:: The location of the 64-bit java development kit
-set JAVA_PATH=C:\Program Files\Java\jdk1.7.0_71
-:: This is required by MtcAccessibilityLogsums below, which will try to start up a 32-bit matrix manager process
-:: However, if the matrix manager fails to start, it'll just read the matrices directly which is fine.
-set JAVA_PATH_32=C:\this_does_not_exit
-
-:: The location of the GAWK binary executable files
-set GAWK_PATH=M:\UTIL\Gawk
-
-:: The location of the RUNTPP executable from Citilabs
-set TPP_PATH=C:\Program Files (x86)\Citilabs\CubeVoyager;C:\Program Files\Citilabs\VoyagerFileAPI
-
-:: The location of the MTC.JAR file
-set RUNTIME=CTRAMP/runtime
-
-:: Add these variables to the PATH environment variable, moving the current path to the back
-set OLD_PATH=%PATH%
-set PATH=%RUNTIME%;%JAVA_PATH%/bin;%TPP_PATH%;%GAWK_PATH%/bin;%OLD_PATH%
+:: Set the path
+call CTRAMP\runtime\SetPath.bat
 
 ::  Set the Java classpath (locations where Java needs to find configuration and JAR files)
-set CLASSPATH=%RUNTIME%/config;%RUNTIME%;%RUNTIME%/config/jppf-2.4/jppf-2.4-admin-ui/lib/*;%RUNTIME%/mtc.jar
+set CLASSPATH=%RUNTIME%/config;%RUNTIME%;%RUNTIME%/config/jppf-2.4/jppf-2.4-admin-ui/lib/*;%RUNTIME%/accessibility.jar
 
 ::  Set the IP address of the host machine which sends tasks to the client machines 
 set HOST_IP_ADDRESS=192.168.1.200
--- model-files/runtime/SetPath.bat
+++ model-files/runtime/SetPath.bat
@@ -7,6 +7,9 @@ set JAVA_PATH=C:\Program Files\Java\jdk1.7.0_71
 :: The location of the GAWK binary executable files
 set GAWK_PATH=M:\UTIL\Gawk
 
+:: The location of R
+set R_HOME=C:\Program Files\R\R-3.2.3
+
 :: The location of the RUNTPP executable from Citilabs
 set TPP_PATH=C:\Program Files\Citilabs\CubeVoyager;C:\Program Files\Citilabs\VoyagerFileAPI;C:\Program Files (x86)\Citilabs\CubeVoyager
 
@@ -17,8 +20,7 @@ set PYTHON_PATH=C:\Python27
 set RUNTIME=CTRAMP/runtime
 
 :: Add these variables to the PATH environment variable, moving the current path to the back
-set OLD_PATH=%PATH%
-set PATH=%RUNTIME%;%JAVA_PATH%/bin;%TPP_PATH%;%GAWK_PATH%/bin;%PYTHON_PATH%;%OLD_PATH%
+set PATH=%RUNTIME%;%JAVA_PATH%/bin;%TPP_PATH%;%GAWK_PATH%/bin;%PYTHON_PATH%
 
 ::  Set the Java classpath (locations where Java needs to find configuration and JAR files)
 set CLASSPATH=%RUNTIME%/config;%RUNTIME%;%RUNTIME%/config/jppf-2.4/jppf-2.4-admin-ui/lib/*;%RUNTIME%/mtc.jar
