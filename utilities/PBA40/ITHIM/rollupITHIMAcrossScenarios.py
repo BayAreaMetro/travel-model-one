c9c3a1dde5eb27507e0a4e527d5192e178b73488
Lisa Zorn <lzorn@mtc.ca.gov>
2016-04-21 14:03:57 -0700

    ITHIM rollup: read incQ files
    and make input/output files args

--- utilities/PBA40/ITHIM/rollupITHIMAcrossScenarios.py
+++ utilities/PBA40/ITHIM/rollupITHIMAcrossScenarios.py
@@ -1,19 +1,28 @@
 USAGE = """
 
-  python rollupITHIMAcrossScenarios.py
+  python rollupITHIMAcrossScenarios.py ScenarioKey.csv scenario_ITHIM_results.csv
 
-  Reads ScenarioKey.csv and then the ITHIM files for the corresponding scenarios.
+  Reads ScenarioKey.csv and then the ITHIM files for the corresponding scenarios,
+  including incQ1 and incQ4 results.
 
   Outputs scenario_ITHIM_results.csv
 
 """
 
-import csv, os
+import csv, os, sys
 import pandas
 
 if __name__ == '__main__':
 
-    csvfile = open('ScenarioKey.csv', 'rb')
+    if len(sys.argv) != 3:
+        print USAGE
+        sys.exit()
+
+    scenario_file = sys.argv[1]
+    output_file   = sys.argv[2]
+
+    print "Reading scenario key file: [%s]" % scenario_file
+    csvfile = open(scenario_file, 'rb')
     csvreader = csv.DictReader(csvfile, delimiter=",")
 
     all_results_df  = None
@@ -29,25 +38,29 @@ if __name__ == '__main__':
             print "Scenario [%s] with src [%s] has modelversion [%s] != 05.  Skipping because ITHIM results don't exist." % (scenario, src, modelversion)
             continue
 
-        ithim_file = os.path.join(src,"OUTPUT","metrics","ITHIM","results.csv")
-        results_df = pandas.read_csv(ithim_file)
-        print "Read [%s] for [%s]" % (ithim_file, scenario)
+        for incQ in [-1,1,4]:
+            ithim_file = os.path.join(src,"OUTPUT","metrics","ITHIM","results%s.csv" % ("" if incQ<0 else "_inc%d" % incQ))
+            results_df = pandas.read_csv(ithim_file)
+            print "Read [%s] for [%s]" % (ithim_file, scenario)
 
-        # set "report_year" to scenario
-        results_df["report_year"] = scenario
+            # set "report_year" to scenario
+            if incQ > 0:
+                results_df["report_year"] = scenario + (", incQ%d" % incQ)
+                results_df["scenario_id"] = results_df["scenario_id"] + ("_incQ%d" % incQ)
+            else:
+                results_df["report_year"] = scenario
 
-        if all_results_set:
-            all_results_df  = pandas.concat([all_results_df, results_df], axis=0)
-        else:
-            all_results_df  = results_df
-            all_results_set = True
+            if all_results_set:
+                all_results_df  = pandas.concat([all_results_df, results_df], axis=0)
+            else:
+                all_results_df  = results_df
+                all_results_set = True
 
     # bug fix
     all_results_df["scenid_itemid_mode_strata_age_sex"] = all_results_df["scenid_itemid_mode_strata_age_sex"].str.replace("freeway", "highway")
 
     # Save it
-    outfile = "scenario_ITHIM_results.csv"
-    all_results_df.to_csv(outfile, index=False)
-    print "Wrote %s" % outfile
+    all_results_df.to_csv(output_file, index=False)
+    print "Wrote %s" % output_file
 
 
