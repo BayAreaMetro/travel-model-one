d0d3a61e352087bb20c1e431c19464c591a02e66
Lisa Zorn <lzorn@mtc.ca.gov>
2017-02-17 16:00:04 -0800

    New: off model summary for bikeshare

--- utilities/PBA40/emissions/Off Model Calculators/Bikeshare.R
+++ utilities/PBA40/emissions/Off Model Calculators/Bikeshare.R
@@ -1,12 +1,12 @@
 #
-# This R script distills the model outputs into the versions used by ICF calculator: "Carshare v4.xlsx"
+# This R script distills the model outputs into the versions used by ICF calculator: "Bikeshare.xlsx"
 #
 
 library(dplyr)
 library(reshape2)
 
 MODEL_DATA_BASE_DIR <-"M:/Application/Model One/RTP2017/Scenarios"
-OUTPUT_FILE         <-"C:/Users/lzorn/Box Sync/ICF Calculators/Model Data/Model Data - Carshare.csv"
+OUTPUT_FILE         <-"C:/Users/lzorn/Box Sync/ICF Calculators/Model Data/Model Data - Bikeshare.csv"
 
 # this is the currently running script
 SCRIPT                <- (function() {attr(body(sys.function()), "srcfile")})()$filename
@@ -14,24 +14,14 @@ SCRIPT                <- normalizePath(SCRIPT)
 # the model runs are in the parent folder
 model_runs            <- read.table(file.path(dirname(SCRIPT),"..","ModelRuns.csv"), header=TRUE, sep=",", stringsAsFactors = FALSE)
 
-# Calculator constants
-# Criteria for applying trip caps
-K_MIN_POP_DENSITY  <- 10   # Minimum density needed to be considered "urban" and support dense carshare (persons/residential acre)
-
 # want:
 # Total population
-# Total population in TAZs with density >10
-# Total population in TAZs with density <10
-# Adult pop (age 20-64) in TAZs with density >10
-# Adult pop (age 20-64) in TAZs with density <10
+# Total employment
 
 # Read tazdata
-TAZDATA_FIELDS <- c("ZONE", "SD", "COUNTY","TOTPOP","RESACRE","AGE2044","AGE4564") # only care about these fields
+TAZDATA_FIELDS <- c("ZONE", "SD", "COUNTY","TOTPOP","TOTEMP") # only care about these fields
 tazdata_df     <- data.frame()
 for (i in 1:nrow(model_runs)) {
-  # We don't need past years for Car Share
-  if (model_runs[i,"category"]=="Past year") next
-  
   tazdata_file    <- file.path(MODEL_DATA_BASE_DIR, model_runs[i,"directory"],"OUTPUT","tazData.csv")
   tazdata_file_df <- read.table(tazdata_file, header=TRUE, sep=",")
   tazdata_file_df <- tazdata_file_df[, TAZDATA_FIELDS] %>%
@@ -42,21 +32,11 @@ for (i in 1:nrow(model_runs)) {
 }
 remove(i, tazdata_file, tazdata_file_df)
 
-# population per residential acre
-tazdata_df <- mutate(tazdata_df,
-                     totpop_per_resacre = ifelse(RESACRE==0,0,TOTPOP/RESACRE),
-                     carshare_dense     = (totpop_per_resacre > K_MIN_POP_DENSITY),
-                     totpop_dense       = TOTPOP*carshare_dense,
-                     totpop_sparse      = TOTPOP*(!carshare_dense),
-                     adultpop_dense     = (AGE2044+AGE4564)*carshare_dense,
-                     adultpop_sparse    = (AGE2044+AGE4564)*(!carshare_dense))
 
 summary_df <- summarise(group_by(tazdata_df, year, category, directory),
                         total_population = sum(TOTPOP),
-                        totpop_dense     = sum(totpop_dense),
-                        totpop_sparse    = sum(totpop_sparse),
-                        adultpop_dense   = sum(adultpop_dense),
-                        adultpop_sparse = sum(adultpop_sparse))
+                        total_employment = sum(TOTEMP))
+
 # columns are: year, category, directory, variable, value
 summary_melted_df <- melt(summary_df, id.vars=c("year","category","directory"))
 
@@ -69,9 +49,6 @@ summary_melted_df <- summary_melted_df[order(summary_melted_df$index),
 # prepend note
 prepend_note <- paste0("Output by ",SCRIPT," on ",format(Sys.time(), "%a %b %d %H:%M:%S %Y"))
 write(prepend_note, file=OUTPUT_FILE, append=FALSE)
-
-prepend_note <- paste0("K_MIN_POP_DENSITY,"     ,K_MIN_POP_DENSITY)
-write(prepend_note, file=OUTPUT_FILE, append=TRUE)
 
 # output
 write.table(summary_melted_df, OUTPUT_FILE, sep=",", row.names=FALSE, append=TRUE)
