6b088e37539bf29c38b2a1ac85b64093f09f5929
Lisa Zorn <lzorn@bayareametro.gov>
2017-11-09 16:10:51 -0800

    Update build_transit_network

--- utilities/check-network/build_transit_network.bat
+++ utilities/check-network/build_transit_network.bat
@@ -4,13 +4,24 @@
 ::
 :: 2015 07 22 dto
 
-set NETWORK_CODING_DIR="M:\Application\Model One\RTP2017\Project Performance Assessment\Baseline Network"
-set SCRIPT_DIR="D:\files\GitHub\travel-model-one\model-files"
+:: Location of travel-model-one local repo (probably including this dir)
+set CODE_DIR=C:\Users\lzorn\Documents\travel-model-one-master
+:: Location of INPUT and CTRAMP directory.
+set MODEL_DIR=M:\Application\Model One\STIP2017\2040_06_lmz
+
+:: this is where we'll do this work
+set TRN_CHECK_DIR=%MODEL_DIR%\INPUT\trn_check
 
 :: Path details
 set TPP_PATH=C:\Program Files\Citilabs\CubeVoyager;C:\Program Files (x86)\Citilabs\CubeVoyager
 set GAWK_PATH=D:\files\Gawk\bin
 SET PATH=%TPP_PATH%;%GAWK_PATH%;%PATH%
+
+:: Step 0: create the trn check dir and go to it
+if not exist "%TRN_CHECK_DIR%" (
+  mkdir "%TRN_CHECK_DIR%"
+)
+cd "%TRN_CHECK_DIR%"
 
 :: Step 1: bring in the working transit network and highway network
 mkdir trn
@@ -18,37 +29,68 @@ mkdir hwy
 mkdir skims
 mkdir logs
 
-copy %NETWORK_CODING_DIR%\trn\transit_lines\   trn\
-copy %NETWORK_CODING_DIR%\trn\transit_fares\   trn\ 
-copy %NETWORK_CODING_DIR%\trn\transit_support\ trn\
+copy "%MODEL_DIR%\INPUT\trn\transit_lines\"   trn\
+copy "%MODEL_DIR%\INPUT\trn\transit_fares\"   trn\
+copy "%MODEL_DIR%\INPUT\trn\transit_support\" trn\
 
-copy %NETWORK_CODING_DIR%\2015U_June22.net hwy\freeflow.net
+copy "%MODEL_DIR%\INPUT\hwy\freeflow.net"     hwy\
 
 :: Step 2: build the transit network
 
-:: Set the prices in the roadway network
-runtpp %SCRIPT_DIR%\scripts\preprocess\SetTolls.job
+:: == Assumes this script is an input ==
+::   Input: hwy\freeflow.net
+::  Output: hwy\withTolls.net
+:: Summary: Sets the prices in the roadway network
+::          Based on columns TOLLCLASS, DISTANCE
+::          Updates columns: TOLL[EA,AM,MD,PM,EV]_[DA,S2,S3,VSM,SML,MED,LRG]
+runtpp "%MODEL_DIR%\CTRAMP\scripts\preprocess\SetTolls.job"
 if ERRORLEVEL 2 goto done
 
-:: Set a penalty to dummy links connecting HOV/HOT lanes and general purpose lanes
-runtpp %SCRIPT_DIR%\scripts\preprocess\SetHovXferPenalties.job
+::   Input: hwy\withTolls.net
+::  Output: hwy\withHovXferPenalties.net (copied back into withTolls.net)
+:: Summary: Set a penalty to dummy links connecting HOV/HOT lanes and general purpose lanes
+::          Based on columns FT, A, B, DISTANCE
+::          Updates column: HovXPen
+runtpp "%CODE_DIR%\model-files\scripts\preprocess\SetHovXferPenalties.job"
 if ERRORLEVEL 2 goto done
 
-:: Create time-of-day-specific 
-runtpp %SCRIPT_DIR%\scripts\preprocess\CreateFiveHighwayNetworks.job
+::   Input: hwy\withTolls.net
+::  Output: hwy\avgload[EA,AM,MD,PM,EV].net
+:: Summary: Creates time-of-day-specific networks
+runtpp "%CODE_DIR%\model-files\scripts\preprocess\CreateFiveHighwayNetworks.job"
 if ERRORLEVEL 2 goto done
 
-:: Prepare the highway network for use by the transit network
-runtpp %SCRIPT_DIR%\scripts\skims\PrepHwyNet.job
+::   Input: hwy\avgload[EA,AM,MD,PM,EV].net
+::          trn\transit_support_nodes.dat
+::          trn\[express_bus,light_rail,ferry,heavy_rail,commuter_rail]_neti_access_links.dat
+::          trn\[express_bus,light_rail,ferry,heavy_rail,commuter_rail]_neti_xfer_links.dat
+::  Output: trn\[EA,AM,MD,PM,EV]_transit_background.net
+::          trn\[EA,AM,MD,PM,EV]_temporary_transit_background_accesslinks.net
+::          trn\[EA,AM,MD,PM,EV]_temporary_transit_background_transferlinks.net
+:: Summary: Prepare the highway network for use by the transit network
+runtpp "%CODE_DIR%\model-files\scripts\skims\PrepHwyNet.job"
 if ERRORLEVEL 2 goto done
 
-:: Create the transit networks
-runtpp %SCRIPT_DIR%\scripts\skims\BuildTransitNetworks.job
+::   Input: trn\[EA,AM,MD,PM,EV]_temporary_transit_background_accesslinks.net
+::          trn\[EA,AM,MD,PM,EV]_temporary_transit_background_transferlinks.net
+::          trn\[light_rail,ferry,heavy_rail,commuter_rail].zac
+::          trn\walk_access.sup
+::          trn\transit_lines.block
+::  Output: trn\[EA,AM,MD,PM,EV]_walk_links.dbf
+::          trn\[EA,AM,MD,PM,EV]_walk_[acc,egr]links.dat
+::          trn\[EA,AM,MD,PM,EV]_drive_links.dbf
+::          trn\[EA,AM,MD,PM,EV]_drive_[acc,egr]links.dat
+::          trn\[EA,AM,MD,PM,EV]_transit_links.dbf
+::          trn\[EA,AM,MD,PM,EV]_transit_suplinks.dat
+::          trn\[EA,AM,MD,PM,EV]_transit_suplinks_[walk,express_bus,light_rail,ferry,heavy_rail,commuter_rail].dat
+::          trn\[EA,AM,MD,PM,EV]_bus_acclinks_KNR.dat
+:: Summary: Create the transit networks
+runtpp "%CODE_DIR%\model-files\scripts\skims\BuildTransitNetworks.job"
 if ERRORLEVEL 2 goto done
 
-:: Create the public transport level-of-service matrices (run again if Cube fails)
+:: Summary: Create the public transport level-of-service matrices (run again if Cube fails)
 start Cluster M:\COMMPATH\CTRAMP 1-16 Start
-runtpp %SCRIPT_DIR%\scripts\skims\TransitSkims.job
+runtpp "%CODE_DIR%\model-files\scripts\skims\TransitSkims.job"
 if ERRORLEVEL 2 goto done
 
 goto victory
