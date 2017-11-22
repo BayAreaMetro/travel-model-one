e2b33eb87ef2e9f6ed22984a9498c3c4ed181e8f
Lisa Zorn <lzorn@bayareametro.gov>
2017-11-22 14:58:19 -0800

    Put USE back into compareNetworks.job and
    Comment out environment variable setting -- use the command line.

--- utilities/check-network/build_transit_network.bat
+++ utilities/check-network/build_transit_network.bat
@@ -7,10 +7,12 @@
 
 :: Location of travel-model-one local repo (probably including this dir)
 set CODE_DIR=C:\Users\lzorn\Documents\travel-model-one-master
+
 :: Location of INPUT and CTRAMP directory.
-set MODEL_DIR=M:\Application\Model One\STIP2017\2040_06_700_CC050028_680SbHov
+:: set MODEL_DIR=M:\Application\Model One\STIP2017\2040_06_700_CC130046_680SR4Int
+
 :: Location of BASE MODEL_DIR
-set MODEL_BASE_DIR=M:\Application\Model One\STIP2017\2040_06_700
+:: set MODEL_BASE_DIR=M:\Application\Model One\STIP2017\2040_06_700
 
 :: this is where we'll do this work
 set TRN_CHECK_DIR=%MODEL_DIR%\INPUT\trn_check
