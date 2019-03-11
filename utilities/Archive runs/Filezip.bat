364ad9235bf6fe1340b6c0acbd4ae3d713803792
bespinmtc <bespin@mtc.ca.gov>
2019-03-11 13:35:34 -0700

    Rename zipped folder with ".archived" extension

--- utilities/Archive runs/Filezip.bat
+++ utilities/Archive runs/Filezip.bat
@@ -1,8 +1,10 @@
-REM Use 7Zip to Zip files in folder "U:\Archived Travel Model Runs" and place the zip archive 
-REM in C:\DATA
-
+REM Use 7Zip to Zip travel model runs (for example in folder "Z:\Projects\2015_06_002_inc075") and place the zip archive 
+REM in "U:\Archived Travel Model Runs". Then renames folder to X.archived
 ECHO
-ECHO Zipping all files in C:\Users\bespin\Documents\2035_06_690
+ECHO Zipping all files in Z:\Projects\2015_06_002_inc125
 ECHO
-"C:\Program Files\7-Zip\7z.exe" a -tzip "U:\Archived Travel Model Runs\2030_06_701.7z" "Y:\Projects\2030_06_701" -mx5
+"C:\Program Files\7-Zip\7z.exe" a -t7z "U:\Archived Travel Model Runs\2015_06_002_inc125.7z" "Z:\Projects\2015_06_002_inc125" -mx9
 ECHO
+Z:
+cd Projects\
+ren 2015_06_002_inc125 2015_06_002_inc125.archived
