7bede7a09d834cd7295143aa42bd067ff36ce2fe
Lisa Zorn <lzorn@bayareametro.gov>
2019-03-11 11:33:23 -0700

    Calibration snapshot
    Including CBD transit constant update and increased 2nd and 3rd perceived board penalties

--- utilities/calibration/copy_output.bat
+++ utilities/calibration/copy_output.bat
@@ -1,7 +1,7 @@
 SETLOCAL EnableDelayedExpansion
 
-set MODEL_DIR=A:\TM150_Calibration\2015_TM150_calib10
-set TARGET_DIR=M:\Development\Travel Model One\Calibration\Version 1.5.0\2015_TM150_calib10
+set MODEL_DIR=A:\TM150_Calibration\2015_TM150_calib11
+set TARGET_DIR=M:\Development\Travel Model One\Calibration\Version 1.5.0\2015_TM150_calib11
 set CODE_DIR=X:\travel-model-one-calibration\utilities\calibration
 set ITER=1
 set SAMPLESHARE=0.2
