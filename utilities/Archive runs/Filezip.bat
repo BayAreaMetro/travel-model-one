8498e8fcc01bd3035310be3e2b2ec926a77f2f51
bespinmtc <bespin@mtc.ca.gov>
2019-03-12 13:48:13 -0700

    Update to include only folders and files necessary to archive

--- utilities/Archive runs/Filezip.bat
+++ utilities/Archive runs/Filezip.bat
@@ -1,9 +1,9 @@
 REM Use 7Zip to Zip travel model runs (for example in folder "Z:\Projects\2015_06_002_inc075") and place the zip archive 
-REM in "U:\Archived Travel Model Runs". Then renames folder to X.archived
+REM in "U:\Archived Travel Model Runs". Then renames folder to XXX.archived
 ECHO
 ECHO Zipping all files in Z:\Projects\2015_06_002_inc125
 ECHO
-"C:\Program Files\7-Zip\7z.exe" a -t7z "U:\Archived Travel Model Runs\2015_06_002_inc125.7z" "Z:\Projects\2015_06_002_inc125" -mx9
+"C:\Program Files\7-Zip\7z.exe" a "U:\Archived Travel Model Runs\2015_06_002_inc125.7z" "Z:\Projects\2015_06_002_inc125\CTRAMP" "Z:\Projects\2015_06_002_inc125\extractor" "Z:\Projects\2015_06_002_inc125\INPUT" "Z:\Projects\2015_06_002_inc125\*.bat" -mx9
 ECHO
 Z:
 cd Projects\
