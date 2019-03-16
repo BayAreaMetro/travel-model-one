346c123a19e0b5da8862d16bb1021e3ab07a70e2
Lisa Zorn <lzorn@bayareametro.gov>
2019-03-15 23:37:02 -0700

    Remove Mobility.AV.Share lines from ModeChoice.xls
    And update RuntimeConfiguration.py since the taxi/TNC constants are configured directly via the Adjust_(Taxi|TNCsingle|Adjust_TNCshared) parameters

--- model-files/scripts/preprocess/RuntimeConfiguration.py
+++ model-files/scripts/preprocess/RuntimeConfiguration.py
@@ -683,17 +683,22 @@ def config_uec(auto_operating_cost, adjTaxi, adjTNCsingle, adjTNCshared):
                 # print rs.cell(rownum,1)
                 trip_half = 0.5 if bookname == "TripModeChoice.xls" else 1.0
 
-                if rs.cell(rownum,2).value=='Exogenous adjustment - taxi':
+                # only join tours; individual are calibrated
+                if rs.cell(rownum,2).value in ['Taxi - Alternative-specific constant - Zero auto - Joint Tours',
+                                               'Taxi - Alternative-specific constant - Auto deficient - Joint Tours',
+                                               'Taxi - Alternative-specific constant - Auto sufficient - Joint Tours']:
                     print "  Sheet '%s': replacing AdjustTaxi '%s' -> %.2f" % \
                         (rs.name, rs.cell(rownum,4).value, adjTaxi*trip_half)
                     wb.get_sheet(sheet_num).write(rownum,4,adjTaxi*trip_half, xlwt.easyxf("align: horiz left"))
 
-                if rs.cell(rownum,2).value=='Exogenous adjustment - tnc single':
+                # both individual and joint tours, for different auto "sufficiency" levels
+                if rs.cell(rownum,2).value.startswith('TNC-Single - Alternative-specific constant'):
                     print "  Sheet '%s': replacing AdjustTNCsingle '%s' -> %.2f" % \
                         (rs.name, rs.cell(rownum,4).value, adjTNCsingle*trip_half)
                     wb.get_sheet(sheet_num).write(rownum,4,adjTNCsingle*trip_half, xlwt.easyxf("align: horiz left"))
 
-                if rs.cell(rownum,2).value=='Exogenous adjustment - tnc shared':
+                # both individual and joint tours, for different auto "sufficiency" levels
+                if rs.cell(rownum,2).value.startswith('TNC-Shared - Alternative-specific constant'):
                     print "  Sheet '%s': replacing AdjustTNCshared '%s' -> %.2f" % \
                         (rs.name, rs.cell(rownum,4).value, adjTNCshared*trip_half)
                     wb.get_sheet(sheet_num).write(rownum,4,adjTNCshared*trip_half, xlwt.easyxf("align: horiz left"))
