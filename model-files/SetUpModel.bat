1dddb5bc1a20a27062da84282c336fe5ac983982
Flavia <flavia.tsang@gmail.com>
2019-03-20 16:36:29 -0700

    added commands to add folder name to the cmd window

--- model-files/SetUpModel.bat
+++ model-files/SetUpModel.bat
@@ -6,7 +6,7 @@
 :: ------------------------------------------------------------------------------------------------------
 
 :: copy over CTRAMP
-set GITHUB_DIR=\\mainmodel\MainModelShare\travel-model-one-1.25.1
+set GITHUB_DIR=\\mainmodel\MainModelShare\travel-model-1.5.0
 mkdir CTRAMP\model
 mkdir CTRAMP\runtime
 mkdir CTRAMP\scripts
@@ -23,28 +23,38 @@ copy /Y "%GITHUB_DIR%\utilities\PBA40\RunScenarioMetrics.bat"              .
 copy /Y "%GITHUB_DIR%\utilities\PBA40\ExtractKeyFiles.bat"                 .
 
 :: copy over INPUTs from baseline
-set MODEL_SETUP_BASE_DIR=Z:\Application\Model One\RTP2021\Scenarios\2030_TM125_FU1_BF_02\INPUT
+set MODEL_SETUP_BASE_DIR=Z:\Application\Model One\RTP2021\ProjectPerformanceAssessment\Projects\2050_TM150_PPA_BF_00\INPUT
+c:\windows\system32\Robocopy.exe /E "%MODEL_SETUP_BASE_DIR%\landuse"                       INPUT\landuse
 c:\windows\system32\Robocopy.exe /E "%MODEL_SETUP_BASE_DIR%\logsums"                       INPUT\logsums
 c:\windows\system32\Robocopy.exe /E "%MODEL_SETUP_BASE_DIR%\metrics"                       INPUT\metrics
 c:\windows\system32\Robocopy.exe /E "%MODEL_SETUP_BASE_DIR%\nonres"                        INPUT\nonres
+c:\windows\system32\Robocopy.exe /E "%MODEL_SETUP_BASE_DIR%\popsyn"                        INPUT\popsyn
 c:\windows\system32\Robocopy.exe /E "%MODEL_SETUP_BASE_DIR%\warmstart"                     INPUT\warmstart
 copy /Y "%MODEL_SETUP_BASE_DIR%\params.properties"                                         INPUT\params.properties
 
 :: copy over project specific INPUTs 
-set MODEL_SETUP_NONBASE_DIR=Z:\Application\Model One\RTP2021\ProjectPerformanceAssessment\Projects\baseline_BF_00\network_2030
+set MODEL_SETUP_NONBASE_DIR=Z:\Application\Model One\RTP2021\ProjectPerformanceAssessment\Projects\1_Crossings5\2050_TM150_PPA_BF_00_1_Crossings5
 c:\windows\system32\Robocopy.exe /E "%MODEL_SETUP_NONBASE_DIR%\hwy"                        INPUT\hwy
 c:\windows\system32\Robocopy.exe /E "%MODEL_SETUP_NONBASE_DIR%\trn"                        INPUT\trn
 
-:: copy over land use and pop syn INPUTs 
-set MODEL_SETUP_NONBASE_DIR2=Z:\Application\Model One\RTP2021\ProjectPerformanceAssessment\TestProjects\2030_TM125_PPA_BF_00
-c:\windows\system32\Robocopy.exe /E "%MODEL_SETUP_NONBASE_DIR2%\landuse"                   INPUT\landuse
-c:\windows\system32\Robocopy.exe /E "%MODEL_SETUP_BASE_DIR%\popsyn"                        INPUT\popsyn
-
 :: set the location of the output folder; this is where the extractor directory will be copied to
-set M_DIR=%MODEL_SETUP_NONBASE_DIR2%
+set M_DIR=%MODEL_SETUP_NONBASE_DIR%
 
 :: copy this batch file itself to M
-copy SetUpModel.bat %M_DIR%\SetUpModel.bat
+copy SetUpModel.bat "%M_DIR%\SetUpModel.bat"
+
+
+
+::-----------------------------------------------------------------------
+:: add folder name to the command prompt window 
+::-----------------------------------------------------------------------
+set MODEL_DIR=%CD%
+set PROJECT_DIR=%~p0
+set PROJECT_DIR2=%PROJECT_DIR:~0,-1%
+:: get the base dir only
+for %%f in (%PROJECT_DIR2%) do set myfolder=%%~nxf
+
+title %myfolder%
 
 
 ::-----------------------------------------------------------------------
