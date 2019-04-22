e3cfe3c0dff46d170aaa11f372a30e6c7d77d960
anuptapase <anuptapase.mtc@gmail.com>
2019-04-22 11:23:44 -0700

    Updates

--- utilities/cube-to-shapefile/cube_to_shapefile.bat
+++ utilities/cube-to-shapefile/cube_to_shapefile.bat
@@ -1,6 +1,8 @@
 :: run this from [M_DIR]\OUTPUT\shapefile
 :: e.g. M:\Application\Model One\RTP2021\ProjectPerformanceAssessment\Projects\1_Crossings3\2050_TM151_PPA_RT_01_1_Crossings3_00\OUTPUT\shapefile
 
+
+
 :: first set to use arcgis version of python so we can use arcpy
 set PATH=C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3;C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\Scripts
 
