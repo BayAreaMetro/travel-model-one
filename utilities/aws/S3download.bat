677d7d23c15e5bed8725cac9ec4d0a663883575f
Flavia <flavia.tsang@gmail.com>
2019-06-21 10:48:18 -0700

    added commands to copy and paste the extractor folder

--- utilities/aws/S3download.bat
+++ utilities/aws/S3download.bat
@@ -8,18 +8,28 @@
 :: run from L:\RTP2021_PPA\Projects_onAWS
 for /d %%f in (
 
-2050_TM151_PPA_RT_05_2101_Geary_BRT_Phase2_00
-2050_TM151_PPA_RT_05_2102_ElCaminoReal_BRT_00
-2050_TM151_PPA_RT_05_2205_BARTtoSV_Phase2_00
-2050_TM151_PPA_RT_05_2402_SJC_People_Mover_00
-2050_TM151_PPA_RT_05_2403_Vasona_LRT_Phase2_00
+2050_TM151_PPA_RT_06_2300_CaltrainDTX_00
 
 )    do    (
 
 rem if not confident, do a dry run first
 rem aws s3 sync --dryrun s3://travel-model-runs/%%modelrun %%modelrun
 rem execute
+rem -------------------------------------------
 aws s3 sync s3://travel-model-runs/%%f %%f
 
-)
+rem copy extract key files
+rem -------------------------------------------
+set run_id=%%f
+echo %run_id%
 
+rem somehow timeout is needed to make sure the variable run_id is registered
+timeout 5
+
+set proj_id=%run_id:~21,-3%
+echo %proj_id%
+
+mkdir "..\Projects\%proj_id%\%run_id%\OUTPUT"
+c:\windows\system32\Robocopy.exe /E %%f\extractor ..\Projects\%proj_id%\%run_id%\OUTPUT
+
+)
