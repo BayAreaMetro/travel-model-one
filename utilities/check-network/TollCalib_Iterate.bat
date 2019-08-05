0f52cc94db5300674436b8917eec18796dac5ccd
Flavia <flavia.tsang@gmail.com>
2019-08-05 15:17:45 -0700

    Run iteration 4

--- utilities/check-network/TollCalib_Iterate.bat
+++ utilities/check-network/TollCalib_Iterate.bat
@@ -9,13 +9,13 @@
 :: -------------------------------------------------
 
 :: set iteration number, starting from 4 as we assume this is a continuation of a "normal" model run
-rem set ITER=4
+set ITER=4
 
-:: Location of the base run directory - the full run is needed because it has the CTRAMP directory
-rem set MODEL_BASE_DIR=L:\RTP2021_PPA\Projects_onAWS\2050_TM151_PPA_BF_07
+:: Location of the base run directory - the full run is needed because it needs the CTRAMP directory
+set MODEL_BASE_DIR=L:\RTP2021_PPA\Projects_onAWS\2050_TM151_PPA_RT_09
 
 :: Name and location of the tolls.csv to be used
-rem set TOLL_FILE=L:\RTP2021_PPA\Projects\2050_TM151_PPA_BF_07\INPUT\hwy\tolls_iter4.csv
+set TOLL_FILE=L:\RTP2021_PPA\Projects\2050_TM151_PPA_RT_09\tolls_iter4.csv
 
 :: -------------------------------------------------
 :: User input for all iterations
@@ -28,7 +28,7 @@ set MODEL_YEAR=2050
 
 
 :: Unloaded network dbf, generated from cube_to_shapefile.py, needed for the R script that determine toll adjustment 
-set UNLOADED_NETWORK_DBF=L:\RTP2021_PPA\Projects\2050_TM151_PPA_baselines_before07\2050_TM151_PPA_BF_06\INPUT\shapefiles\network_links.dbf
+set UNLOADED_NETWORK_DBF=L:\RTP2021_PPA\Projects\2050_TM151_PPA_RT_09\OUTPUT\shapefiles\network_links.dbf
 
 :: The file containing the bridge tolls (i.e. the first half of toll.csv), also needed for the R script that determine toll adjustment
 SET BRIDGE_TOLLS_CSV=M:\Application\Model One\NetworkProjects\Bridge_Toll_Updates\tolls_2050.csv
@@ -37,8 +37,17 @@ SET BRIDGE_TOLLS_CSV=M:\Application\Model One\NetworkProjects\Bridge_Toll_Update
 set TOLL_DESIGNATIONS_XLSX= M:\Application\Model One\Networks\TOLLCLASS Designations.xlsx
 
 :: -------------------------------------------------
+:: Run iteration 4
+:: -------------------------------------------------
+
+call TollCalib_RunModel
+
+:: -------------------------------------------------
 :: For iteration 5+
 :: -------------------------------------------------
+set ITER=5
+call TollCalib_RunModel
+
 set ITER=6
 call TollCalib_RunModel
 
