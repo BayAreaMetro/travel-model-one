2acb2cabfdcae994acc95c011d4ebde8a8bbcf86
Flavia <flavia.tsang@gmail.com>
2019-12-02 15:04:38 -0800

    drop the cube_to_shapefile step; simply run export_network.job

--- utilities/check-network/TollCalib_go.bat
+++ utilities/check-network/TollCalib_go.bat
@@ -10,9 +10,6 @@ rem -------------------------------------
 rem need user inputs here
 rem -------------------------------------
 
-rem where is network_links.dbf?
-set UNLOADED_NETWORK_DBF=\\tsclient\L\RTP2021_PPA\Projects\3000_ExpLanes_Calib\2050_TM151_PPA_RT_14_3000_ExpLanes_Calib_00\OUTPUT\shapefiles\network_links.dbf
-
 rem where is TOLLCLASS Designations.xlsx?
 rem (this file indicates which facilities have mandatory s2 tolls)
 set TOLL_DESIGNATIONS_XLSX=\\tsclient\L\RTP2021_PPA\Projects\TOLLCLASS_Designations.xlsx
@@ -21,18 +18,17 @@ rem if this is being run on aws, what's the Private IP Address?
 if %computername%==WIN-A4SJP19GCV5     set HOST_IP_ADDRESS=10.0.0.70
 
 rem what is the location of the base run (i.e. pre toll calibration) directory - the full run is needed because it needs the CTRAMP directory
-set MODEL_BASE_DIR=D:\Projects\2050_TM151_PPA_CG_14_3000_ExpLanes_Calib_00
+set MODEL_BASE_DIR=E:\Model2C-Share\Projects\2050_TM151_PPA_BF_17_preTollCalib
 
 :: Where do you want the toll calibration outputs to be stored?
 :: (this shoudl be the location of the output folder "tollcalib_iter" on the L drive)
-set L_DIR=\\tsclient\L\RTP2021_PPA\Projects\3000_ExpLanes_Calib\2050_TM151_PPA_CG_14_3000_ExpLanes_TollCalib_00
+set L_DIR=L:\RTP2021_PPA\Projects\2050_TM151_PPA_BF_17_preTollCalib
 
 
 rem copy the two toll calibration inputs 
 rem -------------------------------------
 mkdir tollcalib_iter
 
-copy %UNLOADED_NETWORK_DBF%      tollcalib_iter\network_links.dbf
 copy %TOLL_DESIGNATIONS_XLSX%    tollcalib_iter\TOLLCLASS_Designations.xlsx
 
 
@@ -48,6 +44,20 @@ copy \\tsclient\X\travel-model-one-master\utilities\check-network\TollCalib_RunM
 copy \\tsclient\X\travel-model-one-master\utilities\check-network\TollCalib_CheckSpeeds.R TollCalib_CheckSpeeds.R
 
 copy \\tsclient\X\travel-model-one-master\utilities\check-network\TollCalib_stop.py TollCalib_stop.py
+
+rem generate network_links.dbf
+rem -------------------------------------
+copy \\tsclient\X\travel-model-one-master\utilities\cube-to-shapefile\export_network.job  tollcalib_iter\export_network.job
+
+:: The location of the RUNTPP executable from Citilabs
+set TPP_PATH=C:\Program Files\Citilabs\CubeVoyager;C:\Program Files\Citilabs\VoyagerFileAPI
+:: Add these variables to the PATH environment variable, moving the current path to the back
+set PATH=%path%;%TPP_PATH%
+
+set NET_INFILE=%MODEL_BASE_DIR%\INPUT\hwy\freeflow.net
+set NODE_OUTFILE=tollcalib_iter\network_nodes.shp
+set LINK_OUTFILE=tollcalib_iter\network_links.shp
+runtpp tollcalib_iter\export_network.job
 
 rem generate a tolls_iter4.csv
 rem -------------------------------------
