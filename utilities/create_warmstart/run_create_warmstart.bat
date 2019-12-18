0071efc034d23c1041dd967e1f096aa903b32dc7
Lisa Zorn <lzorn@bayareametro.gov>
2019-12-17 18:01:03 -0800

    Creating inputs for 2005

--- utilities/create_warmstart/run_create_warmstart.bat
+++ utilities/create_warmstart/run_create_warmstart.bat
@@ -1,16 +1,16 @@
 set PATH=%PATH%;C:\Program Files (x86)\Citilabs\CubeVoyager
 
 set timeperiod=EA
-runtpp M:\Projects\create_warmstart\CreateWarmStart.job
+runtpp X:\travel-model-one-master\utilities\create_warmstart\CreateWarmStart.job
 
 set timeperiod=AM
-runtpp M:\Projects\create_warmstart\CreateWarmStart.job
+runtpp X:\travel-model-one-master\utilities\create_warmstart\CreateWarmStart.job
 
 set timeperiod=MD
-runtpp M:\Projects\create_warmstart\CreateWarmStart.job
+runtpp X:\travel-model-one-master\utilities\create_warmstart\CreateWarmStart.job
 
 set timeperiod=PM
-runtpp M:\Projects\create_warmstart\CreateWarmStart.job
+runtpp X:\travel-model-one-master\utilities\create_warmstart\CreateWarmStart.job
 
 set timeperiod=EV
-runtpp M:\Projects\create_warmstart\CreateWarmStart.job
+runtpp X:\travel-model-one-master\utilities\create_warmstart\CreateWarmStart.job
