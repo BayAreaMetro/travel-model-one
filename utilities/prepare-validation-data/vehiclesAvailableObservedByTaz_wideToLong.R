111a3b2356d769d8fb2ccc860d31f30e0286639d
Lisa Zorn <lzorn@bayareametro.gov>
2020-02-07 11:49:41 -0800

    A couple scripts for summarizing observed vehicles available

---
+++ utilities/prepare-validation-data/vehiclesAvailableObservedByTaz_wideToLong.R
@@ -0,0 +1,28 @@
+# Convert vehiclesAvailableByTaz.csv to long form
+#
+# Run this in M:\Data\Census\ACS\ACS2013-2017\B08201 Household Size by Vehicles Available
+#
+# Relevant asana task: https://app.asana.com/0/0/1160229917303441/f
+# lmz 2020.02.07
+
+library(tidyr)
+library(dplyr)
+
+data_wide <- read.table("vehiclesAvailableByTaz.csv", header=TRUE, sep=",", stringsAsFactors = FALSE)
+
+# remove Total
+data_wide <- select(data_wide, -Total)
+
+# make it long and create integer column
+data_long <- gather(data_wide, 
+  key="key_name", value="value", c("No.vehicle","X1.vehicle","X2.vehicles","X3.vehicles","X4.or.more")) %>%
+  mutate(num_vehicles=case_when(key_name == "No.vehicle"  ~ 0,
+                                key_name == "X1.vehicle"  ~ 1,
+                                key_name == "X2.vehicles" ~ 2,
+                                key_name == "X3.vehicles" ~ 3,
+                                key_name == "X4.or.more"  ~ 4))
+
+# remove key_name column and add source column
+data_long <- select(data_long, -key_name) %>% mutate(source = "ACS 2013-2017")
+
+write.csv(data_long, "vehiclesAvailableByTaz_long.csv", row.names=FALSE, quote = TRUE)
