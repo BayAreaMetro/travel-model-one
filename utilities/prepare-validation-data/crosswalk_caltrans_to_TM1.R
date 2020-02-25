c36a5ce03c77a67d9786790e6f727e8b560f3d3d
Lisa Zorn <lzorn@bayareametro.gov>
2020-02-24 17:21:13 -0800

    Update crosswalk_caltrans_to_TM1.R for updated inputs

--- utilities/prepare-validation-data/crosswalk_caltrans_to_TM1.R
+++ utilities/prepare-validation-data/crosswalk_caltrans_to_TM1.R
@@ -14,16 +14,20 @@ library(tibble)
 library(dplyr)
 library(reshape)
 
-CROSSWALK_DIR  = "M:/Crosswalks/CaltransCountLocations_TM1network"
-CROSSWALK_FILE = "typical-weekday-counts-xy-TM1link.csv"
+CROSSWALK_DIR  <- "M:/Crosswalks/CaltransCountLocations_TM1network"
+TM_NETWORK_DIR <- "M:/Application/Model One/Networks/TM1_2015_Base_Network"
+TM_NETWORK     <- "TM1_numberedRoutes_wgs84"
 
-caltrans_locs <- readOGR(dsn=file.path(CROSSWALK_DIR, "shapefiles"), layer="caltrans_locations_nad1983UTMzone10N")
-route_links   <- readOGR(dsn=file.path(CROSSWALK_DIR, "shapefiles"), layer="avgload5period_nonzeroROUTENUM")
+# output files
+CROSSWALK_FILE <- "typical-weekday-counts-xy-TM1link.csv"
+
+caltrans_locs <- readOGR(dsn=file.path(CROSSWALK_DIR,  "shapefiles"), layer="caltrans_locations_nad1983UTMzone10N")
+route_links   <- readOGR(dsn=file.path(TM_NETWORK_DIR, "shapefile"),  layer=TM_NETWORK)
 
 caltrans_locs_df <- mutate(as.data.frame(caltrans_locs), 
                            route_dir = paste(routeNumbe, direction)) %>% rowid_to_column("caltrans_rowid")
 route_links_df   <- mutate(as.data.frame(route_links),
-                           route_dir = paste(ROUTENUM, DIR)) %>% rowid_to_column("link_rowid")
+                           route_dir = paste(ROUTENUM, ROUTEDIR)) %>% rowid_to_column("link_rowid")
 
 # recode some of these
 caltrans_locs_df$route_dir[caltrans_locs_df$route_dir=="84 N"] <- "84 W"
@@ -43,7 +47,7 @@ matching_routes <- left_join(matching_routes, distances_long)
 xwalk <- matching_routes %>% group_by(caltrans_rowid) %>% slice(which.min(distance)) %>% as.data.frame()
 
 # go back to original columns, plus A, B, distance
-xwalk <- select(xwalk, latitude, longitude, countyCode, postmileVa, routeNumbe, direction, postmileSu, ID, A, B, distance) %>%
+xwalk <- select(xwalk, latitude, longitude, countyCode, postmileVa, routeNumbe, direction, postmileSu, link_rowid, A, B, distance) %>%
   dplyr::rename(routeNumber=routeNumbe, postmileValue=postmileVa, postmileSuffix=postmileSu, dist_from_link=distance)
 
 # for joins
