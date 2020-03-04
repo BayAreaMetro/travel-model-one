a6bf4acebdbedff2cd5c560be7e35929a778c410
Flavia <flavia.tsang@gmail.com>
2020-03-04 15:46:45 -0800

    added Tableau GHG visualizer and emission rate files

--- utilities/RTP/Emissions/GHG_visualizer/interpolate_emission_rates.py
+++ utilities/RTP/Emissions/GHG_visualizer/interpolate_emission_rates.py
@@ -1,20 +1,19 @@
+# this scripte interpolates the CO2 emission rates from 5 mph bins to 1 mph bins
 
 import os
 import pandas as pd
 
 InputEmissionRates_df = pd.read_excel(os.path.join(os.getcwd(), "yr2035_passveh_CO2_rates.xlsx"), header=1, index_col=1,skiprows=2,nrows=10)
 # The emission rates are stored in InputEmissionRates_df.loc[county,speed]
+output_filename = os.path.join(os.getcwd(), "interpolated.csv")
 
 # max speed in miles per hour
 max_speed = 100
 
-
-# ---------------------------------------------------------------------------
-
-# the eq. should look like (r(alameda, high_mph) - r(alameda, low_mph)) / 5 * (mph - low_mph) + r(alameda, low_mph)
-
 # note that there is a space after the county name
 counties = ["Alameda ", "Contra Costa ", "Marin ", "Napa ", "San Francisco ", "San Mateo ", "Santa Clara ", "Solano ", "Sonoma "]
+
+# print out the emission rates to check
 # ref_speed = ["5 mph", "10 mph", "15 mph", "20 mph", "25 mph", "30 mph", "35 mph", "40 mph", "45 mph", "50 mph", "55 mph", "60 mph", "65 mph", "70 mph"]
 
 # for c in counties:
@@ -23,7 +22,7 @@ counties = ["Alameda ", "Contra Costa ", "Marin ", "Napa ", "San Francisco ", "S
 #            print(d)
 
 
-# create a new data frame
+# create a new data frame for the interpolated values
 Interpolated_df = pd.DataFrame(columns = counties)
 
 for c in counties:
@@ -57,5 +56,4 @@ Interpolated_df = Interpolated_df.rename(columns={"Alameda ": "Alameda", "Contra
 # reorder the columns
 Interpolated_df = Interpolated_df[["mph", "Alameda", "Contra Costa", "Marin", "Napa", "San Francisco", "San Mateo", "Santa Clara", "Solano", "Sonoma"]]
 
-output_filename = os.path.join(os.getcwd(), "interpolated.csv")
 Interpolated_df.to_csv(output_filename, header=True, index=False)
