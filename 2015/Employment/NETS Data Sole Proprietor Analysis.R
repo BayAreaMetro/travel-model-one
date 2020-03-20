b80dc1ac661ac7aab7f7971f35ffc5a7c3321a8f
Shimon Israel <sisrael@bayareametro.gov>
2020-03-19 19:30:11 -0700

    Create NETS Data Sole Proprietor Analysis.R

---
+++ 2015/Employment/NETS Data Sole Proprietor Analysis.R
@@ -0,0 +1,9 @@
+# NETS Data Sole Proprietor Analysis.R
+# Analyze PUMS data for Bay Area commuters by industry 2017 5-year PUMS data
+# Import Libraries
+
+suppressMessages(library(tidyverse))
+
+# Set working directory
+
+"M:\Data\NETS\2015\nets_2015_bayarea_v3.xlsx"
