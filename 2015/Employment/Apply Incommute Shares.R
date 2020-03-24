51297eb82cce6114238dee401c6a7db39a044d03
Shimon Israel <sisrael@bayareametro.gov>
2020-03-24 12:59:31 -0700

    Upload summary data file

--- 2015/Employment/Apply Incommute Shares.R
+++ 2015/Employment/Apply Incommute Shares.R
@@ -89,59 +89,64 @@ incommute_factors <- temp3 %>%
 
 
 # Join incommute adjustment factors to employment TAZ dataset, by composite superdistrict
-# Calculate new variable values by dataset and industrial sector, at the TAZ level
+# Calculate new variable values by dataset and industrial sector, at the TAZ level, using incommute factors
+# Add self-employment back into LODES_SELF variable
+# Calculate totals by industrial sector and dataset for no-incommute TAZ-level values (e.g., T_ESRI_NOIN)
 # Select out variables of interest
 
 employment_no_incommute <- left_join(employment,incommute_factors,by="Comp_SD") %>% mutate(
-  A_ESRI_NEW              =A_ESRI          *A_ESRI_FACTOR,       
-  A_LODES_NEW      	      =A_LODES         *A_LODES_FACTOR,
-  A_LODES_SELF_NEW        =A_LODES_NEW + A_ACS_Self_Emp,
-  A_NETS_NEW       	      =A_NETS          *A_NETS_FACTOR,       
-  A_NETS_NOSOLE_NEW	      =A_NETS_NOSOLE   *A_NETS_NOSOLE_FACTOR,
-  F_ESRI_NEW       	      =F_ESRI          *F_ESRI_FACTOR,       
-  F_LODES_NEW      	      =F_LODES         *F_LODES_FACTOR, 
-  F_LODES_SELF_NEW        =F_LODES_NEW + F_ACS_Self_Emp,
-  F_NETS_NEW       	      =F_NETS          *F_NETS_FACTOR,       
-  F_NETS_NOSOLE_NEW	      =F_NETS_NOSOLE   *F_NETS_NOSOLE_FACTOR,
-  H_ESRI_NEW       	      =H_ESRI          *H_ESRI_FACTOR,       
-  H_LODES_NEW      	      =H_LODES         *H_LODES_FACTOR,  
-  H_LODES_SELF_NEW        =H_LODES_NEW + H_ACS_Self_Emp,
-  H_NETS_NEW       	      =H_NETS          *H_NETS_FACTOR,       
-  H_NETS_NOSOLE_NEW	      =H_NETS_NOSOLE   *H_NETS_NOSOLE_FACTOR,
-  M_ESRI_NEW       	      =M_ESRI          *M_ESRI_FACTOR,       
-  M_LODES_NEW      	      =M_LODES         *M_LODES_FACTOR,  
-  M_LODES_SELF_NEW        =M_LODES_NEW + M_ACS_Self_Emp,
-  M_NETS_NEW       	      =M_NETS          *M_NETS_FACTOR,       
-  M_NETS_NOSOLE_NEW	      =M_NETS_NOSOLE   *M_NETS_NOSOLE_FACTOR,
-  O_ESRI_NEW       	      =O_ESRI          *O_ESRI_FACTOR,       
-  O_LODES_NEW      	      =O_LODES         *O_LODES_FACTOR, 
-  O_LODES_SELF_NEW        =O_LODES_NEW + O_ACS_Self_Emp,
-  O_NETS_NEW       	      =O_NETS          *O_NETS_FACTOR,       
-  O_NETS_NOSOLE_NEW	      =O_NETS_NOSOLE   *O_NETS_NOSOLE_FACTOR,
-  R_ESRI_NEW       	      =R_ESRI          *R_ESRI_FACTOR,       
-  R_LODES_NEW      	      =R_LODES         *R_LODES_FACTOR,
-  R_LODES_SELF_NEW        =R_LODES_NEW + R_ACS_Self_Emp,
-  R_NETS_NEW       	      =R_NETS          *R_NETS_FACTOR,       
-  R_NETS_NOSOLE_NEW	      =R_NETS_NOSOLE   *R_NETS_NOSOLE_FACTOR) %>% 
-  select(ZONE,county,
-         A_ESRI_NEW,A_LODES_NEW,A_LODES_SELF_NEW,A_NETS_NEW,A_NETS_NOSOLE_NEW,
-         F_ESRI_NEW,F_LODES_NEW,F_LODES_SELF_NEW,F_NETS_NEW,F_NETS_NOSOLE_NEW,
-         H_ESRI_NEW,H_LODES_NEW,H_LODES_SELF_NEW,H_NETS_NEW,H_NETS_NOSOLE_NEW,
-         M_ESRI_NEW,M_LODES_NEW,M_LODES_SELF_NEW,M_NETS_NEW,M_NETS_NOSOLE_NEW,
-         O_ESRI_NEW,O_LODES_NEW,O_LODES_SELF_NEW,O_NETS_NEW,O_NETS_NOSOLE_NEW,
-         R_ESRI_NEW,R_LODES_NEW,R_LODES_SELF_NEW,R_NETS_NEW,R_NETS_NOSOLE_NEW) %>% mutate(
+  A_ESRI_NOIN              =A_ESRI          *A_ESRI_FACTOR,       
+  A_LODES_NOIN      	     =A_LODES         *A_LODES_FACTOR,
+  A_LODES_SELF_NOIN        =A_LODES_NOIN    +A_ACS_Self_Emp,
+  A_NETS_NOIN       	     =A_NETS          *A_NETS_FACTOR,       
+  A_NETS_NOSOLE_NOIN	     =A_NETS_NOSOLE   *A_NETS_NOSOLE_FACTOR,
+  F_ESRI_NOIN       	     =F_ESRI          *F_ESRI_FACTOR,       
+  F_LODES_NOIN      	     =F_LODES         *F_LODES_FACTOR, 
+  F_LODES_SELF_NOIN        =F_LODES_NOIN    +F_ACS_Self_Emp,
+  F_NETS_NOIN       	     =F_NETS          *F_NETS_FACTOR,       
+  F_NETS_NOSOLE_NOIN	     =F_NETS_NOSOLE   *F_NETS_NOSOLE_FACTOR,
+  H_ESRI_NOIN       	     =H_ESRI          *H_ESRI_FACTOR,       
+  H_LODES_NOIN      	     =H_LODES         *H_LODES_FACTOR,  
+  H_LODES_SELF_NOIN        =H_LODES_NOIN    +H_ACS_Self_Emp,
+  H_NETS_NOIN       	     =H_NETS          *H_NETS_FACTOR,       
+  H_NETS_NOSOLE_NOIN	     =H_NETS_NOSOLE   *H_NETS_NOSOLE_FACTOR,
+  M_ESRI_NOIN       	     =M_ESRI          *M_ESRI_FACTOR,       
+  M_LODES_NOIN      	     =M_LODES         *M_LODES_FACTOR,  
+  M_LODES_SELF_NOIN        =M_LODES_NOIN    +M_ACS_Self_Emp,
+  M_NETS_NOIN       	     =M_NETS          *M_NETS_FACTOR,       
+  M_NETS_NOSOLE_NOIN	     =M_NETS_NOSOLE   *M_NETS_NOSOLE_FACTOR,
+  O_ESRI_NOIN       	     =O_ESRI          *O_ESRI_FACTOR,       
+  O_LODES_NOIN      	     =O_LODES         *O_LODES_FACTOR, 
+  O_LODES_SELF_NOIN        =O_LODES_NOIN    +O_ACS_Self_Emp,
+  O_NETS_NOIN       	     =O_NETS          *O_NETS_FACTOR,       
+  O_NETS_NOSOLE_NOIN	     =O_NETS_NOSOLE   *O_NETS_NOSOLE_FACTOR,
+  R_ESRI_NOIN       	     =R_ESRI          *R_ESRI_FACTOR,       
+  R_LODES_NOIN      	     =R_LODES         *R_LODES_FACTOR,
+  R_LODES_SELF_NOIN        =R_LODES_NOIN    +R_ACS_Self_Emp,
+  R_NETS_NOIN       	     =R_NETS          *R_NETS_FACTOR,       
+  R_NETS_NOSOLE_NOIN	     =R_NETS_NOSOLE   *R_NETS_NOSOLE_FACTOR) %>% 
+  rename(TAZ=ZONE,County=county) %>% 
+  select(TAZ,County,
+         A_ESRI,A_ACS_Self_Emp,A_LODES,A_LODES_SELF,A_NETS,A_NETS_NOSOLE, 
+         F_ESRI,F_ACS_Self_Emp,F_LODES,F_LODES_SELF,F_NETS,F_NETS_NOSOLE, 
+         H_ESRI, H_ACS_Self_Emp, H_LODES, H_LODES_SELF,H_NETS,H_NETS_NOSOLE,
+         M_ESRI, M_ACS_Self_Emp, M_LODES, M_LODES_SELF,M_NETS,M_NETS_NOSOLE,
+         O_ESRI, O_ACS_Self_Emp, O_LODES, O_LODES_SELF,O_NETS,O_NETS_NOSOLE,
+         R_ESRI, R_ACS_Self_Emp, R_LODES, R_LODES_SELF,R_NETS,R_NETS_NOSOLE,
+         T_ESRI, T_ACS_Self_Emp, T_LODES, T_LODES_SELF,T_NETS,T_NETS_NOSOLE,
+         
+         A_ESRI_NOIN,A_LODES_NOIN,A_LODES_SELF_NOIN,A_NETS_NOIN,A_NETS_NOSOLE_NOIN,
+         F_ESRI_NOIN,F_LODES_NOIN,F_LODES_SELF_NOIN,F_NETS_NOIN,F_NETS_NOSOLE_NOIN,
+         H_ESRI_NOIN,H_LODES_NOIN,H_LODES_SELF_NOIN,H_NETS_NOIN,H_NETS_NOSOLE_NOIN,
+         M_ESRI_NOIN,M_LODES_NOIN,M_LODES_SELF_NOIN,M_NETS_NOIN,M_NETS_NOSOLE_NOIN,
+         O_ESRI_NOIN,O_LODES_NOIN,O_LODES_SELF_NOIN,O_NETS_NOIN,O_NETS_NOSOLE_NOIN,
+         R_ESRI_NOIN,R_LODES_NOIN,R_LODES_SELF_NOIN,R_NETS_NOIN,R_NETS_NOSOLE_NOIN) %>% mutate(
            
-         T_ESRI_NEW=A_ESRI_NEW+F_ESRI_NEW+H_ESRI_NEW+M_ESRI_NEW+O_ESRI_NEW+R_ESRI_NEW,
-         T_LODES_NEW=A_LODES_NEW+F_LODES_NEW+H_LODES_NEW+M_LODES_NEW+O_LODES_NEW+R_LODES_NEW,
-         T_LODES_SELF_NEW=A_LODES_SELF_NEW+F_LODES_SELF_NEW+H_LODES_SELF_NEW+M_LODES_SELF_NEW+O_LODES_SELF_NEW+R_LODES_SELF_NEW,
-         T_NETS_NEW=A_NETS_NEW+F_NETS_NEW+H_NETS_NEW+M_NETS_NEW+O_NETS_NEW+R_NETS_NEW,
-         T_NETS_NOSOLE_NEW=A_NETS_NOSOLE_NEW+F_NETS_NOSOLE_NEW+H_NETS_NOSOLE_NEW+M_NETS_NOSOLE_NEW+O_NETS_NOSOLE_NEW+R_NETS_NOSOLE_NEW)
-       
-write.csv(employment_no_incommute, "trial.csv", row.names = FALSE, quote = T)
+         T_ESRI_NOIN=A_ESRI_NOIN+F_ESRI_NOIN+H_ESRI_NOIN+M_ESRI_NOIN+O_ESRI_NOIN+R_ESRI_NOIN,
+         T_LODES_NOIN=A_LODES_NOIN+F_LODES_NOIN+H_LODES_NOIN+M_LODES_NOIN+O_LODES_NOIN+R_LODES_NOIN,
+         T_LODES_SELF_NOIN=A_LODES_SELF_NOIN+F_LODES_SELF_NOIN+H_LODES_SELF_NOIN+M_LODES_SELF_NOIN+O_LODES_SELF_NOIN+R_LODES_SELF_NOIN,
+         T_NETS_NOIN=A_NETS_NOIN+F_NETS_NOIN+H_NETS_NOIN+M_NETS_NOIN+O_NETS_NOIN+R_NETS_NOIN,
+         T_NETS_NOSOLE_NOIN=A_NETS_NOSOLE_NOIN+F_NETS_NOSOLE_NOIN+H_NETS_NOSOLE_NOIN+M_NETS_NOSOLE_NOIN+O_NETS_NOSOLE_NOIN+R_NETS_NOSOLE_NOIN)
 
-write.csv(temp3,row.names = FALSE,quote=T)
-
-
-
-employment <- cbind(employment, industry_share)
+write.csv(employment_no_incommute, "Employment data by sector with and wo incommute 032420.csv", row.names = FALSE, quote = T)
 
