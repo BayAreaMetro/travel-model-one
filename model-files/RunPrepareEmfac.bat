49d76cda22229d36e65b253eb1b37ab71a4d5c30
Flavia <flavia.tsang@gmail.com>
2020-03-25 15:02:00 -0700

    Updated to make the awk script use the "no truck" file for SB375

--- model-files/RunPrepareEmfac.bat
+++ model-files/RunPrepareEmfac.bat
@@ -6,8 +6,13 @@
 :: ** New Futures Runs for Fuel Consumption and Fuel Economy Estimates                   **
 :: ** These runs forward account for speed bin VMT overlap      hmb.   12/11/19.         **
 
+:: added an argument to indicate whether we are running emfac for SB375 (no trucks)
+:: To run this batch file, use command: 
+:: RunPrepareEmfac.bat SB375
+:: (Or, RunPrepareEmfac.bat Conformity)
+
 call  ctramp\runtime\setpath
-mkdir emfac_output
+mkdir emfac_prep
 
 :: Step One
 call runtpp CTRAMP\scripts\emfac\CreateSpeedBinsBetweenZones.job
@@ -16,9 +21,18 @@ call runtpp CTRAMP\scripts\emfac\CreateSpeedBinsBetweenZones.job
 call runtpp CTRAMP\scripts\emfac\CreateSpeedBinsWithinZones.job
 
 :: Step Three
-call gawk -f CTRAMP\scripts\emfac\SumSpeedBins1.awk emfac_output\CreateSpeedBins*.csv
+
+:: use the "no truck" files if the run is for SB375
+if %1==SB375 rename emfac_prep\CreateSpeedBinsBetweenZones_sums.csv CreateSpBetweenZones_NotUsed.csv
+if %1==SB375 rename emfac_prep\CreateSpeedBinsWithinZones_sums.csv CreateSpWithinZones_NotUsed.csv
+
+:: use the "with truck" files if the run is for conformity
+if %1==conformity rename emfac_prep\CreateSpeedBinsBetweenZones_sums_NoTrk.csv CreateSpBetweenZonesNoTruck_NotUsed.csv
+if %1==conformity rename emfac_prep\CreateSpeedBinsWithinZones_sums_NoTrk.csv CreateSpWithinZonesNoTruck_NotUsed.csv
+
+call gawk -f CTRAMP\scripts\emfac\SumSpeedBins1.awk emfac_prep\CreateSpeedBins*.csv
 
 
-move HourlyTotalCounty.csv emfac_output\HourlyTotalCounty.csv
-move ShareSpeedBinsAll_sums.csv emfac_output\ShareSpeedBinsAll_sums.csv
-move SumSpeedBinsAll_sums.csv emfac_output\SumSpeedBinsAll_sums.csv
+move HourlyTotalCounty.csv emfac_prep\HourlyTotalCounty.csv
+move ShareSpeedBinsAll_sums.csv emfac_prep\ShareSpeedBinsAll_sums.csv
+move SumSpeedBinsAll_sums.csv emfac_prep\SumSpeedBinsAll_sums.csv
