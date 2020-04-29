8c868edb0864f90c0cdc8318b3beb6dc8bcd2363
Flavia <flavia.tsang@gmail.com>
2020-04-29 12:03:22 -0700

    edited so we need to set UseTollDist for DBP projects only

--- model-files/RunModel.bat
+++ model-files/RunModel.bat
@@ -159,7 +159,7 @@ python CTRAMP\scripts\preprocess\RuntimeConfiguration.py
 if ERRORLEVEL 1 goto done
 
 :: For the "Per-Mile Tolling on Congested Freeways" in the Draft Blueprint
-if %UseTollDist%==Yes (python CTRAMP\scripts\preprocess\updateUECsToUseTollDist.py)
+if (%PROJECT%==DBP) (if (%UseTollDist%==Yes) (python CTRAMP\scripts\preprocess\updateUECsToUseTollDist.py)) else (echo "No need to run updateUECsToUseTollDist.py")
 
 :: Set the prices in the roadway network (convert csv to dbf first)
 python CTRAMP\scripts\preprocess\csvToDbf.py hwy\tolls.csv hwy\tolls.dbf
