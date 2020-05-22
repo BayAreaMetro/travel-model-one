a6fe4a9edba9651ede03c45068cb8a2a9c0c718a
Flavia <flavia.tsang@gmail.com>
2020-05-22 15:55:38 -0700

    adding model year as an arguement for Prep_popsyn_landuse.bat

--- utilities/taz-data-builder/Prep_popsyn_landuse.bat
+++ utilities/taz-data-builder/Prep_popsyn_landuse.bat
@@ -3,12 +3,14 @@
 :: to call this batch script, type the script name and then three arugments
 :: also make sure to add "> prep_popsyn_landuse.log" when you call the batch file so a log will be produced
 :: e.g. 
-:: prep_popsyn_landuse BackToTheFuture 20190724 run10 > prep_popsyn_landuse.log
+:: prep_popsyn_landuse BackToTheFuture 20190724 run10 2035 > prep_popsyn_landuse.log
+:: prep_popsyn_landuse PBA50 20200522 run939 2035 > prep_popsyn_landuse.log
 :: 
 :: the 3 arugments are:
 :: 1. Future Name
 :: 2. Date of land use model run
 :: 3. Land use run number
+:: 4. Model Year
 
 
 ::-----------------------------
@@ -18,8 +20,8 @@ mkdir popsyn
 
 echo
 echo *****THE SOURCES OF THE POPSYN FILES ARE:*****
-copy \\mainmodel\MainModelShare\populationsim\bay_area\output_2050_%1_%2_%3\synthetic_households.csv     popsyn\hhFile.%3.%1.2050.csv
-copy \\mainmodel\MainModelShare\populationsim\bay_area\output_2050_%1_%2_%3\synthetic_persons.csv        popsyn\personFile.%3.%1.2050.csv
+copy \\mainmodel\MainModelShare\populationsim\bay_area\output_%4_%1_%2_%3\synthetic_households.csv     popsyn\hhFile.%3.%1.%4.csv
+copy \\mainmodel\MainModelShare\populationsim\bay_area\output_%4_%1_%2_%3\synthetic_persons.csv        popsyn\personFile.%3.%1.%4.csv
 
 ::-----------------------------
 :: land use files
@@ -28,13 +30,13 @@ mkdir landuse
 
 echo
 echo *****THE SOURCE OF THE TAZDATA FILE IS:*****
-copy \\mainmodel\MainModelShare\populationsim\bay_area\hh_gq\data\taz_summaries_%1_%2_%3_2050.csv        landuse\
+copy \\mainmodel\MainModelShare\populationsim\bay_area\hh_gq\data\taz_summaries_%1_%2_%3_%4.csv        landuse\
 
 :: to generate tazData.csv
 :: ---
 cd landuse
 set path=%path%;c:\python27
-python X:\travel-model-one-master\utilities\taz-data-builder\buildTazdata.py taz_summaries_%1_%2_%3_2050.csv
+python X:\travel-model-one-master\utilities\taz-data-builder\buildTazdata.py taz_summaries_%1_%2_%3_%4.csv
 
 :: to convert csv to dbf
 :: ---
