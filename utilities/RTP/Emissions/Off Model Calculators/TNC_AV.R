8e8c5777ca579a85be193bdfa15da865b47f8690
Shimon Israel <sisrael@bayareametro.gov>
2020-05-27 18:59:00 -0700

    Update script to include dynamic values
    Reads params.properties file to dynamically capture TNC occupancies, 3+ passengers averages for single and shared TNCs, and TNC/zero-passenger vehicle factor.

--- utilities/RTP/Emissions/Off Model Calculators/TNC_AV.R
+++ utilities/RTP/Emissions/Off Model Calculators/TNC_AV.R
@@ -14,60 +14,76 @@ OUTPUT_DIR          <-file.path(Box_H,"Horizon and Plan Bay Area 2050/Blueprint/
 OUTPUT_FILE         <-file.path(OUTPUT_DIR, "Model Data - TNC_AV.csv")
 trips_location      <-file.path(MODEL_DATA_BASE_DIR,"2035_TM152_IPA_aoc1795_00/OUTPUT/updated_output/trips.rdata") 
 
-# Get model run sampleshare for later weighting of trips
+# this is the currently running script
 
-sampleshare <- as.numeric(Sys.getenv("sampleshare"))
-weight      <- 1/sampleshare
+SCRIPT     <- "X:/travel-model-one-master/utilities/RTP/Emissions/Off Model Calculators/TNC_AV.R"
 
-# Occupancy Assumptions for TNC single and shared vehicles
-# https://github.com/BayAreaMetro/travel-model-one/blob/master/config/params_PBA50_Blueprint2050.properties#L116
+# the model runs are in the parent folder
 
-single.da.share = 0.53      # Share of single-passenger rides for non-shared TNCs
-single.s2.share = 0.29      # Share of 2-passenger rides for non-shared TNCs
-single.s3.share = 0.18      # Share of 3plus-passenger rides for non-shared TNCs
+model_runs <- read.table(file.path(dirname(SCRIPT),"..","ModelRuns_RTP2021.csv"), header=TRUE, sep=",", stringsAsFactors = FALSE)
 
-shared.da.share = 0.09      # Share of single-passenger rides for shared TNCs
-shared.s2.share = 0.29      # Share of 2-passenger rides for shared TNCs
-shared.s3.share = 0.62      # Share of 2-passenger rides for shared TNCs
+# Read trips.rdata for respective scenarios and summarize important variables, appending each new scenario
+# Start with empty data frame
 
-# Average occupancy for 3-plus rides, single and shared TNC trips
-# https://github.com/BayAreaMetro/travel-model-one/blob/master/utilities/RTP/metrics/sumAutoTimes.job#L192
+tripdata_df          <- data.frame()
 
-single3p        = 3.60      # Average occupancy for 3-plus passenger non-shared TNCs
-shared3p        = 3.83      # Average occupancy for 3-plus passenger shared TNCs
-
+for (i in 1:nrow(model_runs)) {
+  
+# Bring in parameters file, search for TNC occupancy assumptions and TNC zero-passenger vehicle multiplier
+# Grep Fn separates string into two, extracts the second element (after the equals sign,the parameter of interest)
+  
+    params <- readLines(file.path(MODEL_DATA_BASE_DIR,model_runs[i,"directory"],"INPUT","params.properties"))
+    
+    single.da.share  <- as.numeric(str_split_fixed(grep("TNC.single.da.share",params,value=TRUE),"=",n=2)[2])
+    single.s2.share  <- as.numeric(str_split_fixed(grep("TNC.single.s2.share",params,value=TRUE),"=",n=2)[2])
+    single.s3.share  <- as.numeric(str_split_fixed(grep("TNC.single.s3.share",params,value=TRUE),"=",n=2)[2])
+    
+    shared.da.share  <- as.numeric(str_split_fixed(grep("TNC.shared.da.share",params,value=TRUE),"=",n=2)[2])
+    shared.s2.share  <- as.numeric(str_split_fixed(grep("TNC.shared.s2.share",params,value=TRUE),"=",n=2)[2])
+    shared.s3.share  <- as.numeric(str_split_fixed(grep("TNC.shared.s3.share",params,value=TRUE),"=",n=2)[2])
+      
+    single.s3.occ    <- as.numeric(str_split_fixed(grep("TNC.single.s3.occ",params,value=TRUE),"=",n=2)[2])
+    shared.s3.occ    <- as.numeric(str_split_fixed(grep("TNC.shared.s3.occ",params,value=TRUE),"=",n=2)[2])
+    
+    TNC_ZPV_fac      <- as.numeric(str_split_fixed(grep("TNC_ZPV_fac",params,value=TRUE),"=",n=2)[2])
+    
 # PMT to VMT conversion factors for single and shared TNC trips
+    
+    single_factor   = single.da.share*1 + single.s2.share*2 + single.s3.share*single.s3.occ
+    shared_factor   = shared.da.share*1 + shared.s2.share*2 + shared.s3.share*shared.s3.occ    
 
-single_factor   = single.da.share*1 + single.s2.share*2 + single.s3.share*single3p
-shared_factor   = shared.da.share*1 + shared.s2.share*2 + shared.s3.share*shared3p
+# Now bring in trip file for respective scenarios and perform summaries, then append and repeat until done
+    
+    trip_file          <- file.path(MODEL_DATA_BASE_DIR, model_runs[i,"directory"],"OUTPUT","updated_output","trips.rdata")
+    load(trip_file)                                         
+    trip_file_df       <- trips %>%                         # "trips" comes from native name of loaded .rdata dataset
+      select(trip_mode,distance,sampleRate) %>%             # Mode, distance, and sampleRate are only needed variables
+      filter(trip_mode %in% c(20,21)) %>%                   # TNC single-party and shared, respectively
+      group_by(trip_mode) %>%                               # Sum trips by TNC mode
+      summarize(pmt=sum(distance*1/sampleRate)) %>%         # Apply weight, which is the inverse of sampleRate
+      spread(trip_mode,pmt) %>%                             # List to matrix format
+      rename(TNC_Single_PMT="20",TNC_Shared_PMT="21") %>%   # Rename variable names from mode values
+      mutate(                                               # Add VMT values
+          TNC_Single_VMT = TNC_Single_PMT/single_factor,        # Single PMT divided by single factor = single VMT
+          TNC_Shared_VMT = TNC_Shared_PMT/shared_factor,        # Shared PMT divided by shared factor = shared VMT
+          TNC_Total_VMT  = TNC_Single_VMT + TNC_Shared_VMT,     # Combined total of single and shared TNC VMT
+          ZPV_VMT        = TNC_Total_VMT*TNC_ZPV_fac            # Zero passenger vehicle factor for TNC VMT
+        ) %>% 
+      mutate_if(is.numeric, round,0) %>%                    # Round all numeric values to ones place
+      mutate(
+           year      = model_runs[i,"year"],                # Now append scenario variables
+           directory = model_runs[i,"directory"],
+           category  = model_runs[i,"category"])
+    
+  tripdata_df        <- rbind(tripdata_df, trip_file_df)    # Append
+}
 
-# Bring in trips and subset needed variables, filter only TNC trips
+# Remove i and large trip files, output file
 
-load(trips_location)
+rm(i, trips, trip_file_df)
 
-trips_TNC <- trips %>% 
-  select(trip_mode,distance) %>%                # Mode and distance are only needed variables
-  filter(trip_mode %in% c(20,21))               # TNC single-party and shared, respecively
+write.csv(tripdata_df,OUTPUT_FILE,row.names = FALSE,quote=T)
 
-# Summarize totals by TNC mode, applying weight that corrects for sampleshare
-
-temp1 <- trips_TNC %>%                                   
-  group_by(trip_mode) %>%                               # Sum trips by TNC mode
-  summarize(pmt=sum(distance*weight)) %>%               # Apply weight from sampleshare
-  spread(trip_mode,pmt) %>%                             # List to matrix format
-  rename(TNC_Single_PMT="20",TNC_Shared_PMT="21")       # Rename variable names from mode values
-
-# Calculate VMT from PMT
-
-final <- temp1 %>% mutate(
-  TNC_Single_VMT = TNC_Single_PMT/single_factor,        # Single PMT divided by single factor = single VMT
-  TNC_Shared_VMT = TNC_Shared_PMT/shared_factor,        # Shared PMT divided by shared factor = shared VMT
-  TNC_Total_VMT  = TNC_Single_VMT + TNC_Shared_VMT,     # Combined total of single and shared TNC VMT
-  ZPV_VMT        = TNC_Total_VMT*0.7                    # ZPV factor for TNC VMT
-) %>% 
-  mutate_if(is.numeric, round,0)                        # Round all numeric values to ones place
-
-write.csv(final,OUTPUT_FILE,row.names = FALSE,quote=T)
 
 
 
