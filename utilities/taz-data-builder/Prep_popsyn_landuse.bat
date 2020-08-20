4d1f914b84970a542ea60fb6996d0124488eec59
Lisa Zorn <lzorn@bayareametro.gov>
2020-08-20 16:44:59 -0700

    Update Prep_popsyn_landuse.bat to create walkAccessBuffers.float.csv
    using petrale script / UrbanSim data

--- utilities/taz-data-builder/Prep_popsyn_landuse.bat
+++ utilities/taz-data-builder/Prep_popsyn_landuse.bat
@@ -1,5 +1,5 @@
-:: this batch script is assumed to be run from the relevant subdirectory within the INPUT_repo directory 
-:: M:\Application\Model One\RTP2021\Blueprint\INPUT_repo\PopSyn_n_LandUse\Basic_00\2050
+:: this batch script is assumed to be run from the relevant subdirectory within the INPUT_DEVELOPMENT directory 
+:: M:\Application\Model One\RTP2021\Blueprint\INPUT_DEVELOPMENT\PopSyn_n_LandUse\Basic_00\2050
 
 :: to call this batch script, type the script name and then four arugments
 :: also make sure to add "> prep_popsyn_landuse.log" when you call the batch file so a log will be produced
@@ -8,10 +8,15 @@
 :: X:\travel-model-one-master\utilities\taz-data-builder\prep_popsyn_landuse.bat PBA50 20200522 run939 2050 > prep_popsyn_landuse.log
 :: 
 :: the 3 arugments are:
-:: 1. Future Name
-:: 2. Date of land use model run
-:: 3. Land use run number
-:: 4. Model Year
+:: 1. Future Name - e.g. PBA50PlusCrossing
+:: 2. Date of land use model run - e.g. 20200622
+:: 3. Land use run number - e.g. run98
+:: 4. Model Year - e.g. 2035
+:: 5. UrbanSim outut folder relative to M:\Data\Urban\BAUS\PBA50\Draft_Blueprint\runs - e.g. "Blueprint Plus Crossing (s23)\v1.7.1- FINAL DRAFT BLUEPRINT"
+::
+
+:: note we dequote arg 5 
+echo ARG1=[%1]  ARG2=[%2]  ARG3=[%3]  ARG4=[%4]  ARG5=[%~5]
 
 ::-----------------------------
 :: pop syn files
@@ -48,11 +53,17 @@ set F_INPUT=tazData.csv
 Set F_OUTPUT=tazData.dbf
 "%R_HOME%\Rscript.exe" X:\travel-model-one-master\utilities\taz-data-csv-to-dbf\taz-data-csv-to-dbf.R
 
-:: also need to copy the walk access file
-:: which hasn't change since 2010
-:: ---
 cd ..
-copy "M:\Application\Model One\RTP2017\Scenarios\2040_06_694_Amd1\INPUT_fullcopy\landuse\walkAccessBuffers.float.csv"          landuse\
+:: also need to copy the walk access file for 2010 and before
+:: otherwise create it with clear name and then copy to walkAccessBuffers.float.csv
+:: see https://app.asana.com/0/403262763383022/1161734609745564/f
+if %4 LEQ 2010 (
+  copy "M:\Application\Model One\RTP2017\Scenarios\2040_06_694_Amd1\INPUT_fullcopy\landuse\walkAccessBuffers.float.csv"          landuse\
+
+) else (
+  python X:\petrale\applications\tally_household_share_by_taz_subzone.py "%~5\%3_parcel_data_%4.csv" landuse\taz_subzone_hhshare_%3_%4.csv
+  copy landuse\taz_subzone_hhshare_%3_%4.csv landuse\walkAccessBuffers.float.csv
+)
 
 
 
