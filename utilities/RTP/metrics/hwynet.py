ce6528e22539631d1aac343da35e376d798db451
Lisa Zorn <lzorn@bayareametro.gov>
2021-01-28 18:23:07 -0800

    Added optional arguments to create vmt_vht_metrics_by_taz.csv
    It's implemented more generically but can be used for this purpose

--- utilities/RTP/metrics/hwynet.py
+++ utilities/RTP/metrics/hwynet.py
@@ -2,11 +2,16 @@ import argparse, itertools, optparse, os, sys
 import numpy, pandas
 
 USAGE = """
- python hwynet.py hwynet.csv
+  python hwynet.py --filter filter --year 2035 hwy\\iter3\\avgload5period_vehclasses.csv [--link_mapping links_mapping.csv indexcol sharecol output_suffix]
 
  Reads the csv file of links from hwynet.csv and reports a number of
  metrics into metrics/vmt_vht_metrics by timeperiod and vehicle class.
- 
+
+ Also reads 
+   - INPUT\\metrics\\nonRecurringDelayLookup.csv
+   - INPUT\\metrics\\collisionLookup.csv
+   - INPUT\\metrics\\emissionsLookup.csv
+
  These metrics consist of the following:
   * VMT: Vehicle Miles Traveled
   * VHT: Vehicle Hours Traveled
@@ -38,6 +43,30 @@ USAGE = """
   * PM10
   * PM10_wear
   * PM2.5_wear
+
+  If optional --link_mapping parameters are specified (--link_mapping links_mapping.csv indexcol sharecol output_suffix), then
+  links_mapping.csv is read and columns [A,B,(indexcol),(sharecol)] are read and assumed to cover all the links.
+  The per-link metric values are then joined to this dataframe by A,B and multiplied by sharecol and aggregated by indexcol.
+  The script will then output the additional file, metrics\\vmt_vht_metrics(output_suffix).csv
+
+  For example, to get vmt_vht_metrics by TAZ, create a link-to-TAZ mapping file as follows:
+
+  1) From a directory with the shapefile export of the roadway network 
+     e.g. M:\Application\Model One\RTP2021\Blueprint\INPUT_DEVELOPMENT\Networks\BlueprintNetworks_50\\net_2050_Blueprint Plus Crossing\shapefile
+      
+     Run the following command:
+     python X:\\travel-model-one-master\utilities\cube-to-shapefile\correspond_link_to_TAZ.py network_links.shp network_links_TAZ.csv
+
+     This will create a file with columns A, B, TAZ1454, link_mi, linktaz_mi, linktaz_share
+
+     Here, the indexcol is TAZ1454, and the sharecol is linktaz_share
+
+  2) Then run this script with the --link_mapping args as follows:
+
+    e.g. python X:\\travel-model-one-master\utilities\RTP\metrics\hwynet.py --filter PBA50 --year 2035 
+         --link_mapping "M:\Application\Model One\RTP2021\Blueprint\INPUT_DEVELOPMENT\Networks\BlueprintNetworks_51\\net_2035_Blueprint Plus\shapefile\\network_links_TAZ.csv" TAZ1454 linktaz_share _by_taz
+         hwy\iter3\\avgload5period_vehclasses.csv 
+
 """
 pandas.options.display.width = 1000
 pandas.options.display.max_columns = 100
@@ -46,6 +75,8 @@ parser = argparse.ArgumentParser(description=USAGE, formatter_class=argparse.Raw
 parser.add_argument("--filter", metavar="lookup_filter", help="Filter keyword for lookup files", required=True)
 parser.add_argument("--year",   metavar="year", help="Filter keyword for lookup files", required=True, type=int)
 parser.add_argument("net_csv",  metavar="avgload5period_vehclasses.csv", help="Loaded network export with vehicle classes")
+parser.add_argument("--link_mapping", metavar="link_mapping",  nargs=4,
+                    help="Optional mapping csv to summarize by another index. Four args are: links_mapping.csv indexcol sharecol output_suffix")
 
 args = parser.parse_args()
 datafile            = args.net_csv
@@ -69,6 +100,16 @@ print("Read {} links from {}".format(num_links, datafile))
 id_fields    = ['a','b']
 fields       = ['distance','lanes','at','ft','fft']
 
+link_mapping_df = pandas.DataFrame()
+if args.link_mapping:
+  link_mapping_df = pandas.read_csv(args.link_mapping[0])
+  # keep A,B,[indexcol],[sharecol]
+  link_mapping_df = link_mapping_df[[ 'A','B',args.link_mapping[1], args.link_mapping[2] ]]
+  # rename A,B to a,b
+  link_mapping_df.rename(columns={'A':'a','B':'b'}, inplace=True)
+  print("link_mapping_df read from {}; head:\n{}".format(args.link_mapping[0], link_mapping_df.head()))
+  print("link_mapping_df.{}.sum()={}".format(args.link_mapping[2], link_mapping_df[args.link_mapping[2]].sum()))
+
 # first, unstack timeperiod fields - so instead of xxEA, xxAM, xxMD, etc, we have xx with timeperiod as a column
 # so this will be link_df x 5
 link_tp_df = pandas.DataFrame()
@@ -90,7 +131,7 @@ for tp_field in ['cspd{}','vol{}_tot','vc{}','ctim{}']:
 
 # add in other link-specific non-id fields
 link_tp_df = pandas.merge(how='left', left=link_tp_df, right=link_df[id_fields + fields], on=id_fields)
-print("Melted to timeperiods: {} rows".format(len(link_tp_df)))
+print("Melted to timeperiods to create link_tp_df: {} rows".format(len(link_tp_df)))
 print(link_tp_df.head())
 assert(len(link_tp_df) == len(periods)*num_links)
 
@@ -110,7 +151,7 @@ tp_vc_field_df = pandas.DataFrame({'timeperiod':timeperiod_list, 'vclass':vclass
 link_tp_vehclass_df = pandas.melt(link_df, id_vars=id_fields, value_vars=tp_vc_field_list)
 link_tp_vehclass_df = pandas.merge(how='left', left=link_tp_vehclass_df, right=tp_vc_field_df)
 link_tp_vehclass_df = link_tp_vehclass_df.rename(columns={'value':'vol'}).drop(columns='variable')
-print("Melted to timeperiods x vehclasses: {} rows".format(len(link_tp_vehclass_df)))
+print("Melted to timeperiods x vehclasses to createlink_tp_vehclass_df: {} rows".format(len(link_tp_vehclass_df)))
 print(link_tp_vehclass_df.head())
 assert(len(link_tp_vehclass_df) == len(vclasses)*len(periods)*num_links)
 
@@ -133,7 +174,7 @@ nrclookup_df.set_index('vcratio',inplace=True)
 nrclookup_df.rename(columns={'2lanes':2, '3lanes':3, '4lanes':4}, inplace=True)
 nrclookup_df = nrclookup_df.stack().reset_index().rename(columns={'level_1':'lanes_24',0:'nrcdelay'})
 print(nrclookup_df.head())
-print("Read {} and filtered by {} and year {}:\n{}".format(nrc_file, args.filter, args.year, nrclookup_df.head()))
+print("Read {} and filtered by {} and year {} to create nrclookup_df:\n{}".format(nrc_file, args.filter, args.year, nrclookup_df.head()))
 
 # Units are collisions per 1,000,000 VMT
 # Map headers -> index for this lokup and read lookup data
@@ -151,26 +192,29 @@ if len(collisionlookup_df) == 0:
   print("No collisions lookups for {} found".format(args.filter))
   sys.exit(2)
 collisionlookup_df.drop(columns=['filter','year'],inplace=True)
-print("Read {} and filtered by {} and year {}:\n{}".format(collision_file, args.filter, args.year, collisionlookup_df.head()))
+print("Read {} and filtered by {} and year {} to create collisionlookup_df:\n{}".format(collision_file, args.filter, args.year, collisionlookup_df.head()))
 
 # Units are grams per mile (equivalent to metric tons per 1,000,000 VMT)
-if args.filter!='PBA50':
-    emission_file = os.path.join(lookupdir,"emissionsLookup.csv")
-    emissionslookup_df = pandas.read_csv(emission_file)
-    emission_types = list(emissionslookup_df.columns.values)
-    emission_types.remove('period')
-    emission_types.remove('vclassgroup')
-    emission_types.remove('speed')
-    emission_types.remove('filter')
-    emission_types.remove('year')
-    # filter by given filter
-    emissionslookup_df = emissionslookup_df.loc[ (emissionslookup_df['filter'] == args.filter)&
-                                             (emissionslookup_df['year'  ] == args.year  ) ].copy()
-    if len(emissionslookup_df) == 0:
-      print("No emission lookups for {} found".format(args.filter))
-      sys.exit(2)
-    emissionslookup_df.drop(columns=['filter','year'],inplace=True)
-    print("Read {} and filtered by {} and year {}:\n{}".format(emission_file, args.filter, args.year, emissionslookup_df.head()))
+emission_file = os.path.join(lookupdir,"emissionsLookup.csv")
+emissionslookup_df = pandas.DataFrame()
+if not os.path.exists(emission_file):
+  print("Emissions file {} doesn't exist -- skipping".format(emission_file))
+else:
+  emissionslookup_df = pandas.read_csv(emission_file)
+  emission_types = list(emissionslookup_df.columns.values)
+  emission_types.remove('period')
+  emission_types.remove('vclassgroup')
+  emission_types.remove('speed')
+  emission_types.remove('filter')
+  emission_types.remove('year')
+  # filter by given filter
+  emissionslookup_df = emissionslookup_df.loc[ (emissionslookup_df['filter'] == args.filter)&
+                                               (emissionslookup_df['year'  ] == args.year  ) ].copy()
+  if len(emissionslookup_df) == 0:
+    print("No emission lookups for {} found".format(args.filter))
+    sys.exit(2)
+  emissionslookup_df.drop(columns=['filter','year'],inplace=True)
+  print("Read {} and filtered by {} and year {} to create emissionslookup_df:\n{}".format(emission_file, args.filter, args.year, emissionslookup_df.head()))
 
 # Add link x timeperiod columns
 
@@ -240,7 +284,7 @@ link_tp_vehclass_df = pandas.merge(how='left', left=link_tp_vehclass_df, right=v
 null_vclassgroup = link_tp_vehclass_df.loc[ pandas.isna(link_tp_vehclass_df.vclassgroup) ]
 assert(len(null_vclassgroup) == 0)
 
-if args.filter!='PBA50':
+if len(emissionslookup_df)>0:
     link_tp_vehclass_df = pandas.merge(how='left', left=link_tp_vehclass_df,
                                        right=emissionslookup_df.rename(columns={'period':'timeperiod'}))
     # verify merging with lookup didn't change the number of links/vclass/periods
@@ -257,21 +301,51 @@ aggregate_dict = {'vmt'     :'sum',
                   'hypfft'  :'sum',
                   'nrcdelay':'sum'}
 for collision_type in collision_types: aggregate_dict[collision_type] = 'sum'
-if args.filter!='PBA50':
+if len(emissionslookup_df)>0:
     for emission_type  in emission_types:  aggregate_dict[emission_type]  = 'sum'
 
 # aggregate to timeperiod x vclass
 metrics_df = link_tp_vehclass_df.groupby(['timeperiod','vclass']).aggregate(aggregate_dict).reset_index()
 
+# create version with link mapping (e.g. link to TAZ)
+link_tp_vehclass_mapped_df = pandas.DataFrame()
+metrics_mapped_df          = pandas.DataFrame()
+if args.link_mapping:
+  link_tp_vehclass_mapped_df = link_tp_vehclass_df.merge(link_mapping_df, on=['a','b'], how='outer', indicator=True)
+  print("Merged with link_mapping_df to create link_tp_vehclass_mapped_df with {} rows; head:\n{}".format(
+        len(link_tp_vehclass_mapped_df), link_tp_vehclass_mapped_df.head(20)))
+
+  # for aggregating, we'll want to multiply each variable by the share
+  # so if a link is mapped to two indices, with 0.5 in one and 0.5 in the other
+  # then the metrics will be split that way as well
+  share_col = args.link_mapping[2]
+  for metric_name in aggregate_dict.keys():
+    print("  - multiplying {} by share col {}".format(metric_name, share_col))
+    link_tp_vehclass_mapped_df[metric_name] = link_tp_vehclass_mapped_df[metric_name]*link_tp_vehclass_mapped_df[share_col]
+
+  index_col = args.link_mapping[1]
+  # set missing index_col value to -1
+  link_tp_vehclass_mapped_df.fillna(value={index_col:-1}, inplace=True)
+
+  # and aggregate to timeperiod x vclass x indexcol
+  metrics_mapped_df = link_tp_vehclass_mapped_df.groupby(['timeperiod','vclass',index_col]).aggregate(aggregate_dict).reset_index()
+  print("  - aggregated to timeperiod, vclass, {}; head:\n{}".format(index_col, metrics_mapped_df.head(10)))
+
 # collisionlookup in collisions per 1000000 VMT
 for collision_type in collision_types: 
     metrics_df[collision_type] = metrics_df[collision_type]/1000000.0
 
+    # ditto for mapped
+    if len(metrics_mapped_df)>0: metrics_mapped_df[collision_type] = metrics_mapped_df[collision_type]/1000000.0
+
 # emissionlookup in grams per mile (equivalent to metric tons per 1000000 VMT)
-if args.filter!='PBA50':
+if len(emissionslookup_df)>0:
     for emission_type in emission_types:
         metrics_df[emission_type] = metrics_df[emission_type]/1000000.0
 
+        # ditto for mapped
+        if len(metrics_mapped_df)>0: metrics_mapped_df[emission_type] = metrics_mapped_df[emission_type]/1000000.0
+  
 # print(metrics_df.head())
 metrics_df.rename(columns={'vclass':'vehicle class',
                            'vmt':'VMT',
@@ -279,11 +353,29 @@ metrics_df.rename(columns={'vclass':'vehicle class',
                            'hypfft':'Hypothetical Freeflow Time',
                            'nrcdelay':'Non-Recurring Freeway Delay'},
                   inplace=True)
-if args.filter!='PBA50':
-    metrics_df[["timeperiod","vehicle class","VMT","VHT","Hypothetical Freeflow Time", "Non-Recurring Freeway Delay"]+
-               collision_types+emission_types].to_csv(vmt_vht_outputfile, header=True, index=False)
-if args.filter=='PBA50':
-    metrics_df[["timeperiod","vehicle class","VMT","VHT","Hypothetical Freeflow Time", "Non-Recurring Freeway Delay"]+
-               collision_types].to_csv(vmt_vht_outputfile, header=True, index=False)  
+# ditto for mapped
+if len(metrics_mapped_df) > 0:
+  metrics_mapped_df.rename(columns={'vclass':'vehicle class',
+                           'vmt':'VMT',
+                           'vht':'VHT',
+                           'hypfft':'Hypothetical Freeflow Time',
+                           'nrcdelay':'Non-Recurring Freeway Delay'},
+                  inplace=True)
+
+output_cols = ["timeperiod","vehicle class","VMT","VHT","Hypothetical Freeflow Time", "Non-Recurring Freeway Delay"]+collision_types
+if len(emissionslookup_df)>0:
+  output_cols = output_cols + emission_types
+
+metrics_df[output_cols].to_csv(vmt_vht_outputfile, header=True, index=False)
 print("Wrote {}".format(vmt_vht_outputfile))
+
+# write the mapped version aggregated to its index (e.g. TAZ)
+if len(metrics_mapped_df) > 0:
+  index_col     = args.link_mapping[1]
+  output_suffix = args.link_mapping[3]
+  vmt_vht_outputfile = vmt_vht_outputfile.replace(".csv","{}.csv".format(output_suffix))
+
+  output_cols = [index_col] + output_cols
+  metrics_mapped_df[output_cols].to_csv(vmt_vht_outputfile, header=True, index=False)
+  print("Wrote {}".format(vmt_vht_outputfile))
 sys.exit(0)
