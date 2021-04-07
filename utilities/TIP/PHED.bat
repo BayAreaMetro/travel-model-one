495ad97e859d304f02ae0c089f05d41a8adc2fe5
Flavia <flavia.tsang@gmail.com>
2021-04-06 18:46:44 -0700

    use the urbanized area shapefile

--- utilities/TIP/PHED.bat
+++ utilities/TIP/PHED.bat
@@ -28,11 +28,12 @@ copy M:\Application\Model One\RTP2021\Blueprint\INPUT_DEVELOPMENT\metrics\PHED\f
 :: join the free flow links with the file with the city boundaries
 :: ----------------------------------------------------------------
 set PATH=C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3;C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\Scripts
-set SCENARIO_DIR=M:\Application\Model One\RTP2021\Blueprint\2050_TM152_FBP_PlusCrossing_21
 
-cd /d %SCENARIO_DIR%
-python "X:\travel-model-one-master\utilities\cube-to-shapefile\correspond_link_to_TAZ.py" "%SCENARIO_DIR%\INPUT\hwy\forPHED\freeflow_links.shp" "%SCENARIO_DIR%\INPUT\hwy\forPHED\freeflow_links_cities.csv" --shapefile "M:\Development\Travel Model One\Version 05\Adding City to Master Network\Cityshapes\PBA_Cities_NAD_1983_UTM_Zone_10N.shp"  --shp_id name
+python "X:\travel-model-one-master\utilities\cube-to-shapefile\correspond_link_to_TAZ.py" "%SCENARIO_DIR%\INPUT\hwy\forPHED\freeflow_links.shp" "%SCENARIO_DIR%\INPUT\hwy\forPHED\freeflow_links_UA.csv" --shapefile "M:\Application\Model One\RTP2021\Blueprint\INPUT_DEVELOPMENT\metrics\PHED\UZA_select\UZA_select.shp"  --shp_id NAME10
 
 :: run the R script that calculates the PHED metric
 :: ----------------------------------------------------------------
+:: set R location
+set R_HOME=C:\Program Files\R\R-3.5.2
 
+call "%R_HOME%\bin\x64\Rscript.exe" X:\travel-model-one-master\utilities\TIP\federal_metric_PHED_TM1.5.R
