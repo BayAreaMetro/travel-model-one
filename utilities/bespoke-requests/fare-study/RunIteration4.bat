4ba122ac3f03010874398db2feabae0d03f13f6f
Lisa Zorn <lzorn@bayareametro.gov>
2021-08-19 16:28:40 -0700

    Add FCIS modeling scripts

---
+++ utilities/bespoke-requests/fare-study/RunIteration4.bat
@@ -0,0 +1,299 @@
+::
+:: Iteration 4 run for Fare Coordination/Integration Study (FCIS)
+:: See https://github.com/alexmitrani/travel-model-one
+
+
+:: Figure out SCENARIO - this is the folder name
+set MODEL_DIR=%CD%
+:: get the base dir only
+for %%f in (%CD%) do set SCENARIO=%%~nxf
+echo SCENARIO=[%SCENARIO%]
+title %SCENARIO%
+
+:: ------------------------------------------------------------------------------------------------------
+:: Copy model source
+:: ------------------------------------------------------------------------------------------------------
+set GITHUB_DIR=\\tsclient\X\travel-model-one-master
+:: copy over CTRAMP
+mkdir CTRAMP\model
+mkdir CTRAMP\runtime
+mkdir CTRAMP\scripts
+mkdir CTRAMP\scripts\metrics
+c:\windows\system32\Robocopy.exe /E "%GITHUB_DIR%\model-files\model"       CTRAMP\model
+c:\windows\system32\Robocopy.exe /E "%GITHUB_DIR%\model-files\runtime"     CTRAMP\runtime
+c:\windows\system32\Robocopy.exe /E "%GITHUB_DIR%\model-files\scripts"     CTRAMP\scripts
+copy /Y "%GITHUB_DIR%\utilties\bespoke-requests\fare-study\TransitSkims.job" CTRAMP\scripts\skims\TransitSkims.job
+
+:: Set the Baseline (complete three iteration run) that we're pivoting from
+set BASELINE_FULL_RUN=\\MODEL2-B\Model2B-Share\Projects\2015_TM152_IPA_17
+SET MODEL_YEAR=2015
+
+:: ------------------------------------------------------------------------------------------------------
+:: copy required input files
+:: ------------------------------------------------------------------------------------------------------
+mkdir INPUT
+c:\windows\system32\Robocopy.exe /E "%BASELINE_FULL_RUN%\INPUT\landuse"        INPUT\landuse
+c:\windows\system32\Robocopy.exe /E "%BASELINE_FULL_RUN%\INPUT\nonres"         INPUT\nonres
+c:\windows\system32\Robocopy.exe /E "%BASELINE_FULL_RUN%\INPUT\popsyn"         INPUT\popsyn
+c:\windows\system32\Robocopy.exe /E "%BASELINE_FULL_RUN%\INPUT\warmstart"      INPUT\warmstart
+c:\windows\system32\Robocopy.exe /E "%BASELINE_FULL_RUN%\INPUT\hwy"            INPUT\hwy
+c:\windows\system32\Robocopy.exe /E "%BASELINE_FULL_RUN%\INPUT\trn"            INPUT\trn
+copy /Y "%BASELINE_FULL_RUN%\INPUT\params.properties"                          INPUT\params.properties
+
+:: ------------------------------------------------------------------------------------------------------
+:: Create the working directories
+:: ------------------------------------------------------------------------------------------------------
+mkdir hwy
+mkdir trn
+mkdir skims
+mkdir landuse
+mkdir popsyn
+mkdir nonres
+mkdir main
+mkdir logs
+
+:: ------------------------------------------------------------------------------------------------------
+:: Copy the input files to the working directories
+:: ------------------------------------------------------------------------------------------------------
+copy INPUT\hwy\                 hwy\
+copy INPUT\trn\                 trn\
+copy INPUT\landuse\             landuse\
+copy INPUT\popsyn\              popsyn\
+copy INPUT\nonres\              nonres\
+copy INPUT\warmstart\main\      main\
+copy INPUT\warmstart\nonres\    nonres\
+
+::  Set the IP address of the host machine which sends tasks to the client machines 
+if %computername%==MODEL2-A set HOST_IP_ADDRESS=192.168.1.206
+if %computername%==MODEL2-B set HOST_IP_ADDRESS=192.168.1.207
+if %computername%==MODEL2-C set HOST_IP_ADDRESS=192.168.1.208
+if %computername%==MODEL2-D set HOST_IP_ADDRESS=192.168.1.209
+
+:: Set the paths for executables
+call CTRAMP\runtime\SetPath.bat
+
+:: Runtime configuration: set project directory, auto operating cost, 
+:: and synthesized household/population files in the appropriate places
+python CTRAMP\scripts\preprocess\RuntimeConfiguration.py
+if ERRORLEVEL 1 goto done
+
+:: Start the cube cluster
+Cluster "%COMMPATH%\CTRAMP" 1-48 Starthide Exit
+
+:: ------------------------------------------------------------------------------------------------------
+:: copy over iter3 files
+:: ------------------------------------------------------------------------------------------------------
+copy /Y "%BASELINE_FULL_RUN%\hwy\avgload*.net"               hwy
+copy /Y "%BASELINE_FULL_RUN%\main\ShadowPricing_7.csv"       main
+copy /Y "%BASELINE_FULL_RUN%\main\telecommute_constants.csv" main
+copy /Y "%BASELINE_FULL_RUN%\skims\nonmotskm.tpp"            skims
+mkdir trn\TransitAssignment.iter3
+copy /Y "%BASELINE_FULL_RUN%\trn\TransitAssignment.iter3\transit*.lin" trn\TransitAssignment.iter3
+
+:: set to 5 so trnAssign.bat will copy skims for use
+set MAXITERATIONS=5
+
+
+:: --------TrnAssignment Setup -- Standard Configuration
+:: CHAMP has dwell  configured for buses (local and premium)
+:: CHAMP has access configured for for everything
+:: set TRNCONFIG=STANDARD
+:: set COMPLEXMODES_DWELL=21 24 27 28 30 70 80 81 83 84 87 88
+:: set COMPLEXMODES_ACCESS=21 24 27 28 30 70 80 81 83 84 87 88 110 120 130
+
+:: --------TrnAssignment Setup -- Fast Configuration
+:: NOTE the blank ones should have a space
+set TRNCONFIG=FAST
+set COMPLEXMODES_DWELL= 
+set COMPLEXMODES_ACCESS= 
+
+: iter4
+
+echo STARTED ITERATION %ITER%  %DATE% %TIME% >> logs\feedback.rpt 
+
+:: Set the iteration parameters
+set ITER=4
+set PREV_ITER=3
+set WGT=1.00
+set PREV_WGT=0.00
+set SAMPLESHARE=0.50
+set SEED=0
+
+:: Runtime configuration: set the workplace shadow pricing parameters
+python CTRAMP\scripts\preprocess\RuntimeConfiguration.py --iter %ITER%
+if ERRORLEVEL 1 goto done
+
+:skims
+
+:: Create the automobile level-of-service matrices
+runtpp CTRAMP\scripts\skims\HwySkims.job
+if ERRORLEVEL 2 goto done
+
+:trnAssignSkim
+:: copy a local version for easier restarting
+copy CTRAMP\scripts\skims\trnAssign.bat trnAssign_iter%ITER%.bat
+call trnAssign_iter%ITER%.bat
+if ERRORLEVEL 2 goto done
+
+copy trn\TransitAssignment.iter4\trnskm_*.
+
+:: Create accessibility measures for use by the automobile ownership sub-model
+runtpp CTRAMP\scripts\skims\Accessibility.job
+if ERRORLEVEL 2 goto done
+
+
+:: ------------------------------------------------------------------------------------------------------
+::
+:: Step 2:  Execute the choice models using CT-RAMP java code
+::
+:: ------------------------------------------------------------------------------------------------------
+
+:core
+
+if %ITER%==4 (
+  rem run matrix manager, household manager and jppf driver
+  cd CTRAMP\runtime
+  call javaOnly_runMain.cmd 
+
+  rem run jppf node
+  cd CTRAMP\runtime
+  call javaOnly_runNode0.cmd
+)
+
+::  Call the MtcTourBasedModel class
+java -showversion -Xmx6000m -cp %CLASSPATH% -Dlog4j.configuration=log4j.xml -Djava.library.path=%RUNTIME% -Djppf.config=jppf-clientDistributed.properties com.pb.mtc.ctramp.MtcTourBasedModel mtcTourBased -iteration %ITER% -sampleRate %SAMPLESHARE% -sampleSeed %SEED%
+if ERRORLEVEL 2 goto done
+
+
+:: skip the rest
+goto done
+
+:: ------------------------------------------------------------------------------------------------------
+::
+:: Step 3:  Execute the internal/external and commercial vehicle models
+::
+:: ------------------------------------------------------------------------------------------------------
+
+:nonres
+
+:: Create production/attraction tables based on growth assumptions
+runtpp CTRAMP\scripts\nonres\IxForecasts_horizon.job
+if ERRORLEVEL 2 goto done
+
+:: Apply diurnal factors to the fixed internal/external demand matrices
+runtpp CTRAMP\scripts\nonres\IxTimeOfDay.job
+if ERRORLEVEL 2 goto done
+
+:: Apply a value toll choice model for the interna/external demand
+runtpp CTRAMP\scripts\nonres\IxTollChoice.job
+if ERRORLEVEL 2 goto done
+
+:: Apply the commercial vehicle generation models
+runtpp CTRAMP\scripts\nonres\TruckTripGeneration.job
+if ERRORLEVEL 2 goto done
+
+:: Apply the commercial vehicle distribution models
+runtpp CTRAMP\scripts\nonres\TruckTripDistribution.job
+if ERRORLEVEL 2 goto done
+
+:: Apply the commercial vehicle diurnal factors
+runtpp CTRAMP\scripts\nonres\TruckTimeOfDay.job
+if ERRORLEVEL 2 goto done
+
+:: Apply a value toll choice model for eligibile commercial demand
+runtpp CTRAMP\scripts\nonres\TruckTollChoice.job
+if ERRORLEVEL 2 goto done
+
+:: Apply a transit submode choice model for transit trips to bay area HSR stations
+runtpp CTRAMP\scripts\nonres\HsrTransitSubmodeChoice.job
+if ERRORLEVEL 2 goto done
+
+:: ------------------------------------------------------------------------------------------------------
+::
+:: Step 4:  Build matrices from trip lists and assign trips to the highway network
+::
+:: ------------------------------------------------------------------------------------------------------
+
+:hwyAssign
+
+:: If demand models were executed, translate the trip lists to demand matrices
+if %ITER% GTR 0 (
+	runtpp CTRAMP\scripts\assign\PrepAssign.job
+	if ERRORLEVEL 2 goto done
+)
+
+:: Assign the demand matrices to the highway network
+runtpp CTRAMP\scripts\assign\HwyAssign.job
+if ERRORLEVEL 2 goto done
+
+:trnAssignSkim
+:: copy a local version for easier restarting
+copy CTRAMP\scripts\skims\trnAssign.bat trnAssign_iter%ITER%.bat
+call trnAssign_iter%ITER%.bat
+if ERRORLEVEL 2 goto done
+
+:: ------------------------------------------------------------------------------------------------------
+::
+:: Step 5:  Prepare the networks for the next iteration
+::
+:: ------------------------------------------------------------------------------------------------------
+
+:feedback
+
+
+:: Move assigned networks to a iteration-specific directory
+mkdir hwy\iter%ITER%      
+
+move hwy\LOADEA.net hwy\iter%ITER%\LOADEA.net
+move hwy\LOADAM.net hwy\iter%ITER%\LOADAM.net
+move hwy\LOADMD.net hwy\iter%ITER%\LOADMD.net
+move hwy\LOADPM.net hwy\iter%ITER%\LOADPM.net
+move hwy\LOADEV.net hwy\iter%ITER%\LOADEV.net
+
+:: Give the default TP+ variables more intuitive names
+runtpp CTRAMP\scripts\feedback\RenameAssignmentVariables.job
+
+:: Average the demand for this and the previous iteration and compute a speed estimate for each link 
+IF %ITER% GTR 1 (
+	runtpp CTRAMP\scripts\feedback\AverageNetworkVolumes.job
+	if ERRORLEVEL 2 goto done
+	runtpp CTRAMP\scripts\feedback\CalculateSpeeds.job
+	if ERRORLEVEL 2 goto done
+) ELSE (
+	copy hwy\iter%ITER%\LOADEA_renamed.net hwy\iter%ITER%\avgLOADEA.net /Y
+	copy hwy\iter%ITER%\LOADAM_renamed.net hwy\iter%ITER%\avgLOADAM.net /Y
+	copy hwy\iter%ITER%\LOADMD_renamed.net hwy\iter%ITER%\avgLOADMD.net /Y
+	copy hwy\iter%ITER%\LOADPM_renamed.net hwy\iter%ITER%\avgLOADPM.net /Y
+	copy hwy\iter%ITER%\LOADEV_renamed.net hwy\iter%ITER%\avgLOADEV.net /Y
+)
+
+:: Compute network statistics to measure convergence
+runtpp CTRAMP\scripts\feedback\TestNetworkConvergence.job
+if ERRORLEVEL 2 goto done
+
+:: Combine the time-of-day-specific networks into a single network
+runtpp CTRAMP\scripts\feedback\MergeNetworks.job  
+if ERRORLEVEL 2 goto done                
+
+:: Place a copy of the loaded networks into the root \hwy directory for access by the next iteration
+copy hwy\iter%ITER%\avgLOADEA.net hwy\avgLOADEA.net /Y
+copy hwy\iter%ITER%\avgLOADAM.net hwy\avgLOADAM.net /Y
+copy hwy\iter%ITER%\avgLOADMD.net hwy\avgLOADMD.net /Y
+copy hwy\iter%ITER%\avgLOADPM.net hwy\avgLOADPM.net /Y
+copy hwy\iter%ITER%\avgLOADEV.net hwy\avgLOADEV.net /Y
+
+:: Delete temporary files
+del hwy\iter%ITER%\x*.net
+
+
+:: ------------------------------------------------------------------------------------------------------
+::
+:: Last Step:  Stamp the time of completion to the feedback report file
+::
+:: ------------------------------------------------------------------------------------------------------
+
+echo FINISHED ITERATION %ITER%  %DATE% %TIME% >> logs\feedback.rpt 
+
+python "CTRAMP\scripts\notify_slack.py" "Finished iteration %ITER% in %MODEL_DIR%"
+
+:done
