b246315abd97b4fd2e59ddcb81f9c9c60737830a
Flavia Tsang <flavia.tsang@gmail.com>
2022-09-29 15:20:00 -0700

    Move toll calibration scripts to its own folder, and added a readme

---
+++ utilities/toll_calibration/TollCalib_checkITER3.bat
@@ -0,0 +1,37 @@
+:: ------------------------------------------
+:: Set file paths and iteration number, and run the R script that calculates EL and GP speed
+:: 
+:: ------------------------------------------
+
+set ITER=3
+
+:: User input needed here
+:: Location of the base run directory
+:: Either the directory with the full run, or the directory with the extracted outputs
+set PROJECT_DIR=%MODEL_BASE_DIR%
+
+
+:: the rest doesn't require user inputs if TollCalib_setup.bat is run
+:: otherwise user may need to specify the locations of UNLOADED_NETWORK_DBF and TOLL_DESIGNATIONS_XLSX
+
+:: Unloaded network dbf, generated from cube_to_shapefile.py, needed for the R script that determine toll adjustment 
+:: (okay to borrow it from a different Future as long as we're sure the unloaded network is the same across Futures)
+set UNLOADED_NETWORK_DBF=tollcalib_iter\network_links.dbf
+
+:: The file indicating which facilities have mandatory s2 tolls
+set TOLL_DESIGNATIONS_XLSX=tollcalib_iter\TOLLCLASS_Designations.xlsx
+
+:: set R location
+set R_HOME=C:\Program Files\R\R-3.5.2
+
+:: check if it's being run on AWS
+:: if the prefix will be "WIN-"
+SET computer_prefix=%computername:~0,4%
+
+if "%COMPUTER_PREFIX%" == "WIN-" (
+    call "%R_HOME%\bin\x64\Rscript.exe" TollCalib_CheckSpeeds.R
+) else (
+    call "%R_HOME%\bin\x64\Rscript.exe" "\\mainmodel\MainModelShare\travel-model-one-master\utilities\check-network\TollCalib_CheckSpeeds.R"
+)
+
+copy %PROJECT_DIR%\tollcalib_iter\el_gp_summary_ALL.csv tollcalib_iter\el_gp_summary_ALL.csv
---
+++ utilities/toll_calibration/TollCalib_stop.py
@@ -0,0 +1,51 @@
+# TollCalibration.py
+# Script that reads in the average speed by facility of two successive iterations, calculate the sum of square differences of the two
+# Outputs a csv file indicating calibration should stop when 
+
+
+import os
+#from dbfread import DBF
+import csv
+#import copy
+import pandas as pd
+
+print(os.environ['ITER'])
+
+# Iterations
+iter0=int(os.environ['ITER'])-1
+iter1=os.environ['ITER']
+
+# Read files
+
+AvgSpeedIter0_df = pd.read_table(os.path.join(os.getcwd(), "el_gp_avg_speed_iter" + str(iter0) + ".csv"), sep=",")
+AvgSpeedIter1_df = pd.read_table(os.path.join(os.getcwd(), "el_gp_avg_speed_iter" + str(iter1) + ".csv"), sep=",")
+
+# Deleting unnecessary columns
+AvgSpeedIter0_df = AvgSpeedIter0_df.drop(['TOLLCLASS','Case_AM','Case_MD','Case_PM','facility_name','tollam_da','tollmd_da','tollpm_da','tollam_da_new','tollmd_da_new','tollpm_da_new'], 1)
+AvgSpeedIter1_df = AvgSpeedIter1_df.drop(['TOLLCLASS','Case_AM','Case_MD','Case_PM','facility_name','tollam_da','tollmd_da','tollpm_da','tollam_da_new','tollmd_da_new','tollpm_da_new'], 1)
+
+# Subtract one matrix from the other. Square of the resulting matrix
+SSD_df=(AvgSpeedIter0_df.subtract(AvgSpeedIter1_df)).pow(2)
+#print(SSD_df)
+
+# Add all elemnts in the matrix
+Totalmat = SSD_df.values.sum()
+
+count_row = AvgSpeedIter0_df.shape[0]
+
+#NormTotalmat = Totalmat/count_row
+
+s = open("Results.txt","a+")
+s.write("\nSSD for iterations %s - %s = %s" %(iter0,iter1,Totalmat))
+s.close()
+
+# Create the file to stop the script once the Total < 20
+
+if Totalmat <= 20:
+    #s = open("stop.txt","w+")
+    with open('Stop.csv', mode='w') as csvfile:
+        csvwriter = csv.writer(csvfile)
+
+        csvwriter.writerow(['SSD','Status'])
+        csvwriter.writerow([Totalmat, 'Stop'])
+
