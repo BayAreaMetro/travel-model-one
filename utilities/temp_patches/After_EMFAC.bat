d7a1df9388bafd53f34aa7924e2597a0fc6c7094
LufengLIN <llin@bayareametro.gov>
2022-11-02 15:34:36 -0700

    Added emfac_patch and After_EMFAC batch files (work in progress)

---
+++ utilities/temp_patches/After_EMFAC.bat
@@ -0,0 +1,4 @@
+
+cd /d A:\Projects\2035_TM152_NGF_NP02_BPALTsegmented_03_SensDyn00_2
+set M_DIR=L:\Application\Model_One\NextGenFwys\Scenarios\2035_TM152_NGF_NP02_BPALTsegmented_03_SensDyn00_2
+python ctramp/scripts/emfac/emfac_postproc.py SB375
