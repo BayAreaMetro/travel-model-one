53c8566e68e48cc0ed16f1542b779ed18c144274
Flavia Tsang <flavia.tsang@gmail.com>
2022-11-18 00:46:58 -0800

    roadway speed and toll viewer for NGF (could be useful for other future projects too)

--- utilities/cube-to-shapefile/run_CubeToShapefile.bat
+++ utilities/cube-to-shapefile/run_CubeToShapefile.bat
@@ -7,3 +7,5 @@ set PATH=C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3;C:\Program Fi
 set PYTHONPATH=%PYTHONPATH%;C:\Users\%USERNAME%\Documents\GitHub\NetworkWrangler;C:\Users\%USERNAME%\Documents\GitHub\NetworkWrangler\_static
 
 python \\mainmodel\MainModelShare\travel-model-one-master\utilities\cube-to-shapefile\cube_to_shapefile.py  --trn_stop_info "M:\\Application\Model One\\Networks\\TM1_2015_Base_Network\\Node Description.xls" --linefile ..\\..\\INPUT\\trn\\transitLines.lin --loadvol_dir ..\\trn ..\\avgload5period.net --transit_crowding ..\metrics\transit_crowding_complete.csv
+
+copy \\mainmodel\MainModelShare\travel-model-one-master\utilities\cube-to-shapefile\RoadwaySpeedViewer.twb .
