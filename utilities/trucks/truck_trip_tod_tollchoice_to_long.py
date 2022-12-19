f902bdfb5331b86b2c9b23b89fbaecd899a2ed7c
Lisa Zorn <lzorn@bayareametro.gov>
2022-12-19 13:21:17 -0800

    Add distance, time and vtoll from truck skims to TOD/toll choice debug info

--- utilities/trucks/truck_trip_tod_tollchoice_to_long.py
+++ utilities/trucks/truck_trip_tod_tollchoice_to_long.py
@@ -6,7 +6,7 @@ USAGE = r"""
   by moving the truck classes and time periods from columns to rows.
 """
 
-import argparse
+import argparse,sys
 import pandas
 
 if __name__ == '__main__':
@@ -71,6 +71,49 @@ if __name__ == '__main__':
     print("Length: {:,}".format(len(trucks_long_df)))
     trucks_long_df = trucks_long_df.loc[ trucks_long_df["truck trips"] > 0 ]
     print("After filter, length: {:,}".format(len(trucks_long_df)))
+
+    # read skims
+    skim_file = "truck_skims_{}.csv".format(model_run)
+    skims_df = pandas.read_csv(skim_file)
+    skims_df.drop(columns=["one_a","one_b"], inplace=True)
+    print(skims_df.head())
+    print("columns: {}".format(skims_df.columns.values))
+
+    # join to trucks_long_df to fill in time, dist, vtoll
+    trucks_long_df["time"] = 0.00
+    trucks_long_df["dist"] = 0.00
+    trucks_long_df["vtoll"] = 0.00
+    trucks_long_cols = list(trucks_long_df.columns.values)
+    trucks_long_df = pandas.merge(
+        left=trucks_long_df,
+        right=skims_df,
+        on=["orig","dest"]
+    )
+    for time_period in ["EA","AM","MD","PM","EV"]:
+        for truck_class in ["verySmall","small","medium","large"]:
+            for toll_choice in ["toll","noToll"]:
+                skim_col = "{} {} {}".format(toll_choice, truck_class, time_period)
+                # small==verySmall so the skims only have verySmall
+                if truck_class == "small":
+                    skim_col = skim_col.replace("small","verySmall")
+                print("setting skim values for {}".format(skim_col))
+                time_skim_col = "time {}".format(skim_col)
+                trucks_long_df.loc[ (trucks_long_df["time period"]==time_period) & \
+                                    (trucks_long_df["truck class"]==truck_class) & \
+                                    (trucks_long_df["toll choice"]==toll_choice), "time"] = trucks_long_df[time_skim_col]
+                dist_skim_col = "dist {}".format(skim_col)
+                trucks_long_df.loc[ (trucks_long_df["time period"]==time_period) & \
+                                    (trucks_long_df["truck class"]==truck_class) & \
+                                    (trucks_long_df["toll choice"]==toll_choice), "dist"] = trucks_long_df[dist_skim_col]
+                if toll_choice == "noToll": continue
+
+                vtoll_skim_col = "vtoll {}".format(skim_col)
+                trucks_long_df.loc[ (trucks_long_df["time period"]==time_period) & \
+                                    (trucks_long_df["truck class"]==truck_class) & \
+                                    (trucks_long_df["toll choice"]==toll_choice), "vtoll"] = trucks_long_df[vtoll_skim_col]
+
+    print("Dropping extra cols")
+    trucks_long_df = trucks_long_df[trucks_long_cols]
 
     trucks_long_df.to_csv(output_file, index=False)
     print("Wrote {}".format(output_file))
