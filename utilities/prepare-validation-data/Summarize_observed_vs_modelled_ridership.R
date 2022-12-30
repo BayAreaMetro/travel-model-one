87ae7ef697ae08157d896f71d472add5d3d3d40a
Lisa Zorn <lzorn@bayareametro.gov>
2022-12-30 13:02:47 -0800

    Update transit validation script to:
    1) do full_join or left_join rather than inner_join to make sure rows aren't dropped unexpectedly
    2) keep more detail (e.g. operator from consolidated survey) in the final summary
    3) replace values instead of dropping/creating new dataframe/rbinding
    
    Also update transit validation tableau to work with this dataset

--- utilities/prepare-validation-data/Summarize_observed_vs_modelled_ridership.R
+++ utilities/prepare-validation-data/Summarize_observed_vs_modelled_ridership.R
@@ -16,7 +16,7 @@ library(dplyr)
 ##################
 
 # users to specify the project directory:
-PROJECT_DIR = "L:/Application/Model_One/NextGenFwys/2015_TM152_NGF_04"
+PROJECT_DIR = "L:/Application/Model_One/TransitRecovery/2015_TM152_TRR_00"
 
 # typically, users do not need to specify other input paths, as they do not change across model runs
 # onboard survey data
@@ -35,16 +35,24 @@ TransitMode_OperatorGroups_CSV    <- "X:/travel-model-one-master/utilities/prepa
 ##################
 # Analyse observed data
 ##################
+# Loads a data.frame named TPS with detailed Transit Passenger Survey data:
+# operator, surey_year, SURVEY_MODE, access_mode, depart_hour, etc.
+# These are individual survey records so it is large: 130k rows
 load(F_INPUT_CONSOLIDATED_RDATA)
+print(paste("TPS has",nrow(TPS),"rows with final_boardWeight_2015 total",sum(TPS$final_boardWeight_2015)))
 
-ConsolidatedDB_OperatorGroups_df     <-  read.csv(ConsolidatedDB_OperatorGroups_CSV, header=TRUE, sep=",")
+# columns: operator_in_consolidated_database, operator_in_legacy_database, technology_6groups, operator_group
+ConsolidatedDB_OperatorGroups_df <- read.csv(ConsolidatedDB_OperatorGroups_CSV, header=TRUE, sep=",")
 
-TPS_df <- TPS %>% inner_join(ConsolidatedDB_OperatorGroups_df,
-                  by=c("operator"="operator_in_consolidated_database"))
+# join TPS to add technology_6groups, operator_group
+TPS_df <- left_join(TPS, ConsolidatedDB_OperatorGroups_df,
+                    by=c("operator"="operator_in_consolidated_database"))
 
 Observed_ByOperatorTech_df <- TPS_df %>%
-   group_by(operator_group, technology_6groups) %>%
-   summarise(observed_boardings = sum(final_boardWeight_2015))
+   group_by(operator, operator_group, technology_6groups) %>%
+   summarise(observed_boardings = sum(final_boardWeight_2015), .groups="keep")
+print(paste("Observed_ByOperatorTech_df has",nrow(Observed_ByOperatorTech_df),
+   "rows with final_boardWeight_2015 total",sum(Observed_ByOperatorTech_df$observed_boardings)))
 
 # The "technology" variable in the consolidated database was not useful. Somehow there were lots of NA.
 # For example, technology was NA for both SamTrans [EXPRESS] and SamTrans [LOCAL]
@@ -59,13 +67,12 @@ Observed_ByOperatorTech_df <- TPS_df %>%
 # We contacted SamTrans about the KX and they us provided with average daily ridership data for the KX in 2015. The data was collected from GFI.
 # The figure below represent the average daily riders on the KX in Oct 2015
 
-# drop the SamTrans express bus from the observed data
-Observed_ByOperatorTech_df     <- filter(Observed_ByOperatorTech_df , operator_group!="SamTrans" | technology_6groups!="express bus")
+# overwrite the SamTrans express bus observed_boardings value
+Observed_ByOperatorTech_df$observed_boardings[ 
+   (Observed_ByOperatorTech_df$operator_group=="SamTrans") &
+   (Observed_ByOperatorTech_df$technology_6groups=="express bus")
+] <- 146
 
-# create a new data frame (which has just one row)
-SamTrans_patch_df <- data.frame(operator_group=c("SamTrans"),
-                                technology_6groups=c("express bus"),
-                                observed_boardings=c(146))
 
 ##################
 # AC Transit Observed Data Patch
@@ -84,39 +91,59 @@ SamTrans_patch_df <- data.frame(operator_group=c("SamTrans"),
 # Local bus is defined as all other labels, although school routes are excluded.
 # The total in "Ending December 2015 Service Statistics.xls" is 170,499.
 
-# drop the AC Transit express and local bus from the observed data
-Observed_ByOperatorTech_df     <- filter(Observed_ByOperatorTech_df , operator_group!="AC Transit")
+# overwrite the AC Transit local bus and express bus with observed_boardings value
+Observed_ByOperatorTech_df$observed_boardings[ 
+   (Observed_ByOperatorTech_df$operator_group=="AC Transit") &
+   (Observed_ByOperatorTech_df$technology_6groups=="local bus")
+] <- 170499
 
-# create a new data frame (with two rows)
-ACT_patch_df <- data.frame(operator_group=c("AC Transit","AC Transit"),
-                           technology_6groups=c("local bus","express bus"),
-                           observed_boardings=c(170499,19722))
-
-# append the new data frame
-Observed_ByOperatorTech_df <- rbind(Observed_ByOperatorTech_df, SamTrans_patch_df, ACT_patch_df)
+# overwrite the AC Transit local bus and express bus with observed_boardings value
+Observed_ByOperatorTech_df$observed_boardings[ 
+   (Observed_ByOperatorTech_df$operator_group=="AC Transit") &
+   (Observed_ByOperatorTech_df$technology_6groups=="express bus")
+] <- 19722
 
 # sort the data frame
-Observed_ByOperatorTech_df <- Observed_ByOperatorTech_df[order(Observed_ByOperatorTech_df$operator_group,Observed_ByOperatorTech_df$technology_6groups),]
+Observed_ByOperatorTech_df <- arrange(
+   Observed_ByOperatorTech_df,
+   operator_group, 
+   technology_6groups)
 
 ##################
 # Analyse modeled data
 ##################
-trnline_df     <-  read.csv(trnline_CSV, header=TRUE, sep=",")
+trnline_df <-  read.csv(trnline_CSV, header=TRUE, sep=",")
 
-TransitMode_OperatorGroups_df     <-  read.csv(TransitMode_OperatorGroups_CSV, header=TRUE, sep=",")
+# columns: mode_code, operator_in_legacy_database, operator_in_consolidated_database, technology_6groups
+# Note that a number of these have operator_in_consolidated_database = "NA" because they weren't surveyed in
+# that database
+TransitMode_OperatorGroups_df <- read.csv(TransitMode_OperatorGroups_CSV, header=TRUE, sep=",")
 
-trnline_df <- trnline_df %>% inner_join(TransitMode_OperatorGroups_df,
-              by=c("mode"="mode_code"))
+# add operator_in_legacy_database, operator_in_consolidated_database, technology_6groups,operator_group to trnline_df
+trnline_df <- left_join(
+   trnline_df, TransitMode_OperatorGroups_df,
+   by=c("mode"="mode_code"))
+
+# report which modelled operators are NA
+print("trnline_df with NA operator_in_consolidated_database:")
+Modelled_Lines_Not_In_Observed <- filter(trnline_df, is.na(operator_in_consolidated_database))
+print(table(Modelled_Lines_Not_In_Observed$mode_name))
 
 Modelled_ByOperatorTech_df <- trnline_df %>%
-   group_by(operator_group, technology_6groups) %>%
-   summarise(modelled_boardings = sum(total.boardings))
+   group_by(operator_in_consolidated_database, operator_group, technology_6groups) %>%
+   summarise(modelled_boardings = sum(total.boardings), .groups="keep")
 
 ##################
 # Compare observed and modelled side-by-side  
 ##################
-compare_df <- Observed_ByOperatorTech_df %>% inner_join(Modelled_ByOperatorTech_df,
-              by=c("operator_group"="operator_group","technology_6groups"="technology_6groups"))
+compare_df <- full_join(
+   Observed_ByOperatorTech_df,
+   Modelled_ByOperatorTech_df,
+   by=c(
+      "operator"="operator_in_consolidated_database",
+      "operator_group",
+      "technology_6groups")
+)
 
 # output the observed vs modelled table
 write.csv(compare_df, file.path(PROJECT_DIR, "OUTPUT", "validation", "transit", "ridership_observed_vs_modelled.csv"), row.names = FALSE)
