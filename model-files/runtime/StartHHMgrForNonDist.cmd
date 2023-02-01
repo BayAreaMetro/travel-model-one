38b142f26cc7a73278d699d8028c3b3c98a917a4
Raghu Sidharthan <r.sidharthan@wsp.com>
2023-02-01 09:37:56 -0800

    remove redundant from script

--- model-files/runtime/StartHHMgrForNonDist.cmd
+++ model-files/runtime/StartHHMgrForNonDist.cmd
@@ -6,10 +6,4 @@ rem ############  PARAMETERS  ############
 :: Set the path
 call CTRAMP\runtime\SetPath.bat
 
-rem ############  PARAMETERS  ############
-set RUNTIME=.
-set JAVA_PATH=C:/Progra~1/Java/jdk1.8.0_162
-set HOST_IP= localhost
-
-
 start java -Xmx20g -Xmx30g -Dlog4j.configuration=log4j_hh.xml com.pb.mtc.ctramp.MtcHouseholdDataManager -hostname localhost
