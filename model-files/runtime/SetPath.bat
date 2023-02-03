a39c6973d42a68cb7124852b7e9555ad8cf86310
Raghu Sidharthan <r.sidharthan@wsp.com>
2023-02-03 14:09:24 -0800

    SetPath locations for VDI

--- model-files/runtime/SetPath.bat
+++ model-files/runtime/SetPath.bat
@@ -2,7 +2,7 @@
 :: Utility to set the path.  Used in RunModel as well as RunMain and RunNodeX. 
 
 :: The commpath
-SET COMMPATH=D:\Projects\BCM\COMMPATH
+SET COMMPATH=Z:\projects\ccta\31000190\R\COMMPATH
 if "%COMPUTER_PREFIX%" == "WIN-" (  SET COMMPATH=D:\COMMPATH)
 if %computername%==MODEL2-A      (  set COMMPATH=E:\Model2A-Share\COMMPATH)
 if %computername%==MODEL2-B      (  set COMMPATH=E:\Model2B-Share\COMMPATH)
@@ -10,26 +10,26 @@ if %computername%==MODEL2-C      (  set COMMPATH=E:\Model2C-Share\COMMPATH)
 if %computername%==MODEL2-D      (  set COMMPATH=E:\Model2D-Share\COMMPATH)
 
 :: The location of the 64-bit java development kit
-set JAVA_PATH=C:\Program Files\Java\jdk1.8.0_121
+set JAVA_PATH=C:\Program Files\Java\jdk1.8.0_162
 
 :: The location of the GAWK binary executable files
-set GAWK_PATH=C:\Software\GnuWin32
+set GAWK_PATH=Z:\projects\ccta\31000190\R\GnuWin32
 if "%COMPUTER_PREFIX%" == "WIN-" (
-  set GAWK_PATH=C:\Software\GnuWin32
+  set GAWK_PATH=Z:\projects\ccta\31000190\R\GnuWin32
 )
 
 :: The location of R and R libraries
-set R_HOME=C:\Program Files\R\R-4.1.2\bin\x64
-set R_LIB=C:\Users\USJH706661\Documents\R\win-library\4.1
+set R_HOME=Z:\projects\ccta\31000190\R\R-4.0.4\bin\x64
+set R_LIB=Z:\projects\ccta\31000190\R\4.0
 if "%COMPUTER_PREFIX%" == "WIN-" (
-  set R_LIB=C:\Users\USJH706661\Documents\R\win-library\4.1
+  set R_LIB=Z:\projects\ccta\31000190\R\4.0
 )
 
 :: The location of the RUNTPP executable from Citilabs
-set TPP_PATH=C:\Program Files\Citilabs\CubeVoyager;C:\Program Files\Citilabs\VoyagerFileAPI
+set TPP_PATH=C:\Program Files\Citilabs\VoyagerFileAPI
 
 :: The location of python
-set PYTHON_PATH=D:\Projects\py27
+set PYTHON_PATH=Z:\projects\ccta\31000190\R\user_py27
 
 :: The location of the MTC.JAR file
 set RUNTIME=CTRAMP/runtime
