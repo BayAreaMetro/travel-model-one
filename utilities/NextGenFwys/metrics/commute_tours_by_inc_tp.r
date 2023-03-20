f1d465daf7453e39c55f212ab56b78c56379b48d
Flavia Tsang <flavia.tsang@gmail.com>
2023-03-20 16:24:29 -0700

    adding an additional core summaries output

--- utilities/NextGenFwys/metrics/commute_tours_by_inc_tp.r
+++ utilities/NextGenFwys/metrics/commute_tours_by_inc_tp.r
@@ -1,11 +1,13 @@
-# to do:  incorporate this in the model run workflow
+# this script generates an output that is similar to the core summary output of CommuteByIncomeHousehold.csv, except that time period information is added
+# ie. commute characteristics by household location. Sum(freq) = commute tours
+# Input : updated_output\commute_tours.rdata
+# Output: core_summaries\CommuteByIncomeByTPHousehold.csv
 
 
 library(dplyr)
 
-#TARGET_DIR   <- Sys.getenv("TARGET_DIR")  # The location of the input files
-#TARGET_DIR   <- gsub("\\\\","/",TARGET_DIR) # switch slashes around
-TARGET_DIR <- "//MODEL2-D/Model2D-Share/Projects/2035_TM152_NGF_NP07_Path1b_01_LowestTolls03"
+TARGET_DIR   <- Sys.getenv("TARGET_DIR")  # The location of the input files
+TARGET_DIR   <- gsub("\\\\","/",TARGET_DIR) # switch slashes around
 UPDATED_DIR <- file.path(TARGET_DIR,"updated_output")
 RESULTS_DIR <- file.path(TARGET_DIR,"core_summaries")
 
