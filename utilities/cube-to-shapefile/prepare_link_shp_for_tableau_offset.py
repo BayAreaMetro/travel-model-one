4e64e7bd783a1ddcf6d75f6552bc8c011c0e25b2
yuqi <ywang@bayareametro.gov>
2023-04-05 17:33:46 -0700

    add logging and QAQC info

--- utilities/cube-to-shapefile/prepare_link_shp_for_tableau_offset.py
+++ utilities/cube-to-shapefile/prepare_link_shp_for_tableau_offset.py
@@ -14,9 +14,13 @@ Returns:
 """
 
 import geopandas as gpd
-import argparse, os
+import argparse, os, logging, time
+from datetime import datetime
 
 TARGET_CRS = 4326
+
+# get date for the logging file
+today = time.strftime("%Y_%m_%d")
 
 
 def linestring_to_points(line):
@@ -40,20 +44,42 @@ if __name__ == '__main__':
     FILE_DIR = args.folder_dir
     # OUTPUT
     LINK_WITH_XY_FILE = RAW_LINK_FILE.split('.shp')[0] + '_withXY.shp'
+    LOG_FILE = os.path.join(FILE_DIR, "prepare_link_shp_for_tableau_offset_{}.log".format(today))
+
+    # set up logging
+    # create logger
+    logger = logging.getLogger(__name__)
+    logger.setLevel('DEBUG')
+
+    # console handler
+    ch = logging.StreamHandler()
+    ch.setLevel('INFO')
+    ch.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p'))
+    logger.addHandler(ch)
+    # file handler
+    fh = logging.FileHandler(LOG_FILE, mode='w')
+    fh.setLevel('DEBUG')
+    fh.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p'))
+    logger.addHandler(fh)
 
     # load the data
+    logger.info('start loading the network data')
     raw_link_gdf = gpd.read_file(os.path.join(FILE_DIR, RAW_LINK_FILE))
-    print('raw data crs: {}'.format(raw_link_gdf.crs))
-    print('data has {:,} rows'.format(raw_link_gdf.shape[0]))
-    print(raw_link_gdf.head())
+    logger.info('finished loading the network data')
+    logger.info('raw data crs: {}'.format(raw_link_gdf.crs))
+    logger.info('data has {:,} rows'.format(raw_link_gdf.shape[0]))
+    logger.info(raw_link_gdf.head())
 
     # convert to 4326
+    logger.info('converting to crs 4326')
     link_gdf = raw_link_gdf.to_crs(TARGET_CRS)
 
-    # extract points from lingstring
+    # extract points from linestring
+    logger.info('extracting points from linestring')
     link_gdf['points'] = link_gdf.apply(lambda l: linestring_to_points(l), axis=1)
 
     # extract X, Y of A/B nodes
+    logger.info('adding X Y')
     link_gdf['A_point'] = link_gdf['points'].apply(lambda x: x[0])
     link_gdf['B_point'] = link_gdf['points'].apply(lambda x: x[1])
     link_gdf['A_X'] = link_gdf['A_point'].apply(lambda x: x[0])
@@ -61,10 +87,22 @@ if __name__ == '__main__':
     link_gdf['B_X'] = link_gdf['B_point'].apply(lambda x: x[0])
     link_gdf['B_Y'] = link_gdf['B_point'].apply(lambda x: x[1])
     # double check
-    print(link_gdf[['A', 'B', 'geometry', 'A_point', 'B_point', 'A_X', 'A_Y', 'B_X', 'B_Y']].head())
+    logger.info(link_gdf[['A', 'B', 'geometry', 'A_point', 'B_point', 'A_X', 'A_Y', 'B_X', 'B_Y']].head())
+
+    # check column types
+    coltypes_before = raw_link_gdf.dtypes
+    coltypes_after = link_gdf.dtypes
+    for colname in list(raw_link_gdf):
+        logger.debug('{}: {} --> {}'.format(colname, coltypes_before[colname], coltypes_after[colname]))
     
+    # check memory usage
+    logger.debug('memory usage of old file: {}'.format(raw_link_gdf.memory_usage(index=True).sum()))
+    logger.debug('memory usage of new file: {}'.format(link_gdf.memory_usage(index=True).sum()))
+    logger.debug('memory usage of new columns :{}'.format(link_gdf[['A_X', 'A_Y', 'B_X', 'B_Y']].memory_usage(index=True).sum()))
+
     # export
+    logger.info('Start writing out')
     link_gdf[list(raw_link_gdf) + ['A_X', 'A_Y', 'B_X', 'B_Y']].to_file(
         os.path.join(FILE_DIR, LINK_WITH_XY_FILE)
     )
-    print('wrote {} to {}'.format(LINK_WITH_XY_FILE, FILE_DIR) )
+    logger.info('wrote {} to {}'.format(LINK_WITH_XY_FILE, FILE_DIR) )
