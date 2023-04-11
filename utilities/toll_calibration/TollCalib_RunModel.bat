c4f2747469b5c13fab829c29d10aaedbcb377a32
LufengLIN <llflinlufeng@gmail.com>
2023-04-11 09:08:42 -0700

    add configurations for model3-c and model3-d

--- utilities/toll_calibration/TollCalib_RunModel.bat
+++ utilities/toll_calibration/TollCalib_RunModel.bat
@@ -294,6 +294,8 @@ if %computername%==MODEL2-C set HOST_IP_ADDRESS=192.168.1.208
 if %computername%==MODEL2-D set HOST_IP_ADDRESS=192.168.1.209
 if %computername%==MODEL3-A set HOST_IP_ADDRESS=10.164.0.200
 if %computername%==MODEL3-B set HOST_IP_ADDRESS=10.164.0.201
+if %computername%==MODEL3-C set HOST_IP_ADDRESS=10.164.0.202
+if %computername%==MODEL3-D set HOST_IP_ADDRESS=10.164.0.203
 if %computername%==PORMDLPPW01 set HOST_IP_ADDRESS=172.24.0.101
 if %computername%==PORMDLPPW02 set HOST_IP_ADDRESS=172.24.0.102
 if %computername%==MAINMODEL set HOST_IP_ADDRESS=192.168.1.200
