4f0933c9839ae543a754be02610f429c069415bf
Flavia Tsang <flavia.tsang@gmail.com>
2023-05-04 10:59:40 -0700

    add "if ERRORLEVEL 1 goto done" so it's more obvious when a script fails (even tho this is a v. short batch file)

--- utilities/cube-to-shapefile/run_CubeToShapefile.bat
+++ utilities/cube-to-shapefile/run_CubeToShapefile.bat
@@ -9,7 +9,7 @@ set PATH=C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3;C:\Program Fi
 set PYTHONPATH=%PYTHONPATH%;C:\Users\%USERNAME%\Documents\GitHub\NetworkWrangler;C:\Users\%USERNAME%\Documents\GitHub\NetworkWrangler\_static
 
 call python \\mainmodel\MainModelShare\travel-model-one-master\utilities\cube-to-shapefile\cube_to_shapefile.py  --trn_stop_info "M:\\Application\Model One\\Networks\\TM1_2015_Base_Network\\Node Description.xls" --linefile ..\\..\\INPUT\\trn\\transitLines.lin --loadvol_dir ..\\trn ..\\avgload5period.net --transit_crowding ..\metrics\transit_crowding_complete.csv
-
+if ERRORLEVEL 1 goto done
 
 REM Switch to an environment that has geopandas
 REM After that, prepare_link_shp_for_tableau_offset.py takes about 20 minutes to run
@@ -18,5 +18,8 @@ if %username%==mtcpb  (call activate geo_env)
 if %username%==ftsang (call activate geo_env)
 
 call python \\mainmodel\MainModelShare\travel-model-one-master\utilities\cube-to-shapefile\prepare_link_shp_for_tableau_offset.py . network_links.shp
+if ERRORLEVEL 1 goto done
 
 copy \\mainmodel\MainModelShare\travel-model-one-master\utilities\cube-to-shapefile\RoadwaySpeedViewer.twb .
+
+:done
