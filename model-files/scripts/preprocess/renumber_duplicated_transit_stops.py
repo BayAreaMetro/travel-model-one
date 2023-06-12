106a39bb63639660bb8cc78443165fc10538f4d3
Jawad Mahmud Hoque <35570340+jawadmhoque@users.noreply.github.com>
2023-06-11 19:30:51 -0700

    Update script to renumber first node to a stop

--- model-files/scripts/preprocess/renumber_duplicated_transit_stops.py
+++ model-files/scripts/preprocess/renumber_duplicated_transit_stops.py
@@ -14,10 +14,31 @@ net.parseFile(fullfile="transitUpdated.lin",insert_replace=True)
 
 for lineidx in xrange(len(net.lines)-1, -1, -1):
     if not isinstance(net.lines[lineidx],TransitLine): continue
+    
+    _stop_to_idx = {}
+    _stop_list   = []
+    all_nodes=net.lines[lineidx].listNodeIds(ignoreStops=False)
+    WranglerLogger.info("Updated the first node {} on Line: {} to be a stop".format(_stop_list[all_nodes[0]],net.lines[lineidx].name))
+    _stop_list.append(np.abs(all_nodes[0]))
+    for node in all_nodes[1:]:
+        node_num=node             
+        if node_num not in _stop_to_idx: 
+            _stop_to_idx[node_num] = []
+        _stop_to_idx[node_num].append(len(_stop_list))
+        if np.int(node_num)>0:
+            _stop_list.append(np.int(node_num))
+        else:
+            if np.abs(np.int(node_num)) not in _stop_list:
+                _stop_list.append(np.int(node_num))
+                
+
+    net.lines[lineidx].setNodes(_stop_list)
+
     if net.lines[lineidx].hasDuplicateStops():
         _stop_to_idx = {}
         _stop_list   = []
         all_nodes=net.lines[lineidx].listNodeIds(ignoreStops=False)
+        # _stop_list.append(np.abs(all_nodes[0]))
         for node in all_nodes:
             node_num=node             
             if node_num not in _stop_to_idx: 
