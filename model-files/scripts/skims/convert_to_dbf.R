b200b237d1d8b70a8ea829bb8c95ad5946594427
Jawad Mahmud Hoque <35570340+jawadmhoque@users.noreply.github.com>
2023-06-20 23:30:14 -0400

    Update convert_to_dbf.R

--- model-files/scripts/skims/convert_to_dbf.R
+++ model-files/scripts/skims/convert_to_dbf.R
@@ -15,14 +15,22 @@ for (tp in time_period_list){
   routelink_dbf <- paste('trnlink', tp, '_ALLMSA.dbf', sep='')
   linksum_dbf <- paste('trnlink', tp, '_ALLMSA_linksum.dbf', sep='')
   
-  routeLink_csv = read.csv(paste(input_dir, routelink_filename, sep=''))
+  if (file.exists(paste(input_dir, routelink_filename, sep=''))){
+    
+    routeLink_csv = read.csv(paste(input_dir, routelink_filename, sep=''))
+    write.dbf(routeLink_csv, paste(input_dir, routelink_dbf, sep=''))
+    
+  } else {
+      print ("MSA File does not exist")
+    }
   
-  #linksum_csv = read.csv(paste(input_dir,sub_dir, linksum_filename), sep='//')
-  
-  write.dbf(routeLink_csv, paste(input_dir, routelink_dbf, sep=''))
-  #write.dbf(linksum_csv, paste(input_dir,sub_dir, linksum_dbf), sep='//')
-  
-  print(paste(routeLink_csv,' File converted to dbf', sep=""))
-  #print(paste(linksum_dbf,' File converted to dbf', sep=""))
+  if (file.exists(paste(input_dir, linksum_filename, sep=''))){
+    
+    linksum_csv = read.csv(paste(input_dir, linksum_filename, sep=''))
+    write.dbf(linksum_csv, paste(input_dir, linksum_filename, sep=''))
+    
+  }else {
+    print ("Linksum File does not exist")
+  }
   
 }
