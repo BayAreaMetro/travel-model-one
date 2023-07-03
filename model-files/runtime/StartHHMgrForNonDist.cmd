31429e471da481c9f4b794525d5568104d53647e
Raghu Sidharthan <sidharthanr@pbworld.com>
2023-07-03 11:55:23 -0700

    sleep 8 secs to get started

--- model-files/runtime/StartHHMgrForNonDist.cmd
+++ model-files/runtime/StartHHMgrForNonDist.cmd
@@ -6,4 +6,7 @@ rem ############  PARAMETERS  ############
 :: Set the path
 call CTRAMP\runtime\SetPath.bat
 
+echo Started: %date% %time%
 start java -Xmx20g -Xmx30g -Dlog4j.configuration=log4j_hh.xml com.pb.mtc.ctramp.MtcHouseholdDataManager -hostname localhost
+timeout /t 8
+echo Waited: %date% %time%
