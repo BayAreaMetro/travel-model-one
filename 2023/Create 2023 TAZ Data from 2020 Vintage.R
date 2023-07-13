99f3a97e60fb47d0d32ee92d669c458e1694cf29
Shimon Israel <sisrael@bayareametro.gov>
2023-07-12 17:53:33 -0700

    Updated script to output 2023 dataset
    Also updated employment scaling

--- 2023/Create 2023 TAZ Data from 2020 Vintage.R
+++ 2023/Create 2023 TAZ Data from 2020 Vintage.R
@@ -14,49 +14,75 @@ setwd(wd)
 suppressMessages(library(tidyverse))
 library(readxl)
 
-# Set up directories, import TAZ/census block equivalence, install census key, set ACS year,set CPI inflation
-
-employment_2015_data           <- "M:/Data/BusinessData/Employment_by_TAZ_industry/BusinessData_2015_TAZ_industry_noincommute.csv"
+# Set up directories
 
 USERPROFILE          <- gsub("\\\\","/", Sys.getenv("USERPROFILE"))
 PETRALE              <- file.path(USERPROFILE,"Documents","GitHub","Petrale","Applications","travel_model_lu_inputs")
 
 # Bring in TAZ 2020 dataset, dataframe is named "final_2020"
 
-load(file.path(Petrale,"2020","TAZ Land Use File 2020.rdata"))
+load(file.path(PETRALE,"2020","TAZ Land Use File 2020.rdata"))
 
-# Bring in California Department of Finance 2023/2020 scaling values for counties
+# Bring in California Department of Finance 2023/2020 county scaling values for population/hh-based variables
 
-DOF_scaling <- read_excel(file.path(Petrale,"2023","P2A_County_Total.xlsx"),sheet = "2023_2020 Ratio")
+DOF_scaling <- read_excel(file.path(PETRALE,"2023","P2A_County_Total.xlsx"),sheet = "2023_2020 Ratio")
 
-# Select out scaling vars, apply scaling factors and round values
+# Use total employment from 2023/2020 to scale by county for employment-related variables
 
-scaling_vars <- final_2020 %>% select("ZONE", "DISTRICT", "SD", "COUNTY", "County_Name",
-   "HSENROLL", "COLLFTE", "COLLPTE",  "TOTHH", "HHPOP", "EMPRES", "HHINCQ1", "HHINCQ2", 
-  "HHINCQ3", "HHINCQ4", "AGE0004", "AGE0519", "AGE2044", "AGE4564", 
-  "AGE65P", "SFDU", "MFDU", "hh_size_1", "hh_size_2", "hh_size_3", 
-  "hh_size_4_plus", "hh_wrks_0", "hh_wrks_1", "hh_wrks_2", "hh_wrks_3_plus", 
-  "hh_kids_yes", "hh_kids_no", "AGE62P", "gq_type_univ", "gq_type_mil", 
-  "gq_type_othnon", "gqpop", "pers_occ_management", "pers_occ_professional", 
-  "pers_occ_services", "pers_occ_retail", "pers_occ_manual", "pers_occ_military", 
-  "white_nonh", "black_nonh", "asian_nonh", "other_nonh", "hispanic", 
-   "TOTPOP", "emp_sec11", "emp_sec21", 
-  "emp_sec22", "emp_sec23", "emp_sec3133", "emp_sec42", "emp_sec4445", 
-  "emp_sec4849", "emp_sec51", "emp_sec52", "emp_sec53", "emp_sec54", 
-  "emp_sec55", "emp_sec56", "emp_sec61", "emp_sec62", "emp_sec71", 
-  "emp_sec72", "emp_sec81", "emp_sec92", "emp_sec99", "TOTEMP", 
-  "AGREMPN", "FPSEMPN", "HEREMPN", "MWTEMPN", "OTHEMPN", "RETEMPN")
+employment_2023        <- read.csv(file.path(PETRALE,"2023","employment_2020_with_QCEW_pct_change_applied.csv"),header = T) 
+employment_2023_joiner <- employment_2023 %>% select(TAZ1454,TOTEMP_2023=TOTEMP)
+
+employment_2020      <- read.csv(file.path(PETRALE,"2020","Employment","lodes_wac_employment.csv"),header = T) %>% 
+  select(TAZ1454,TOTEMP_2020=TOTEMP)
+
+county_joiner<- final_2020 %>% select(ZONE,County_Name)
+
+employment_scaling <- left_join(employment_2020,employment_2023_joiner,by="TAZ1454") %>% 
+  left_join(.,county_joiner,by=c("TAZ1454"="ZONE")) %>% 
+  group_by(County_Name) %>% 
+  summarize(TOTEMP_2020=sum(TOTEMP_2020),TOTEMP_2023=sum(TOTEMP_2023)) %>% 
+  mutate(Ratio_2023_2020=TOTEMP_2023/TOTEMP_2020) %>% 
+  ungroup
+
+
+# Select out population/housing scaling vars (scaled by population change), 
+# employment scaling vars (scaled by employment change), and non-scaled vars
+# apply respective scaling factors to relevant columns and round values
+
+pop_scaling_vars <- final_2020 %>% select("ZONE", "DISTRICT", "SD", "COUNTY", "County_Name",
+                                          "TOTHH", "HHPOP", "HHINCQ1", "HHINCQ2","HHINCQ3", 
+                                          "HHINCQ4", "AGE0004", "AGE0519", "AGE2044", "AGE4564", 
+                                          "AGE65P", "SFDU", "MFDU", "hh_size_1", "hh_size_2", "hh_size_3", 
+                                          "hh_size_4_plus", "hh_wrks_0", "hh_wrks_1", "hh_wrks_2", "hh_wrks_3_plus", 
+                                          "hh_kids_yes", "hh_kids_no", "AGE62P", "gq_type_univ", "gq_type_mil", 
+                                          "gq_type_othnon", "gqpop", "white_nonh", "black_nonh", "asian_nonh", 
+                                          "other_nonh", "hispanic", "TOTPOP")
+
+emp_scaling_vars <- final_2020 %>% select("ZONE", "DISTRICT", "SD", "COUNTY", "County_Name","EMPRES",  
+                                          "pers_occ_management", "pers_occ_professional", "pers_occ_services", 
+                                          "pers_occ_retail", "pers_occ_manual", "pers_occ_military")
 
 non_scaling_vars <- final_2020 %>% select("ZONE", "DISTRICT", "SD", "COUNTY", "County_Name",
   "TOTACRE", "RESACRE", "CIACRE", "PRKCST", "OPRKCST", "AREATYPE","TOPOLOGY", "TERMINAL", "ZERO", 
-  "SHPOP62P")
+  "SHPOP62P","HSENROLL", "COLLFTE", "COLLPTE")
 
-scaling_vars_updated <- left_join(scaling_vars,DOF_scaling[,c("County_Name","Ratio_2023_2020")],by=c("County_Name")) %>% 
-  mutate_at(c(6:77),~.*Ratio_2023_2020) %>% 
+pop_scaling_vars_updated <- left_join(pop_scaling_vars,DOF_scaling[,c("County_Name","Ratio_2023_2020")],by=c("County_Name")) %>% 
+  mutate_at(c(6:39),~.*Ratio_2023_2020) %>% 
   select(-Ratio_2023_2020) %>% 
-  mutate_at(c(6:77),~round(.,0))
+  mutate_at(c(6:39),~round(.,0))
 
- 
+emp_scaling_vars_updated <- left_join(emp_scaling_vars,employment_scaling[,c("County_Name","Ratio_2023_2020")],by=c("County_Name")) %>% 
+  mutate_at(c(6:12),~.*Ratio_2023_2020) %>% 
+  select(-Ratio_2023_2020) %>% 
+  mutate_at(c(6:12),~round(.,0))
+
+# Join everything back together and append 2023 employment file too
+
+scaling_vars_updated <- left_join(pop_scaling_vars_updated,emp_scaling_vars,
+                                  by=c("ZONE", "DISTRICT", "SD", "COUNTY", "County_Name")) %>% 
+                        left_join(.,non_scaling_vars,by=c("ZONE", "DISTRICT", "SD", "COUNTY", "County_Name")) %>% 
+                        left_join(.,employment_2023,by=c("ZONE"="TAZ1454"))
+  
 # Now make fix small variations due to rounding for precisely-matching totals
 # Find max value in categorical data to adjust totals so they match univariate totals
 # For example, the households by income across categories should sum to equal total HHs
@@ -148,41 +174,12 @@ final_2023 <- scaling_vars_updated %>%
   
 # Remove max variables
   
-  select(-max_age,-max_gq,-max_size,-max_workers,-max_kids,-max_income,-max_occ, -max_eth) %>% 
+  select(-max_age,-max_gq,-max_size,-max_workers,-max_kids,-max_income,-max_occ, -max_eth) 
   
-# Join with non-scaled vars
-  
-  left_join(.,non_scaling_vars,by=c("ZONE", "DISTRICT", "SD", "COUNTY", "County_Name"))
-
 ### End of recoding
-
-# Write out ethnic variables
-
-ethnic <- temp_rounded_adjusted %>% 
-  select(TAZ1454,hispanic, white_nonh,black_nonh,asian_nonh,other_nonh,TOTPOP, COUNTY, County_Name)
-
-write.csv (ethnic,file = "TAZ1454_Ethnicity.csv",row.names = FALSE)
-
-# Read in old PBA data sets and select variables for joining to new 2015 dataset
-# Bring in updated 2020 employment for joining
-# Bring in school and parking data from PBA 2040, 2015 TAZ data 
-# Add HHLDS variable (same as TOTHH), select new 2015 output
-
-PBA2015_joiner <- PBA2015%>%
-  select(ZONE,DISTRICT,SD,TOTACRE,RESACRE,CIACRE,PRKCST,OPRKCST,AREATYPE,HSENROLL,COLLFTE,COLLPTE,TOPOLOGY,TERMINAL, ZERO)
-
-#employment_2015 <- read.csv(employment_2015_data,header=TRUE) 
-
-
-joined_15_20      <- left_join(PBA2015_joiner,temp_rounded_adjusted, by=c("ZONE"="TAZ1454")) # Join 2015 topology, parking, enrollment
-joined_employment <- left_join(joined_15_20,employment_2015, by=c("ZONE"="TAZ1454"))        # Join employment
-
-# Save R version of data for 2020 to later inflate to 2023
-
-
 # Write out subsets of final 2020 data
  
-New2020 <- joined_employment %>%
+New2023 <- final_2023 %>%
   mutate(hhlds=TOTHH) %>%
   select(ZONE,DISTRICT,SD,COUNTY,TOTHH,HHPOP,TOTPOP,EMPRES,SFDU,MFDU,HHINCQ1,HHINCQ2,HHINCQ3,HHINCQ4,TOTACRE,
          RESACRE,CIACRE,SHPOP62P,TOTEMP,AGE0004,AGE0519,AGE2044,AGE4564,AGE65P,RETEMPN,FPSEMPN,HEREMPN,AGREMPN,
@@ -191,16 +188,7 @@ New2020 <- joined_employment %>%
 
 # Summarize ACS and employment data by superdistrict for both 2015 and 2020
 
-summed15 <- PBA2015 %>%
-  group_by(DISTRICT) %>%
-  summarize(TOTHH=sum(TOTHH),HHPOP=sum(HHPOP),TOTPOP=sum(TOTPOP),EMPRES=sum(EMPRES),SFDU=sum(SFDU),MFDU=sum(MFDU),
-            HHINCQ1=sum(HHINCQ1),HHINCQ2=sum(HHINCQ2),HHINCQ3=sum(HHINCQ3),HHINCQ4=sum(HHINCQ4),TOTEMP=sum(TOTEMP),
-            AGE0004=sum(AGE0004),AGE0519=sum(AGE0519),AGE2044=sum(AGE2044),AGE4564=sum(AGE4564),AGE65P=sum(AGE65P),
-            RETEMPN=sum(RETEMPN),FPSEMPN=sum(FPSEMPN),HEREMPN=sum(HEREMPN),AGREMPN=sum(AGREMPN),MWTEMPN=sum(MWTEMPN),
-            OTHEMPN=sum(OTHEMPN),HSENROLL=sum(HSENROLL),COLLFTE=sum(COLLFTE),COLLPTE=sum(COLLPTE),gqpop=TOTPOP-HHPOP) %>% 
-  ungroup()
-
-summed20 <- New2020 %>%
+summed23 <- New2023 %>%
   group_by(DISTRICT) %>%
   summarize(TOTHH=sum(TOTHH),HHPOP=sum(HHPOP),TOTPOP=sum(TOTPOP),EMPRES=sum(EMPRES),SFDU=sum(SFDU),MFDU=sum(MFDU),
             HHINCQ1=sum(HHINCQ1),HHINCQ2=sum(HHINCQ2),HHINCQ3=sum(HHINCQ3),HHINCQ4=sum(HHINCQ4),TOTEMP=sum(TOTEMP),
@@ -211,19 +199,18 @@ summed20 <- New2020 %>%
 
 # Export new 2020 data, 2015 and 2020 district summary data
 
-write.csv(New2020, "TAZ1454 2020 Land Use.csv", row.names = FALSE, quote = T)
-write.csv(summed15, "TAZ1454 2015 District Summary.csv", row.names = FALSE, quote = T)
-write.csv(summed20, "TAZ1454 2020 District Summary.csv", row.names = FALSE, quote = T)
+write.csv(New2023, "TAZ1454 2023 Land Use.csv", row.names = FALSE, quote = T)
+write.csv(summed23, "TAZ1454 2023 District Summary.csv", row.names = FALSE, quote = T)
 
 # Select out PopSim variables and export to separate csv
 
-popsim_vars <- temp_rounded_adjusted %>% 
-  rename(TAZ=TAZ1454,gq_tot_pop=gqpop)%>%
+popsim_vars <- final_2023 %>% 
+  rename(TAZ=ZONE,gq_tot_pop=gqpop)%>%
   select(TAZ,TOTHH,TOTPOP,hh_size_1,hh_size_2,hh_size_3,hh_size_4_plus,hh_wrks_0,hh_wrks_1,hh_wrks_2,hh_wrks_3_plus,
          hh_kids_no,hh_kids_yes,HHINCQ1,HHINCQ2,HHINCQ3,HHINCQ4,AGE0004,AGE0519,AGE2044,AGE4564,AGE65P,
          gq_tot_pop,gq_type_univ,gq_type_mil,gq_type_othnon)
 
-write.csv(popsim_vars, "TAZ1454 2020 Popsim Vars.csv", row.names = FALSE, quote = T)
+write.csv(popsim_vars, "TAZ1454 2023 Popsim Vars.csv", row.names = FALSE, quote = T)
 
 # region popsim vars
 popsim_vars_region <- popsim_vars %>% 
@@ -231,10 +218,10 @@ popsim_vars_region <- popsim_vars %>%
   group_by(REGION) %>%
   summarize(gq_num_hh_region=sum(gq_tot_pop))
 
-write.csv(popsim_vars_region, "TAZ1454 2020 Popsim Vars Region.csv", row.names = FALSE, quote = T)
+write.csv(popsim_vars_region, "TAZ1454 2023 Popsim Vars Region.csv", row.names = FALSE, quote = T)
 
 # county popsim vars
-popsim_vars_county <- joined_15_20 %>%
+popsim_vars_county <- final_2023 %>%
   group_by(COUNTY) %>% summarize(
     pers_occ_management  =sum(pers_occ_management),
     pers_occ_professional=sum(pers_occ_professional),
@@ -243,6 +230,16 @@ popsim_vars_county <- joined_15_20 %>%
     pers_occ_manual      =sum(pers_occ_manual),
     pers_occ_military    =sum(pers_occ_military))
 
-write.csv(popsim_vars_county, "TAZ1454 2020 Popsim Vars County.csv", row.names = FALSE, quote = T)
+write.csv(popsim_vars_county, "TAZ1454 2023 Popsim Vars County.csv", row.names = FALSE, quote = T)
+
+# Output into Tableau-friendly format
+
+Tableau2023 <- New2023 %>%
+  select(ZONE,TOTHH,HHPOP,TOTPOP,EMPRES,SFDU,MFDU,HHINCQ1,HHINCQ2,HHINCQ3,HHINCQ4,SHPOP62P,TOTEMP,AGE0004,AGE0519,AGE2044,AGE4564,AGE65P,RETEMPN,FPSEMPN,HEREMPN,AGREMPN,
+         MWTEMPN,OTHEMPN,PRKCST,OPRKCST,HSENROLL,COLLFTE,COLLPTE,gqpop) %>%
+  gather(Variable,Value2023,TOTHH,HHPOP,TOTPOP,EMPRES,SFDU,MFDU,HHINCQ1,HHINCQ2,HHINCQ3,HHINCQ4,SHPOP62P,TOTEMP,AGE0004,AGE0519,AGE2044,AGE4564,AGE65P,RETEMPN,FPSEMPN,HEREMPN,AGREMPN,
+         MWTEMPN,OTHEMPN,PRKCST,OPRKCST,HSENROLL,COLLFTE,COLLPTE,gqpop)
+
+write.csv(Tableau2023,"TAZ1454_2023_Tableau_Version.csv",row.names = F)
 
 
