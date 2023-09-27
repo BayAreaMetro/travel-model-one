dc8dab8f65aa17f4d246371cdfcc654009423f1a
Flavia Tsang <flavia.tsang@gmail.com>
2023-09-27 16:33:12 -0700

    added RTP_2025IP to run set

--- utilities/CoreSummaries/copyFilesAcrossScenarios.py
+++ utilities/CoreSummaries/copyFilesAcrossScenarios.py
@@ -57,6 +57,7 @@ OUTPUT_FILES = {
 
 # mapping based on 'run_set' column to location of model runs on M/L
 RUN_SET_MODEL_PATHS = {
+    'RTP_2025IP'    :'M:\\Application\\Model One\\RTP2025\\IncrementalProgress',
     'IP'            :'M:\\Application\\Model One\\RTP2021\\IncrementalProgress',
     'DraftBlueprint':'M:\\Application\\Model One\\RTP2021\\IncrementalProgress',
     'FinalBlueprint':'M:\\Application\\Model One\\RTP2021\\Blueprint',
