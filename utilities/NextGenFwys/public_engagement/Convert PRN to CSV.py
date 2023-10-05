be85e175456843cbcb79e60e477b434c51b7c693
jose-alatorre <71753814+jose-alatorre@users.noreply.github.com>
2023-10-05 09:52:16 -0700

    Update Convert PRN to CSV.py
    use the last iteration of the path
    Fix the wait time column by replacing "--" with 0
    only sub origin for nan in "a" column
    Change column naming convention

--- utilities/NextGenFwys/public_engagement/Convert PRN to CSV.py
+++ utilities/NextGenFwys/public_engagement/Convert PRN to CSV.py
@@ -46,14 +46,12 @@ def save_table(original_file_name, table_lines, output_dir):
 
         # Process the table content
         for line in table_lines[1:]:
+
             if line.startswith('M') & (line.startswith('Metropolitan') == False):  # Check if the line starts with 'M'
                 row = line.split()  # Split the line into parts
                 if (len(row)>7) & (len(row)< 15):
                     csvwriter.writerow(row[1:])  # Append the parts (excluding the first element 'M') to the data list
-            # elif (line.startswith(('', 'Metropolitan', '-')) == False):
-            #     row = line.split()
-            #     if (len(row)>7) & (len(row)< 15):
-            #         csvwriter.writerow(row)
+
             elif (line.startswith(('1','2','3','4','5','6','7','8','9','0',\
                 ' 1',' 2',' 3',' 4',' 5',' 6',' 7',' 8',' 9',' 0',\
                 '  1','  2','  3','  4','  5','  6','  7','  8','  9','  0',\
@@ -64,6 +62,7 @@ def save_table(original_file_name, table_lines, output_dir):
 
 # Define a function to extract tables from multiple PRN files
 def extract_tables_from_prn_files(input_dir, output_dir):
+    
     # Generate a list of PRN file names (formatted as TPPLXXXX.PRN)
     # prn_files = [f for f in os.listdir(input_dir) if f.endswith(".PRN")]
     prn_files = ["TPPL{:04d}.PRN".format(i) for i in range(116, 126)]
@@ -82,16 +81,20 @@ def process_saved_csvs(input_dir):
     # Get a list of all CSV files in the specified directory
     csv_files = [f for f in os.listdir(input_dir)]
 
+    # Initialize a list to hold all the data frames
+    df_list = []
+
     # Loop through the list of CSV files
     for csv_file in csv_files:
         input_path = os.path.join(input_dir, csv_file)  # Get the full path of the CSV file
-        # print('path: ' + input_path)
+
         # Define column names for the CSV
-        column_names = ['Column1', 'Column2', 'Column3', 'Column4', 'Column5', 'Column6', 'Column7', 'Column8', 'Column9', 'Column10', 'Column11']
-        final_columns = ['Column1', 'Column2', 'Column3', 'Column4', 'Column5', 'Column6', 'Column7', 'Column8']
+        column_names = ['mode', 'b', 'wait', 'time', 'actual', 'percvd', 'dist', 'total', 'lines', 'lines (2)', 'lines (3)']
+        final_columns = ['mode', 'lines', 'wait', 'time', 'b']
+
         # Read the CSV into a DataFrame and select specific columns
-        table = pd.read_csv(input_path, names = column_names)
-        table = table[final_columns]
+        df = pd.read_csv(input_path, names = column_names)
+        df = df[final_columns]
 
         # Get the origin from the file name
         origin = csv_file.split('_')[1].split('-')[0]
@@ -100,22 +103,44 @@ def process_saved_csvs(input_dir):
         # Get the iteration number from the file name
         iteration = csv_file.split('_')[0].split('L')[1]
 
-        # Add an 'origin' column to the DataFrame
-        table['Column9'] = table['Column2'].shift(1)
+        # Add an 'a' column to the DataFrame
+        df['a'] = df['b'].shift(1)
+        # Fill NaN values in 'a' column with the first origin
+        df['a'].fillna(origin, inplace = True)
+        # cast 'a' column as integers
+        df['a'] = df['a'].astype(int)
 
-        # Fill NaN values in 'origin' column with the first origin
-        table.fillna(origin, inplace = True)
-
-        # cast Column9 as integers
-        table['Column9'] = table['Column9'].astype(int)
-        # add origin destination TAZ columns
-        table['Column10'] = int(origin)
-        table['Column11'] = int(destination)
+        # add origin TAZ column
+        df['origin'] = int(origin)
+        # add destination TAZ column
+        df['destination'] = int(destination)
         # add iteration column
-        table['Column12'] = int(iteration)
+        df['iteration'] = int(iteration)
 
-        # Overwrite the original file with the processed table
-        table.to_csv(input_path, index=False, header = False)
+        # replace all occurences of '--' with 0 in 'wait'
+        df['wait'] = df['wait'].replace('--', 0).astype(float)
+
+        # extract the name of the line before the weight value and overwrite the column with these cleaned strings
+        df['lines'] = df['lines'].str.extract(r'(.+?)\(')
+
+        # Overwrite the original file with the processed df
+        df.to_csv(input_path, index=False, header = False)
+
+        # Append the DataFrame to the list
+        df_list.append(df)
+
+    # Concatenate all DataFrames in the list
+    combined_df = pd.concat(df_list)
+
+    # Find the maximum value of 'iteration' for each group of 'origin' and 'destination'
+    max_iteration = combined_df.groupby(['origin', 'destination'])['iteration'].transform('max')
+
+    # Filter the DataFrame for rows where 'iteration' is equal to the maximum value
+    combined_df = combined_df[combined_df['iteration'] == max_iteration]
+
+    # Save the combined DataFrame as a single CSV
+    combined_path = os.path.join(input_dir, 'TPPL.csv')
+    combined_df.to_csv(combined_path, index=False, header=False)
 
 if __name__ == "__main__":
     input_dir = os.getcwd()  # Use the current directory
