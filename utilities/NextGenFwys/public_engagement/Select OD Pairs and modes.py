783044e757640309a66b9a8524bc9623578cc200
jose-alatorre <71753814+jose-alatorre@users.noreply.github.com>
2023-10-16 20:37:54 -0700

    Update Select OD Pairs and modes.py
    change merge to find the closest travel time from the skims for the path to the calculated route path

--- utilities/NextGenFwys/public_engagement/Select OD Pairs and modes.py
+++ utilities/NextGenFwys/public_engagement/Select OD Pairs and modes.py
@@ -37,8 +37,11 @@ def clean_existing_table(existing_table):
     return existing_table
 
 # Define a function to merge with another table
-def merge_with_existing(df_processed, existing_table):
-    return pd.merge(existing_table, df_processed, on=['orig_taz', 'dest_taz', 'access_egress'], how='inner')
+def merge_with_existing(df_processed, existing_table, access_egress):
+    print('df_processed:')
+    print(df_processed)
+    existing_table[access_egress] = existing_table[access_egress].fillna('')
+    return pd.merge(existing_table, df_processed, on=['orig_taz', 'dest_taz', access_egress], how='inner')
 
 # Define a list of relevant pathways to process the files for
 pathways = ['2035_TM152_NGF_NP10', '2035_TM152_NGF_NP10_Path1a_02']
@@ -48,33 +51,41 @@ transit_paths_dir = 'L:\\Application\\Model_One\\NextGenFwys\\metrics\\Engagamen
 
 
 # Initialize a list to hold all the data frames
-combined_df = pd.DataFrame(columns=['orig_taz', 'dest_taz', 'actual', 'runid', 'fare', 'access_egress', 'total'])
+combined_df = pd.DataFrame(columns=['orig_taz', 'dest_taz', 'actual', 'runid', 'fare', 'total',\
+                                    "wlk_loc_wlk", "wlk_lrf_wlk", "wlk_exp_wlk", "wlk_hvy_wlk", "wlk_com_wlk", "drv_loc_wlk", "drv_lrf_wlk",\
+                                    "drv_exp_wlk", "drv_hvy_wlk", "drv_com_wlk", "wlk_loc_drv", "wlk_lrf_drv", "wlk_exp_drv", "wlk_hvy_drv", "wlk_com_drv"])
 
 for runid in pathways:
     directory = os.path.join(scenarios_dir, runid, 'OUTPUT', 'skims_csv')
 
     existing_table = pd.read_csv(os.path.join(transit_paths_dir,f'TPPL_{runid}.csv'))
-    existing_table.columns = ['mode', 'lines', 'wait', 'time', 'actual', 'b', 'a', 'orig_taz', 'dest_taz', 'iteration', 'runid', 'access_egress']
-    final_columns = ['orig_taz', 'dest_taz', 'actual', 'runid', 'access_egress']
+    existing_table.columns = ['mode', 'lines', 'wait', 'time', 'actual', 'b', 'a', 'orig_taz', 'dest_taz', 'iteration', 'runid',\
+                              "wlk_loc_wlk", "wlk_lrf_wlk", "wlk_exp_wlk", "wlk_hvy_wlk", "wlk_com_wlk", "drv_loc_wlk", "drv_lrf_wlk",\
+                              "drv_exp_wlk", "drv_hvy_wlk", "drv_com_wlk", "wlk_loc_drv", "wlk_lrf_drv", "wlk_exp_drv", "wlk_hvy_drv", "wlk_com_drv"]
+    final_columns = ['orig_taz', 'dest_taz', 'actual', 'runid',\
+                     "wlk_loc_wlk", "wlk_lrf_wlk", "wlk_exp_wlk", "wlk_hvy_wlk", "wlk_com_wlk", "drv_loc_wlk", "drv_lrf_wlk",\
+                     "drv_exp_wlk", "drv_hvy_wlk", "drv_com_wlk", "wlk_loc_drv", "wlk_lrf_drv", "wlk_exp_drv", "wlk_hvy_drv", "wlk_com_drv"]
     existing_table = clean_existing_table(existing_table[final_columns])
 
     # Iterate through files in the directory
     for filename in os.listdir(directory):
-        if filename.endswith(".csv") and 'trnskmam' in filename:
+        if filename.endswith(".csv") and 'trnskmam' in filename and'_trn_' not in filename:
             # pull access/egress combination from filename
             access_egress = filename.split('trnskmam_')[-1].split('.')[0]
             file_path = os.path.join(directory, filename)
             print('read: ' + file_path)
             df_processed = process_csv(file_path)
-            df_processed['access_egress'] = access_egress
-            merged_table = merge_with_existing(df_processed, existing_table)
+            df_processed[access_egress] = access_egress
+            merged_table = merge_with_existing(df_processed, existing_table, access_egress)
             print('merged table:')
             print(merged_table)
             # Append the DataFrame to the list
             combined_df = pd.concat([combined_df, merged_table], ignore_index=True)
 
 # drop duplicate rows
-combined_df = combined_df.drop_duplicates(['orig_taz', 'dest_taz', 'access_egress', 'runid'])
+combined_df['absolute_difference'] = abs(combined_df['actual'] - combined_df['total'])
+combined_df = combined_df.sort_values(by=['absolute_difference'], ascending=True).drop_duplicates(['orig_taz', 'dest_taz', 'runid'])
+combined_df = combined_df[['orig_taz', 'dest_taz', 'actual', 'runid', 'fare', 'total']]
 # Print the final merged table
 print(combined_df)
 
