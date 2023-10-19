483bceda1923f1f87850e2b465e67ce6ce06a0af
David Hensle <david.hensle@rsginc.com>
2023-10-19 17:08:04 +0000

    renamed extractor to OUTPUT

--- utilities/RTP/ExtractKeyFiles.bat
+++ utilities/RTP/ExtractKeyFiles.bat
@@ -6,132 +6,132 @@
 ::
 :: lmz - Create version for PBAU (Plan Bay Area Update)
 ::
-:: This just pulls output into extractor\
+:: This just pulls output into OUTPUT\
 ::
 :: See also CopyFilesToM.bat, which is meant to run afterwards
 ::~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 echo STARTED EXTRACTOR RUN  %DATE% %TIME% >> logs\feedback.rpt
 
 :: Create the needed directories
-mkdir extractor
+mkdir OUTPUT
 
 :: Assume this is set already
 :: set ITER=3
 
 :: Highway assignment results
-copy hwy\iter%ITER%\avgload5period.net            extractor\avgload5period.net
-copy hwy\iter%ITER%\avgload5period.csv            extractor\avgload5period.csv
-copy hwy\iter%ITER%\avgload5period_vehclasses.csv extractor\avgload5period_vehclasses.csv
+copy hwy\iter%ITER%\avgload5period.net            OUTPUT\avgload5period.net
+copy hwy\iter%ITER%\avgload5period.csv            OUTPUT\avgload5period.csv
+copy hwy\iter%ITER%\avgload5period_vehclasses.csv OUTPUT\avgload5period_vehclasses.csv
 
 :: Transit assignment results
-mkdir extractor\trn
-copy trn\TransitAssignment.iter3\trnline*.csv    extractor\trn
-copy trn\trnlink*.dbf                            extractor\trn
-copy trn\trnlink.csv                             extractor\trn
-copy trn\trnline.csv                             extractor\trn
-copy trn\quickboards.xls                         extractor\trn
+mkdir OUTPUT\trn
+copy trn\TransitAssignment.iter3\trnline*.csv    OUTPUT\trn
+copy trn\trnlink*.dbf                            OUTPUT\trn
+copy trn\trnlink.csv                             OUTPUT\trn
+copy trn\trnline.csv                             OUTPUT\trn
+copy trn\quickboards.xls                         OUTPUT\trn
 
 :: Demand results
-copy main\householdData_%ITER%.csv extractor\main\householdData_%ITER%.csv
-copy main\personData_%ITER%.csv    extractor\main\personData_%ITER%.csv
-copy main\indivTripData_%ITER%.csv extractor\main\indivTripData_%ITER%.csv
-copy main\indivTourData_%ITER%.csv extractor\main\indivTourData_%ITER%.csv
-copy main\jointTripData_%ITER%.csv extractor\main\jointTripData_%ITER%.csv
-copy main\jointTourData_%ITER%.csv extractor\main\jointTourData_%ITER%.csv
-copy main\wsLocResults_%ITER%.csv  extractor\main\wsLocResults_%ITER%.csv
+copy main\householdData_%ITER%.csv OUTPUT\main\householdData_%ITER%.csv
+copy main\personData_%ITER%.csv    OUTPUT\main\personData_%ITER%.csv
+copy main\indivTripData_%ITER%.csv OUTPUT\main\indivTripData_%ITER%.csv
+copy main\indivTourData_%ITER%.csv OUTPUT\main\indivTourData_%ITER%.csv
+copy main\jointTripData_%ITER%.csv OUTPUT\main\jointTripData_%ITER%.csv
+copy main\jointTourData_%ITER%.csv OUTPUT\main\jointTourData_%ITER%.csv
+copy main\wsLocResults_%ITER%.csv  OUTPUT\main\wsLocResults_%ITER%.csv
 
 :: Report results
-copy logs\HwySkims.debug  extractor\HwySkims.debug
-copy logs\feedback.rpt    extractor\feedback.rpt
-copy logs\SpeedErrors.log extractor\SpeedErrors.log
+copy logs\HwySkims.debug  OUTPUT\HwySkims.debug
+copy logs\feedback.rpt    OUTPUT\feedback.rpt
+copy logs\SpeedErrors.log OUTPUT\SpeedErrors.log
 
 :: Skim databases
-mkdir extractor\skimDB
-copy database\*.csv extractor\skimDB\*.csv
+mkdir OUTPUT\skimDB
+copy database\*.csv OUTPUT\skimDB\*.csv
 
 :: Trip tables
-mkdir extractor\main
-copy main\tripsEA.tpp extractor\main
-copy main\tripsAM.tpp extractor\main
-copy main\tripsMD.tpp extractor\main
-copy main\tripsPM.tpp extractor\main
-copy main\tripsEV.tpp extractor\main
-copy main\ShadowPricing_7.csv extractor\main
+mkdir OUTPUT\main
+copy main\tripsEA.tpp OUTPUT\main
+copy main\tripsAM.tpp OUTPUT\main
+copy main\tripsMD.tpp OUTPUT\main
+copy main\tripsPM.tpp OUTPUT\main
+copy main\tripsEV.tpp OUTPUT\main
+copy main\ShadowPricing_7.csv OUTPUT\main
 
-mkdir extractor\nonres
-copy nonres\ixDaily2015.tpp       extractor\nonres
-copy nonres\ixDailyx4.tpp         extractor\nonres
-copy nonres\tripsIX*.tpp          extractor\nonres
-copy nonres\tripsTrk*.tpp         extractor\nonres
+mkdir OUTPUT\nonres
+copy nonres\ixDaily2015.tpp       OUTPUT\nonres
+copy nonres\ixDailyx4.tpp         OUTPUT\nonres
+copy nonres\tripsIX*.tpp          OUTPUT\nonres
+copy nonres\tripsTrk*.tpp         OUTPUT\nonres
 
-:: copy skims\hwyskmEA.tpp extractor\emfac\hwyskmEA.tpp
-:: copy skims\hwyskmAM.tpp extractor\emfac\hwyskmAM.tpp
-:: copy skims\hwyskmMD.tpp extractor\emfac\hwyskmMD.tpp
-:: copy skims\hwyskmPM.tpp extractor\emfac\hwyskmPM.tpp
-:: copy skims\hwyskmEV.tpp extractor\emfac\hwyskmEV.tpp
+:: copy skims\hwyskmEA.tpp OUTPUT\emfac\hwyskmEA.tpp
+:: copy skims\hwyskmAM.tpp OUTPUT\emfac\hwyskmAM.tpp
+:: copy skims\hwyskmMD.tpp OUTPUT\emfac\hwyskmMD.tpp
+:: copy skims\hwyskmPM.tpp OUTPUT\emfac\hwyskmPM.tpp
+:: copy skims\hwyskmEV.tpp OUTPUT\emfac\hwyskmEV.tpp
 
-:: copy skims\com_hwyskimEA.tpp extractor\emfac\com_hwyskimEA.tpp
-:: copy skims\com_hwyskimAM.tpp extractor\emfac\com_hwyskimAM.tpp
-:: copy skims\com_hwyskimMD.tpp extractor\emfac\com_hwyskimMD.tpp
-:: copy skims\com_hwyskimPM.tpp extractor\emfac\com_hwyskimPM.tpp
-:: copy skims\com_hwyskimEV.tpp extractor\emfac\com_hwyskimEV.tpp
+:: copy skims\com_hwyskimEA.tpp OUTPUT\emfac\com_hwyskimEA.tpp
+:: copy skims\com_hwyskimAM.tpp OUTPUT\emfac\com_hwyskimAM.tpp
+:: copy skims\com_hwyskimMD.tpp OUTPUT\emfac\com_hwyskimMD.tpp
+:: copy skims\com_hwyskimPM.tpp OUTPUT\emfac\com_hwyskimPM.tpp
+:: copy skims\com_hwyskimEV.tpp OUTPUT\emfac\com_hwyskimEV.tpp
 
-:: copy nonres\tripsTrkEA.tpp extractor\emfac\tripsTrkEA.tpp
-:: copy nonres\tripsTrkAM.tpp extractor\emfac\tripsTrkAM.tpp
-:: copy nonres\tripsTrkMD.tpp extractor\emfac\tripsTrkMD.tpp
-:: copy nonres\tripsTrkPM.tpp extractor\emfac\tripsTrkPM.tpp
-:: copy nonres\tripsTrkEV.tpp extractor\emfac\tripsTrkEV.tpp
+:: copy nonres\tripsTrkEA.tpp OUTPUT\emfac\tripsTrkEA.tpp
+:: copy nonres\tripsTrkAM.tpp OUTPUT\emfac\tripsTrkAM.tpp
+:: copy nonres\tripsTrkMD.tpp OUTPUT\emfac\tripsTrkMD.tpp
+:: copy nonres\tripsTrkPM.tpp OUTPUT\emfac\tripsTrkPM.tpp
+:: copy nonres\tripsTrkEV.tpp OUTPUT\emfac\tripsTrkEV.tpp
 
 :: Save the control file
-copy CTRAMP\runtime\mtcTourBased.properties extractor\mtcTourBased.properties
-copy CTRAMP\runtime\mtcTourBased.properties extractor\logsums.properties
+copy CTRAMP\runtime\mtcTourBased.properties OUTPUT\mtcTourBased.properties
+copy CTRAMP\runtime\mtcTourBased.properties OUTPUT\logsums.properties
 
 :: Accessibility files
-mkdir extractor\accessibilities
-copy accessibilities\nonMandatoryAccessibilities.csv extractor\accessibilities
-copy accessibilities\mandatoryAccessibilities.csv    extractor\accessibilities
-copy skims\accessibility.csv                         extractor\accessibilities
+mkdir OUTPUT\accessibilities
+copy accessibilities\nonMandatoryAccessibilities.csv OUTPUT\accessibilities
+copy accessibilities\mandatoryAccessibilities.csv    OUTPUT\accessibilities
+copy skims\accessibility.csv                         OUTPUT\accessibilities
 
 :: Accessibility files - model results
-mkdir extractor\logsums
-copy logsums\householdData_%ITER%.csv                extractor\logsums
-copy logsums\personData_%ITER%.csv                   extractor\logsums
-copy logsums\indivTripData_%ITER%.csv                extractor\logsums
-copy logsums\indivTourData_%ITER%.csv                extractor\logsums
-copy logsums\wsLocResults_%ITER%.csv                 extractor\logsums
+mkdir OUTPUT\logsums
+copy logsums\householdData_%ITER%.csv                OUTPUT\logsums
+copy logsums\personData_%ITER%.csv                   OUTPUT\logsums
+copy logsums\indivTripData_%ITER%.csv                OUTPUT\logsums
+copy logsums\indivTourData_%ITER%.csv                OUTPUT\logsums
+copy logsums\wsLocResults_%ITER%.csv                 OUTPUT\logsums
 :: summaries from logsumJoiner
-copy logsums\shopDCLogsum.csv                        extractor\logsums
-copy logsums\tour_shopDCLogsum.csv                   extractor\logsums
-copy logsums\workDCLogsum.csv                        extractor\logsums
-copy logsums\person_workDCLogsum.csv                 extractor\logsums
-copy logsums\mandatoryAccessibilities.csv            extractor\logsums
-copy logsums\nonMandatoryAccessibilities.csv         extractor\logsums
+copy logsums\shopDCLogsum.csv                        OUTPUT\logsums
+copy logsums\tour_shopDCLogsum.csv                   OUTPUT\logsums
+copy logsums\workDCLogsum.csv                        OUTPUT\logsums
+copy logsums\person_workDCLogsum.csv                 OUTPUT\logsums
+copy logsums\mandatoryAccessibilities.csv            OUTPUT\logsums
+copy logsums\nonMandatoryAccessibilities.csv         OUTPUT\logsums
 
-mkdir extractor\logsums\ForUrbanSim
-copy logsums\mandatoryAccessibilities.csv            extractor\logsums\ForUrbanSim\mandatoryAccessibilities_%model_year%_%UrbanSimScenario%.csv 
-copy logsums\nonMandatoryAccessibilities.csv         extractor\logsums\ForUrbanSim\nonMandatoryAccessibilities_%model_year%_%UrbanSimScenario%.csv   
-copy core_summaries\AccessibilityMarkets.csv         extractor\logsums\ForUrbanSim\AccessibilityMarkets_%model_year%_%UrbanSimScenario%.csv   
+mkdir OUTPUT\logsums\ForUrbanSim
+copy logsums\mandatoryAccessibilities.csv            OUTPUT\logsums\ForUrbanSim\mandatoryAccessibilities_%model_year%_%UrbanSimScenario%.csv 
+copy logsums\nonMandatoryAccessibilities.csv         OUTPUT\logsums\ForUrbanSim\nonMandatoryAccessibilities_%model_year%_%UrbanSimScenario%.csv   
+copy core_summaries\AccessibilityMarkets.csv         OUTPUT\logsums\ForUrbanSim\AccessibilityMarkets_%model_year%_%UrbanSimScenario%.csv   
 
 :: Core summaries
-mkdir extractor\core_summaries
-copy core_summaries\*.*                              extractor\core_summaries
-mkdir extractor\updated_output
-copy updated_output\*.*                              extractor\updated_output
+mkdir OUTPUT\core_summaries
+copy core_summaries\*.*                              OUTPUT\core_summaries
+mkdir OUTPUT\updated_output
+copy updated_output\*.*                              OUTPUT\updated_output
 
 :: metrics
-mkdir extractor\metrics
-copy metrics extractor\metrics
+mkdir OUTPUT\metrics
+copy metrics OUTPUT\metrics
 if exist metrics\ITHIM (
-  mkdir extractor\metrics\ITHIM
-  copy metrics\ITHIM\*.* extractor\metrics\ITHIM
+  mkdir OUTPUT\metrics\ITHIM
+  copy metrics\ITHIM\*.* OUTPUT\metrics\ITHIM
 )
 
 :: make generating shapefile outputs easier
-mkdir extractor\shapefile
-copy X:\travel-model-one-master\utilities\cube-to-shapefile\run_CubeToShapefile.bat extractor\shapefile
+mkdir OUTPUT\shapefile
+copy X:\travel-model-one-master\utilities\cube-to-shapefile\run_CubeToShapefile.bat OUTPUT\shapefile
 
 :success
-echo ExtractKeyFiles into extractor for PBAU Complete
-echo ENDED EXTRACTOR RUN  %DATE% %TIME% >> logs\feedback.rpt
+echo ExtractKeyFiles into OUTPUT for PBAU Complete
+echo ENDED OUTPUT RUN  %DATE% %TIME% >> logs\feedback.rpt
 
 :done
