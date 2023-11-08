b814366eee17dedd9d4d92926977b40f1e0aeee9
jose-alatorre <71753814+jose-alatorre@users.noreply.github.com>
2023-11-08 14:55:21 -0800

    Create UpdateMapDataForNewOD.bat
    batch file to update engagement visualization data with new OD pairs

---
+++ utilities/NextGenFwys/public_engagement/UpdateMapDataForNewOD.bat
@@ -0,0 +1,13 @@
+:: run in powershell
+
+cd \\model3-a\Model3A-Share\Projects\2035_TM152_NGF_NP10_Path1a_02
+Set PATH=%PATH%;C:\Program Files\Citilabs\CubeVoyager;C:\Program Files\Citilabs\VoyagerFileAPI
+X:\travel-model-one-master\utilities\NextGenFwys\public_engagement\TraceTransitPaths.bat 
+
+cd \\MODEL2-C\Model2C-Share\Projects\2035_TM152_NGF_NP10_Path4_02
+Set PATH=%PATH%;C:\Program Files\Citilabs\CubeVoyager;C:\Program Files\Citilabs\VoyagerFileAPI
+X:\travel-model-one-master\utilities\NextGenFwys\public_engagement\TraceTransitPaths.bat 
+
+cd \\MODEL2-D\Model2D-Share\Projects\2035_TM152_NGF_NP10
+Set PATH=%PATH%;C:\Program Files\Citilabs\CubeVoyager;C:\Program Files\Citilabs\VoyagerFileAPI
+X:\travel-model-one-master\utilities\NextGenFwys\public_engagement\TraceTransitPaths.bat 
