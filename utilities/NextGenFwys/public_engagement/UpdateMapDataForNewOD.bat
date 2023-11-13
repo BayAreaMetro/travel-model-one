f231a34a0bf30af4722631dfec638802a2fe391b
jose-alatorre <71753814+jose-alatorre@users.noreply.github.com>
2023-11-13 14:00:37 -0800

    improving process of updating OD map tableau data
     - create script to run multiple batch files sequentially
     - edit TraceDApaths.bat to take in OD lists as arguments

--- utilities/NextGenFwys/public_engagement/UpdateMapDataForNewOD.bat
+++ utilities/NextGenFwys/public_engagement/UpdateMapDataForNewOD.bat
@@ -1,13 +1,32 @@
 :: update paths based on mapped drives on respective modeling server
 
+set ORIGIN_LIST=1,2,3,4,5
+set DESTINATION_LIST=10,20,30,40,50
+
+@echo off
+
+:: saved on \\model3-a\Model3A-Share
+X:
 cd X:\Projects\2035_TM152_NGF_NP10_Path1a_02
 Set PATH=%PATH%;C:\Program Files\Citilabs\CubeVoyager;C:\Program Files\Citilabs\VoyagerFileAPI
-X:\travel-model-one-master\utilities\NextGenFwys\public_engagement\TraceTransitPaths.bat 
+call X:\travel-model-one-master\utilities\NextGenFwys\public_engagement\TraceDApaths.bat %ORIGIN_LIST% %DESTINATION_LIST%
 
-cd Y:\Projects\2035_TM152_NGF_NP10_Path4_02
+:: saved on \\model3-a\Model3A-Share
+X:
+cd X:\Projects\2035_TM152_NGF_NP10_Path1a_02
 Set PATH=%PATH%;C:\Program Files\Citilabs\CubeVoyager;C:\Program Files\Citilabs\VoyagerFileAPI
-X:\travel-model-one-master\utilities\NextGenFwys\public_engagement\TraceTransitPaths.bat 
+call X:\travel-model-one-master\utilities\NextGenFwys\public_engagement\TraceTransitPaths.bat 
 
+::saved on \\MODEL2-C\Model2C-Share
+Z:
+cd Z:\Projects\2035_TM152_NGF_NP10_Path4_02
+Set PATH=%PATH%;C:\Program Files\Citilabs\CubeVoyager;C:\Program Files\Citilabs\VoyagerFileAPI
+call X:\travel-model-one-master\utilities\NextGenFwys\public_engagement\TraceTransitPaths.bat 
+
+:: saved on \\MODEL2-D\Model2D-Share
+F:
 cd F:\Projects\2035_TM152_NGF_NP10
 Set PATH=%PATH%;C:\Program Files\Citilabs\CubeVoyager;C:\Program Files\Citilabs\VoyagerFileAPI
-X:\travel-model-one-master\utilities\NextGenFwys\public_engagement\TraceTransitPaths.bat 
+call X:\travel-model-one-master\utilities\NextGenFwys\public_engagement\TraceTransitPaths.bat 
+
+pause
