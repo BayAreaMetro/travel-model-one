c112ad51bb1371550b44568f70a66409f3ecac19
Lisa Zorn <lzorn@bayareametro.gov>
2023-11-21 11:00:36 -0800

    2035_TM160_IPA_09 is currently the all-in IPA run

--- utilities/RTP/config_RTP2025/SetUpModel_2035IPA.bat
+++ utilities/RTP/config_RTP2025/SetUpModel_2035IPA.bat
@@ -5,7 +5,7 @@
 :: ------------------------------------------------------------------------------------------------------
 
 :: set the location of the model run folder on M; this is where the input and output directories will be copied to
-set M_DIR=M:\Application\Model One\RTP2025\IncrementalProgress\2035_TM160_IPA_07
+set M_DIR=M:\Application\Model One\RTP2025\IncrementalProgress\2035_TM160_IPA_09
 
 :: Should strategies be included? AddStrategies=Yes for Project runs; AddStrategies=No for NoProject runs.
 set AddStrategies=Yes
