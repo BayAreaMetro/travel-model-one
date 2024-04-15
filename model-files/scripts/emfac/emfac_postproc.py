49692b8afe5ba506cf12a242325483b5b7a5222a
Lisa Zorn <lzorn@bayareametro.gov>
2024-04-14 18:21:50 -0700

    Update to be more flexible for multiple EMFAC runs

--- model-files/scripts/emfac/emfac_postproc.py
+++ model-files/scripts/emfac/emfac_postproc.py
@@ -1,115 +1,107 @@
 USAGE = """
 
-    This script is to be run after EMFAC is completed.
-    It takes the EMFAC output file as an input and then write out a concise summary of the regional level results.
+  This script is to be run after EMFAC is completed (any number of times).
+  Run from the model run directory on M.
 
-    To run this script, start from the model run directory, e.g. A:\Projects\2050_TM152_FBP_PlusCrossing_16
-
-    - Run this script, with two arguments: SB375 or EIR, and M_DIR
-      e.g. python ctramp\scripts\emfac\emfac_postproc.py SB375  "M:\Application\Model One\RTP2025\IncrementalProgress\2035_TM160_IPA_17"
+  This script will find all the EMFAC output files and extract relevant information, writing a summary file
+  with all information included to OUTPUT\\emfac\\emfac_summary.csv
+   
+  Columns are:
+  - analysis_type
+  - source
+  - emfac_version
+  - season
+  - sb375 = bool
+  - planning = bool
+  - run_timestamp
+  - Veh_Tech
+  - VMT
+  - Trips
+  - CO2_RUNEX
+  - CO2_IDLEX
+  - CO2_STREX
+  - CO2_TOTEX
 
 """
 
-import argparse, glob, shutil, os
+import os, pathlib, re, sys
 import openpyxl
 import pandas as pd
 
 # -------------------------------------------------------------------
 # Input/output file names and locations
 # -------------------------------------------------------------------
-project_dir = os.getcwd()
-run_id = project_dir.split('\\')[-1]
+CWD              = pathlib.Path.cwd()
+MODEL_RUN_ID     = CWD.name  # run this from M_DIR or model run dir on modeling machine
+CWD_OUTPUT_EMFAC = CWD / "OUTPUT/emfac"
+OUTPUT_FILE      = CWD_OUTPUT_EMFAC / "emfac_summary.csv"
+# columns to extract
+EXTRACT_COLUMNS  = ['Veh_Tech', 'VMT', 'Trips', 'CO2_RUNEX', 'CO2_IDLEX', 'CO2_STREX', 'CO2_TOTEX']
 
-parser = argparse.ArgumentParser(description=USAGE)
-parser.add_argument("emfacVersion", help="Specify SB375 or EIR", choices=["SB375","EIR"])
-parser.add_argument("M_DIR", help="Specify the M_DIR path for this model run")
-args = parser.parse_args()
+# emfac output file ends with timestamp: _YYYYMMDDHHMMSS.xlsx  e.g. _20240410131422.xlsx
+# by convention, it's E[emfacversion]_[model_run_id]_[season][_sb375?]_[YYYYMMDDHHMMSS].xlsx
+EMFAC_OUTPUT_STR = f"E(?P<emfac_version>2014|2017|2021)_{MODEL_RUN_ID}_(?P<season>annual|summer|winter)(?P<planning>_planning)?(?P<sb375>_sb375)?_(?P<run_timestamp>\d{{14}}).xlsx"
+EMFAC_OUTPUT_RE  = re.compile(EMFAC_OUTPUT_STR)
 
-if args.emfacVersion=="SB375":
-    # this is the path for the output of EMFAC 2014, used for SB375
-    EMFAC_PATH = "\\\mainmodel\MainModelShare\emfac\emfac2014_v1.0.7\output"
+if __name__ == '__main__':
 
-if args.emfacVersion=="EIR":
-    # this is the path for the output of EMFAC 2014, used for SB375
-    EMFAC_PATH = "\\\mainmodel\MainModelShare\emfac\emfac2017-v1.0.2\EMFAC2017-v1.0.2\output"
+    print(f"MODEL_RUN_ID     = {MODEL_RUN_ID}")
+    print(f"CWD_OUTPUT_EMFAC = {CWD_OUTPUT_EMFAC}")
+    print(f"EMFAC_OUTPUT_STR = {EMFAC_OUTPUT_STR}")
+    print("")
 
-for emfacfile in glob.glob(EMFAC_PATH + "\\" + "ready4emfac_" + run_id + "_sb375*.xlsx"):
-    print(emfacfile)
-    shutil.copy2(emfacfile, project_dir + "/emfac_prep")
+    all_emfac_run_data = [] # list of dicts
+    emfac_xlsx_files = sorted(CWD_OUTPUT_EMFAC.glob("**/*.xlsx"))
+    for emfac_xlsx_file in emfac_xlsx_files:
+        
+        relative_emfac_filepath = emfac_xlsx_file.relative_to(CWD_OUTPUT_EMFAC)
+        print(f"Checking {relative_emfac_filepath}")
 
-emfac_output_xlsx = emfacfile.split('\\')[-1]
+        match = EMFAC_OUTPUT_RE.match(emfac_xlsx_file.name)
+        if match == None: 
+            print("  -> not an EMFAC output file")
+            continue
 
-emfac_output_xlsx_fullpath = "emfac_prep\\" + emfac_output_xlsx
+        # fetch metadata
+        emfac_run_data = {
+            'analysis_type':    'unknown',
+            'source':           str(emfac_xlsx_file),
+        }
+        if relative_emfac_filepath.parts[0] in ['SB375','EIR','AQConformity']:
+            emfac_run_data['analysis_type'] = relative_emfac_filepath.parts[0]
+        emfac_run_data['emfac_version'] = match.group('emfac_version')
+        emfac_run_data['season']        = match.group('season')
+        emfac_run_data['sb375' ]        = True if match.group('sb375')=="_sb375" else False
+        emfac_run_data['planning' ]     = True if match.group('planning')=="_planning" else False
+        emfac_run_data['run_timestamp'] = match.group('run_timestamp')
 
-output_csv = "emfac_prep\\emfac_ghg.csv"
+        # Load workbook
+        workbook = openpyxl.load_workbook(filename=emfac_xlsx_file)
+        print(f"  workbook.sheetnames:{workbook.sheetnames}")
 
-# -------------------------------------------------------------------
-# Read the results in the "By Sub-Area" tab and then write them out
-# -------------------------------------------------------------------
+        if 'Total MTC' in workbook.sheetnames:
+            print(f"  Reading 'Total MTC' worksheet")
+            total_mtc_df = pd.read_excel(emfac_xlsx_file, sheet_name="Total MTC")
 
-print("\n\n================================================================")
-print("Generating a regional-level summary of the EMFAC output")
-print("================================================================")
+            # remove leading or trailing spaces
+            total_mtc_df.Veh_Tech = total_mtc_df.Veh_Tech.str.strip()
 
-print("\nLoading the workbook "+emfac_output_xlsx_fullpath)
-workbook = openpyxl.load_workbook(filename=emfac_output_xlsx_fullpath)
-print("\nWhat are the different tabs in this workbook?")
-print(workbook.sheetnames)
+            # select row with All Vehicles
+            total_mtc_df = total_mtc_df.loc[ total_mtc_df.Veh_Tech == 'All Vehicles', :]
+            assert(len(total_mtc_df)==1)
+            all_veh_series = total_mtc_df.squeeze()
+            # print(all_veh_series)
+            for extract_column in EXTRACT_COLUMNS:
+                emfac_run_data[extract_column] = all_veh_series[extract_column]
 
-# make "By Sub-Area" the active tab
-sheet = workbook["By Sub-Area"]
-print("\nActivated the tab:")
-print(sheet)
+            all_emfac_run_data.append(emfac_run_data)
+            continue
+    
+    if len(all_emfac_run_data) == 0:
+        print("Did not find any EMFAC output workbooks to process")
+        sys.exit(0)
 
-# Read the results in the "By Sub-Area" tab
-print("\nReading the data from the <By Sub-Area> tab")
-EMFACresults = sheet.values
-
-# Set the first row as the headers for the DataFrame
-cols = next(EMFACresults)
-EMFACresults = list(EMFACresults)
-
-EMFACresults_df = pd.DataFrame(EMFACresults, columns=cols)
-
-# Read the results in the "By Sub-Area" tab
-print("\nFinished reading the data from the <By Sub-Area> tab")
-
-# keep if the column "EMFAC2007 Category" = "All Vehicles"
-EMFACsummary_df = EMFACresults_df.loc[EMFACresults_df['EMFAC2007 Category'] == " All Vehicles"]
-
-# keep only the relevant columns
-EMFACsummary_df = EMFACsummary_df[['EMFAC2007 Category','VMT', 'Trips', 'CO2_RUNEX', 'CO2_IDLEX', 'CO2_STREX', 'CO2_TOTEX']]
-
-# calculate the sum for the region
-EMFACsummary_df = EMFACsummary_df.groupby('EMFAC2007 Category', as_index=False).sum()
-
-# add a column named directory
-EMFACsummary_df['Directory'] = run_id
-
-# drop the column named "EMFAC2007 Category"
-EMFACsummary_df = EMFACsummary_df[['Directory','VMT', 'Trips', 'CO2_RUNEX', 'CO2_IDLEX', 'CO2_STREX', 'CO2_TOTEX']]
-
-EMFACsummary_df.to_csv(output_csv, header=True, index=False)
-
-# Read the results in the "By Sub-Area" tab
-print("\nFinished writing out the regional-level EMFAC results to emfac_prep\\emfac_ghg.csv")
-
-# copy emfac_ghg.csv back to the metrics folder in the model output directory on M
-M_OUTPUT = os.path.join(args.M_DIR, "OUTPUT") 
-if not os.path.isdir(M_OUTPUT):
-    os.mkdir(M_OUTPUT)
-
-M_METRICS = os.path.join(args.M_DIR, "OUTPUT", "metrics") 
-if not os.path.isdir(M_METRICS):
-    os.mkdir(M_METRICS)
-
-shutil.copy2("emfac_prep\\emfac_ghg.csv", os.path.join(args.M_DIR,"OUTPUT","metrics"))
-
-# copy the output of EMFAC 2014 back to the metrics folder in the model output directory on M
-M_EMFAC = os.path.join(args.M_DIR, "OUTPUT", "emfac") 
-if not os.path.isdir(M_EMFAC):
-    os.mkdir(M_EMFAC)
-
-for emfacfile in glob.glob(EMFAC_PATH + "\\" + "ready4emfac_" + run_id + "_sb375*.xlsx"):
-    print(emfacfile)
-    os.rename(shutil.copy2(emfacfile, M_EMFAC), os.path.join(M_EMFAC, "emfac_" + run_id + "_sb375.xlsx"))
+    # write it
+    all_emfac_run_data_df = pd.DataFrame(all_emfac_run_data)
+    all_emfac_run_data_df.to_csv(OUTPUT_FILE, index=False)
+    print(f"Wrote {len(all_emfac_run_data_df)} rows to {OUTPUT_FILE}")
