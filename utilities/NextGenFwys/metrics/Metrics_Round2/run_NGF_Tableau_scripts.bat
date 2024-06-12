db5bc4186cc1729822227eea59db37d49a27a10a
jose-alatorre <71753814+jose-alatorre@users.noreply.github.com>
2024-06-12 11:27:10 -0700

    set default to skip files if they exist

--- utilities/NextGenFwys/metrics/Metrics_Round2/run_NGF_Tableau_scripts.bat
+++ utilities/NextGenFwys/metrics/Metrics_Round2/run_NGF_Tableau_scripts.bat
@@ -1,9 +1,9 @@
 ::
 :: This batch script runs the metric scripts needed to update the metrics tableau for the NextGenFwys study.
 :: run this from L:\Application\Model_One\NextGenFwys_Round2\Metrics
-:: optional environment variable SKIP
+:: optional environment variable SKIP, default set to True
 :: example usage:
-:: set SKIP=--skip_if_exists
+:: set SKIP=n
 :: "X:\travel-model-one-master\utilities\NextGenFwys\metrics\Metrics_Round2\run_NGF_Tableau_scripts.bat"
 ::
 
@@ -18,22 +18,8 @@ set CODE_DIR=X:\travel-model-one-master\utilities\NextGenFwys\metrics\Metrics_Ro
 :: Location of the model files
 set TARGET_DIR=%CD%
 
-rem Check if the variable SKIP equals "--skip_if_exists"
-IF "%SKIP%"=="--skip_if_exists" (
-  echo skipping if files exist
-  call python "%CODE_DIR%\Affordable1_transportation_costs.py" --skip_if_exists
-  call python "%CODE_DIR%\Affordable2_ratio_time_cost.py" --skip_if_exists
-  call python "%CODE_DIR%\Change_in_vmt_from_auto_times.py" --skip_if_exists
-  call python "%CODE_DIR%\Change_in_vmt_from_loaded_network.py" --skip_if_exists
-  call python "%CODE_DIR%\Efficient1_ratio_travel_time.py" --skip_if_exists
-  call python "%CODE_DIR%\Efficient2_commute_tours_mode_share.py" --skip_if_exists
-  call python "%CODE_DIR%\Efficient2b_non_commute_trips_mode_share.py" --skip_if_exists
-  call python "%CODE_DIR%\Reliable1_change_travel_time.py" --skip_if_exists
-  call python "%CODE_DIR%\Reliable2_ratio_peak_nonpeak.py" --skip_if_exists
-  call python "%CODE_DIR%\Top_level_metrics_toll_revenues.py" --skip_if_exists
-  call python "%CODE_DIR%\run_fatalities_Rscript.py" --skip_if_exists
-
-) else (
+rem Check if the variable SKIP equals "n"
+IF "%SKIP%"=="n" (
   echo not skipping
   call python "%CODE_DIR%\Affordable1_transportation_costs.py"
   call python "%CODE_DIR%\Affordable2_ratio_time_cost.py"
@@ -46,6 +32,20 @@ IF "%SKIP%"=="--skip_if_exists" (
   call python "%CODE_DIR%\Reliable2_ratio_peak_nonpeak.py"
   call python "%CODE_DIR%\Top_level_metrics_toll_revenues.py"
   call python "%CODE_DIR%\run_fatalities_Rscript.py"
+
+) else (
+  echo skipping if files exist
+  call python "%CODE_DIR%\Affordable1_transportation_costs.py" --skip_if_exists
+  call python "%CODE_DIR%\Affordable2_ratio_time_cost.py" --skip_if_exists
+  call python "%CODE_DIR%\Change_in_vmt_from_auto_times.py" --skip_if_exists
+  call python "%CODE_DIR%\Change_in_vmt_from_loaded_network.py" --skip_if_exists
+  call python "%CODE_DIR%\Efficient1_ratio_travel_time.py" --skip_if_exists
+  call python "%CODE_DIR%\Efficient2_commute_tours_mode_share.py" --skip_if_exists
+  call python "%CODE_DIR%\Efficient2b_non_commute_trips_mode_share.py" --skip_if_exists
+  call python "%CODE_DIR%\Reliable1_change_travel_time.py" --skip_if_exists
+  call python "%CODE_DIR%\Reliable2_ratio_peak_nonpeak.py" --skip_if_exists
+  call python "%CODE_DIR%\Top_level_metrics_toll_revenues.py" --skip_if_exists
+  call python "%CODE_DIR%\run_fatalities_Rscript.py" --skip_if_exists
 )
 
 rem Update Round 2 across_runs_union folder
