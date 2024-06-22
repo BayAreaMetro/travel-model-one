10552740a4c6f52d994cb3f8bd9a0efe7062279b
Flavia Tsang <flavia.tsang@gmail.com>
2024-06-21 17:14:19 -0700

    do this for both /INPUT/hwy and /hwy

--- utilities/toll_calibration/Swap_networks_prior_to_tollcalib.bat
+++ utilities/toll_calibration/Swap_networks_prior_to_tollcalib.bat
@@ -17,8 +17,8 @@ rem ---------------------------------------------
 rem User inputs
 rem ---------------------------------------------
 
-set PreTollCalibRun=\\MODEL2-D\Model2D-Share\Projects\2035_TM160_NGFr2_NP04_Path4_01_pretollcalib
-set NewNetwork=L:\Application\Model_One\NextGenFwys_Round2\INPUT_DEVELOPMENT\Networks\NGF_Networks_NGFround2_P4_09\NGF_R2_R2P4_2035_Express_Lanes_network_2035
+set PreTollCalibRun=\\MODEL2-B\Model2B-Share\Projects\2035_TM160_NGFr2_NP04_Path6_01_pretollcalib
+set NewNetwork=L:\Application\Model_One\NextGenFwys_Round2\INPUT_DEVELOPMENT\Networks\NGF_Networks_NGFround2_P6_13\NGF_R2_R2P6_Dual_Express_Lanes_network_2035
 
 
 rem ---------------------------------------------
@@ -27,17 +27,52 @@ rem
 rem Commands to replace the freeflow.net and tolls.csv in pre-toll-calibration run (aka the base run):
 rem ---------------------------------------------
 
+rem ------------------------
+rem freeflow.net
+rem ------------------------
+
+rem -------------
+rem in /hwy
+rem -------------
+
 rem save a copy of the original freeflow.net
 copy %PreTollCalibRun%\hwy\freeflow.net %PreTollCalibRun%\hwy\freeflow_used_in_pre_toll_calibration.net
 
 rem replace it with the revised freeflow.net
 copy %NewNetwork%\hwy\freeflow.net %PreTollCalibRun%\hwy\freeflow.net
 
+rem -------------
+rem in /INPUT/hwy
+rem -------------
+rem save a copy of the original freeflow.net
+copy %PreTollCalibRun%\INPUT\hwy\freeflow.net %PreTollCalibRun%\INPUT\hwy\freeflow_used_in_pre_toll_calibration.net
+
+rem replace it with the revised freeflow.net
+copy %NewNetwork%\hwy\freeflow.net %PreTollCalibRun%\INPUT\hwy\freeflow.net
+
+rem ------------------------
+rem tolls.csv
+rem ------------------------
+
+rem -------------
+rem in /hwy
+rem -------------
 
 rem save a copy of the original tolls.csv
 copy %PreTollCalibRun%\hwy\tolls.csv %PreTollCalibRun%\hwy\tolls_used_in_pre_toll_calibration.csv
 
 rem replace it with the revised freeflow.net
 copy %NewNetwork%\hwy\tolls.csv %PreTollCalibRun%\hwy\tolls.csv
+
+
+rem -------------
+rem in /INPUT/hwy
+rem -------------
+
+rem save a copy of the original tolls.csv
+copy %PreTollCalibRun%\INPUT\hwy\tolls.csv %PreTollCalibRun%\INPUT\hwy\tolls_used_in_pre_toll_calibration.csv
+
+rem replace it with the revised freeflow.net
+copy %NewNetwork%\hwy\tolls.csv %PreTollCalibRun%\INPUT\hwy\tolls.csv
 
 pause
