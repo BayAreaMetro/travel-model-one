663207e2f94b2af9ae7ee3294cf7edf0c4d90aec
anuptapase <atapase@bayareametro.gov>
2024-07-02 12:03:40 -0700

    Update Affordable2_ratio_time_cost.py
    debug SR85 - similar to previous two fixes; this one for the A2 script

--- utilities/NextGenFwys/metrics/Metrics_Round2/Affordable2_ratio_time_cost.py
+++ utilities/NextGenFwys/metrics/Metrics_Round2/Affordable2_ratio_time_cost.py
@@ -441,7 +441,7 @@ def Affordable2_ratio_time_cost(tm_run_id):
     
     network_with_nonzero_tolls = tm_loaded_network_df.loc[(tm_loaded_network_df['USEAM'] == 1)|(tm_loaded_network_df['USEAM'] == 4)]
     network_with_nonzero_tolls = network_with_nonzero_tolls.loc[(network_with_nonzero_tolls['FT'] != 6)]
-    if any(x in tm_run_id for x in ['Path4', 'Path5', 'Path6']):
+    if any(x in tm_run_id for x in ['NoProject','Path4', 'Path5', 'Path6']):
         network_with_nonzero_tolls = network_with_nonzero_tolls.loc[(network_with_nonzero_tolls['TOLLCLASS'] == 0)]
     network_with_nonzero_tolls['sum of tolls'] = network_with_nonzero_tolls['TOLLAM_DA'] + network_with_nonzero_tolls['TOLLAM_LRG'] + network_with_nonzero_tolls['TOLLAM_S3']
     # check if run has all lane tolling, if not return 0 for this metric 
