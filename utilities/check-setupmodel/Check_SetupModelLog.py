b440dbf2934eacbcc7b6fb9a3b6fad3ba5062904
Lisa Zorn <lzorn@bayareametro.gov>
2024-12-02 11:59:24 -0800

    Output error line and line before it

--- utilities/check-setupmodel/Check_SetupModelLog.py
+++ utilities/check-setupmodel/Check_SetupModelLog.py
@@ -7,27 +7,30 @@
 # (5) 'The filename, directory name, or volume label syntax is incorrect'; and
 # (6) [SomeProcess] is not recognized as an internal or external command'
 
-import os
+import os, sys
 
 ErrorInSetupModel=0
 
 f = open('setupmodel.log')
 
 log_contents = f.read()
-print("Read {} chars from setupmodel.log".format(len(log_contents)))
-
-if ('The system cannot find' in log_contents) or \
-   ('The network name cannot be found' in log_contents) or \
-   ('The specified path is invalid' in log_contents) or \
-   ('The filename, directory name, or volume label syntax is incorrect' in log_contents) or \
-   ('not recognized as an internal or external command' in log_contents):
-    ErrorInSetupModel=1
-    print ("Setupmodel.log contains errors.")
-
-    # produce an assertion error if setupmodel.log cotains any of the above errors
-    assert ErrorInSetupModel==0
-
-else:
-    print ("Setupmodel.log does not contain any obvious errors.")
-
+print("Read {:,} chars from setupmodel.log".format(len(log_contents)))
 f.close()
+
+log_lines = log_contents.splitlines()
+
+for line_num in range(len(log_lines)):
+    if (('The system cannot find' in log_lines[line_num]) or \
+        ('The network name cannot be found' in log_lines[line_num]) or \
+        ('The specified path is invalid' in log_lines[line_num]) or \
+        ('The filename, directory name, or volume label syntax is incorrect' in log_lines[line_num]) or \
+        ('not recognized as an internal or external command' in log_lines[line_num])):
+
+        print(f"Setupmodel.log contains errors on line {line_num+1}:\n{log_lines[line_num-1]}\n{log_lines[line_num]}")
+
+        # return an error if setupmodel.log contains any of the above errors
+        sys.exit(999)
+
+print ("Setupmodel.log does not contain any obvious errors.")
+
+
