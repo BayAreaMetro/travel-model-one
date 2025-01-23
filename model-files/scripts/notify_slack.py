ec39d6aa07c5e92948e3fb308f10ba5a8defdd0c
Lisa Zorn <lzorn@bayareametro.gov>
2025-01-22 17:19:21 -0800

    Move more functionality into post_message() for other scripts to use

--- model-files/scripts/notify_slack.py
+++ model-files/scripts/notify_slack.py
@@ -12,21 +12,6 @@ def post_message(message):
     Posts the given message to the slack channel via the webhook if SLACK_WEBHOOK_URL
     Also prints to console
     """
-    headers  = { 'Content-type':'application/json'}
-    data     = { "text": message }
-    if SLACK_WEBHOOK_URL:
-        response = requests.post(SLACK_WEBHOOK_URL, headers=headers, json=data)
-
-    print("*** {}".format(message))
-    print("response: {}".format(response))
-    print("")
-
-if __name__ == '__main__':
-
-    parser = argparse.ArgumentParser(description=USAGE, formatter_class=argparse.RawDescriptionHelpFormatter,)
-    parser.add_argument("message", type=str, help="The message to send")
-    args = parser.parse_args()
-
     hostname = socket.getfqdn()
     instance = os.environ['INSTANCE']
     if hostname.endswith(".mtc.ca.gov"):
@@ -42,5 +27,21 @@ if __name__ == '__main__':
     f.close()
     print("Read slack webhook URL: {}".format(SLACK_WEBHOOK_URL))
 
-    post_message("*{}*: {}".format(instance, args.message))
+    headers  = { 'Content-type':'application/json'}
+    data     = { "text": message }
+    if SLACK_WEBHOOK_URL:
+        response = requests.post(SLACK_WEBHOOK_URL, headers=headers, json=data)
+
+    full_message = f"*{instance}*: {message}"
+    print("*** {}".format(full_message))
+    print("response: {}".format(response))
+    print("")
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description=USAGE, formatter_class=argparse.RawDescriptionHelpFormatter,)
+    parser.add_argument("message", type=str, help="The message to send")
+    args = parser.parse_args()
+
+    post_message(args.message)
     # that's all
